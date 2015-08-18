8eb585ed810a69eb5f729ecf56ce111af12855a7
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-08-18 13:53:09 +0100

    Extracted common test functionality into a rule.

--- src/test/java/uk/gov/pay/connector/it/PaymentDaoITest.java
+++ src/test/java/uk/gov/pay/connector/it/PaymentDaoITest.java
@@ -1,43 +1,24 @@
 package uk.gov.pay.connector.it;
 
-import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
-import uk.gov.pay.connector.app.ConnectorApp;
-import uk.gov.pay.connector.app.ConnectorConfiguration;
 import uk.gov.pay.connector.dao.PaymentDao;
-import uk.gov.pay.connector.util.PostgresDockerRule;
+import uk.gov.pay.connector.util.DropwizardAppWithPostgresRule;
 
-import static io.dropwizard.testing.ConfigOverride.config;
-import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class PaymentDaoITest {
-    private String configFilePath = resourceFilePath("config/test-it-config.yaml");
-
-    private PostgresDockerRule postgres = new PostgresDockerRule();
-
-    private DropwizardAppRule<ConnectorConfiguration> app = new DropwizardAppRule<>(
-            ConnectorApp.class,
-            configFilePath,
-            config("database.url", postgres.getConnectionUrl()),
-            config("database.user", postgres.getUsername()),
-            config("database.password", postgres.getPassword()));
 
     @Rule
-    public RuleChain rules = RuleChain.outerRule(postgres).around(app);
+    public DropwizardAppWithPostgresRule app = new DropwizardAppWithPostgresRule();
 
     private PaymentDao paymentDao;
 
     @Before
     public void setUp() throws Exception {
-        ConnectorApp connectorApp = app.getApplication();
-        connectorApp.run("db", "drop-all", "--confirm-delete-everything", configFilePath);
-        connectorApp.run("db", "migrate", configFilePath);
-        paymentDao = new PaymentDao(connectorApp.getJdbi());
+        paymentDao = new PaymentDao(app.getJdbi());
     }
 
     @Test
