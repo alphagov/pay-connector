f3f4d00be2e49f7e7edc58b401314f2dd8584a0e
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-08-18 15:45:24 +0100

    Assert the content type of the returned JSON.

--- src/test/java/uk/gov/pay/connector/it/ChargeRequestTest.java
+++ src/test/java/uk/gov/pay/connector/it/ChargeRequestTest.java
@@ -1,12 +1,12 @@
 package uk.gov.pay.connector.it;
 
-import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.ValidatableResponse;
 import org.junit.Rule;
 import org.junit.Test;
 import uk.gov.pay.connector.util.DropwizardAppWithPostgresRule;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.http.ContentType.JSON;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
@@ -20,11 +20,12 @@ public class ChargeRequestTest {
     public void makeChargeAndRetrieveAmount() throws Exception {
         int expectedAmount = 2113;
         ValidatableResponse response = given().port(app.getLocalPort())
-                .contentType(ContentType.JSON)
+                .contentType(JSON)
                 .body(String.format("{\"amount\":%d}", expectedAmount))
                 .post("/v1/api/charge")
                 .then()
-                .statusCode(201);
+                .statusCode(201)
+                .contentType(JSON);
         String chargeId = response.extract().path("charge_id");
 
         response.header("location", containsString("frontend/charge/" + chargeId));
@@ -33,6 +34,7 @@ public class ChargeRequestTest {
                 .get("/v1/frontend/charge/" + chargeId)
                 .then()
                 .statusCode(200)
+                .contentType(JSON)
                 .extract()
                 .path("amount");
 
