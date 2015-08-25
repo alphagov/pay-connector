dbcffa8f8900c7f28a41d0dd43b55ee8401c41bb
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-08-25 10:56:06 +0100

    Uncommented and fixed tests.

--- src/test/java/uk/gov/pay/connector/it/dao/ChargeDaoITest.java
+++ src/test/java/uk/gov/pay/connector/it/dao/ChargeDaoITest.java
@@ -1,13 +1,18 @@
 package uk.gov.pay.connector.it.dao;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import uk.gov.pay.connector.dao.ChargeDao;
 import uk.gov.pay.connector.util.DropwizardAppWithPostgresRule;
 
+import java.util.Map;
+import java.util.UUID;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class ChargeDaoITest {
 
@@ -26,20 +31,29 @@ public class ChargeDaoITest {
     @Test
     public void insertANewChargeAndReturnTheId() throws Exception {
         long amount = 100;
-//        Charge charge = chargeDao.saveNewCharge(amount, gateway_account);
-//        assertThat(charge.getStatus(), is("CREATED"));
-//        assertThat(charge.getAmount(), is(100));
+        Map<String, Object> newCharge = newCharge(amount);
+
+        long chargeId = chargeDao.saveNewCharge(newCharge);
+        assertThat(chargeId, is(1L));
     }
 
     @Test
     public void insertAmountAndThenGetAmountById() throws Exception {
         long expectedAmount = 101;
-//        UUID chargeId = chargeDao.saveNewCharge(expectedAmount, gateway_account).getId();
+        long chargeId = chargeDao.saveNewCharge(newCharge(expectedAmount));
 
-//        Charge charge = chargeDao.findById(chargeId);
-//
-//        assertThat(charge.getAmount(), is(expectedAmount));
-//        assertThat(charge.getStatus(), is("CREATED"));
-//        assertThat(charge.getGatewayAccount(), is(gateway_account));
+        Map<String, Object> charge = chargeDao.findById(chargeId);
+
+        assertThat(charge.get("amount"), is(expectedAmount));
+        assertThat(charge.get("status"), is("CREATED"));
+        assertThat(charge.get("gateway_account"), is(nullValue()));
     }
+
+
+    private ImmutableMap<String, Object> newCharge(long amount) {
+        return ImmutableMap.of(
+                "amount", amount,
+                "gateway_account", gateway_account);
+    }
+
 }
