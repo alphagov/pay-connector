6d4905d0747b4df96a9e9fb2b2441bd856c2d325
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-09-14 15:43:04 +0100

    PP-117 Added additional sandbox card numbers.

--- src/main/java/uk/gov/pay/connector/resources/CardDetailsValidator.java
+++ src/main/java/uk/gov/pay/connector/resources/CardDetailsValidator.java
@@ -9,7 +9,7 @@ public class CardDetailsValidator {
     public static final String EXPIRY_DATE_FIELD = "expiry_date";
 
     public static boolean isWellFormattedCardDetails(Map<String, Object> cardDetails) {
-        return cardDetails.containsKey(CARD_NUMBER_FIELD) && is16Digits(cardDetails.get(CARD_NUMBER_FIELD)) &&
+        return cardDetails.containsKey(CARD_NUMBER_FIELD) && isValidCardNumberLength(cardDetails.get(CARD_NUMBER_FIELD)) &&
                 cardDetails.containsKey(CVC_FIELD) && is3Digits(cardDetails.get(CVC_FIELD)) &&
                 cardDetails.containsKey(EXPIRY_DATE_FIELD) && hasExpiryDateFormat(cardDetails.get(EXPIRY_DATE_FIELD));
     }
@@ -18,8 +18,8 @@ public class CardDetailsValidator {
         return date != null && date.toString().matches("[0-9]{2}/[0-9]{2}");
     }
 
-    private static boolean is16Digits(Object number) {
-        return number != null && number.toString().matches("[0-9]{16}");
+    private static boolean isValidCardNumberLength(Object number) {
+        return number != null && number.toString().matches("[0-9]{14,16}");
     }
 
     private static boolean is3Digits(Object number) {
