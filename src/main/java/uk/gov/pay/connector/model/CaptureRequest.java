ad7a955eacd546f0a57367142fe25c1d785f29b2
kasun.kumara <kasun.kumara@gmail.com>
2015-09-24 17:29:14 +0100

    PP-121 @kakumara incorporating PR feedbacks - 2

--- src/main/java/uk/gov/pay/connector/model/CaptureRequest.java
+++ src/main/java/uk/gov/pay/connector/model/CaptureRequest.java
@@ -11,8 +11,8 @@ public class CaptureRequest {
         this.transactionId = transactionId;
     }
 
-    public static CaptureRequest captureRequest(String chargeId, String amount) {
-        return new CaptureRequest(amount, chargeId);
+    public static CaptureRequest captureRequest(String transactionId, String amount) {
+        return new CaptureRequest(amount, transactionId);
     }
 
     public String getAmount() {
