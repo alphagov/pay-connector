58c2a0f3b59a5b2047dfaa9597c8cd05e6dd5050
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-10-15 16:51:06 +0100

    pp-147 Removed unhelpful comments.

--- src/main/java/uk/gov/pay/connector/service/worldpay/WorldpayStatusesMapper.java
+++ src/main/java/uk/gov/pay/connector/service/worldpay/WorldpayStatusesMapper.java
@@ -19,25 +19,6 @@ public class WorldpayStatusesMapper {
                 .put("REFUSED", AUTHORISATION_REJECTED)
                 .put("REFUSED_BY_BANK", AUTHORISATION_REJECTED).build();
 
-//    "SIGNED_FORM_RECEIVED"
-//    "CAPTURED_FAILED"
-//    "ERROR"
-//    "CHARGED_BACK"
-//    "SETTLED"
-//    "SETTLED_BY_MERCHANT"
-//    "SENT_FOR_REFUND"
-//    "REFUND_FAILED"
-//    "REFUNDED"
-//    "REFUNDED_BY_MERCHANT"
-//    "EXPIRED"
-//    "INFORMATION_REQUESTED"
-//    "INFORMATION_SUPPLIED"
-//    "CHARGEBACK_REVERSED"
-//    "REFUND_WEBFORM_ISSUED"
-//    "REVOKE_REQUESTED"
-//    "REVOKE_FAILED"
-//    "REVOKED"
-
     public static Optional<ChargeStatus> mapToChargeStatus(String worldpayStatus) {
         return ofNullable(worldpayStatuses.get(worldpayStatus));
     }
