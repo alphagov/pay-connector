49245e5cc9d93441b0b850466d053993b8ea7c61
kakumara <kasun.kumara@gmail.com>
2016-01-29 14:45:45 +0000

    PP-495 fix bug in external status mappings
     - Mapped all internal statuses correctly to an external status
     - Added a test to make sure the integrity between the two status mappings so that
       it makes sure there is an external status for all internal statuses

---
+++ src/test/java/uk/gov/pay/connector/model/api/ExternalChargeStatusTest.java
@@ -0,0 +1,31 @@
+package uk.gov.pay.connector.model.api;
+
+import org.junit.Test;
+import uk.gov.pay.connector.model.domain.ChargeStatus;
+
+import java.util.EnumSet;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static uk.gov.pay.connector.model.api.ExternalChargeStatus.*;
+import static uk.gov.pay.connector.model.domain.ChargeStatus.*;
+
+public class ExternalChargeStatusTest {
+
+    @Test
+    public void shouldMapAllInternalStatuesCorrectlyToExternalStatuses() throws Exception {
+        for (ChargeStatus chargeStatus : EnumSet.allOf(ChargeStatus.class)) {
+            assertThat(ExternalChargeStatus.mapFromStatus(chargeStatus), is(instanceOf(ExternalChargeStatus.class)));
+        }
+    }
+
+    @Test
+    public void shouldMapAnInternalStatusToAnExternalStatusCorrectly() throws Exception {
+            assertThat(ExternalChargeStatus.mapFromStatus(READY_FOR_CAPTURE), is(EXT_IN_PROGRESS));
+            assertThat(ExternalChargeStatus.mapFromStatus(CREATED), is(EXT_CREATED));
+            assertThat(ExternalChargeStatus.mapFromStatus(SYSTEM_ERROR), is(EXT_FAILED));
+            assertThat(ExternalChargeStatus.mapFromStatus(CAPTURE_SUBMITTED), is(EXT_SUCCEEDED));
+            assertThat(ExternalChargeStatus.mapFromStatus(SYSTEM_CANCELLED), is(EXT_SYSTEM_CANCELLED));
+    }
+}
