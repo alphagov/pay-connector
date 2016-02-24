bf5d6b27f4308f44f5656a7a7a8f101d4ba5f36a
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2016-02-24 14:00:29 +0000

    PP-542 Migrating to JPA
        * Created TokenJpaDao.
        * IT tests using the new dao.
        * Deprecated old TokenDao.

---
+++ src/main/java/uk/gov/pay/connector/model/domain/TokenEntity.java
@@ -0,0 +1,34 @@
+package uk.gov.pay.connector.model.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tokens")
+@SequenceGenerator(name="charge_tokens_id_seq", sequenceName="charge_tokens_id_seq", allocationSize=1)
+public class TokenEntity extends AbstractEntity {
+
+    @Column(name = "charge_id")
+    private Long chargeId;
+
+    @Column(name = "secure_redirect_token")
+    private String token;
+
+    public TokenEntity() {
+    }
+
+    public TokenEntity(Long chargeId, String token) {
+        this.chargeId = chargeId;
+        this.token = token;
+    }
+
+    public Long getChargeId() {
+        return chargeId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
