12f3e61a2c68e3393a252dab021c3bbfc470ecac
Kasun Kumara <kasun.kumara@gmail.com>
2016-03-04 15:55:30 +0000

    PP-438 enabling http proxy for connector outbound traffic
     - This is done by optionally setting `ClientProperties.PROXY_URI` in JerseyClient
     - enabling/disabling can be done by env var `HTTP_PROXY_ENABLED` [true | false]
     - Added a mockerserver based integration test to test traffic via proxy
     - removed conflicting old servlet-api version (`2.x`) and upgraded to `3.x`

--- src/main/java/uk/gov/pay/connector/app/CustomJerseyClientConfiguration.java
+++ src/main/java/uk/gov/pay/connector/app/CustomJerseyClientConfiguration.java
@@ -4,11 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import io.dropwizard.util.Duration;
 
+import javax.validation.Valid;
+
 public class CustomJerseyClientConfiguration extends Configuration {
     private Duration readTimeout = Duration.milliseconds(20000L);
+
+    @Valid
+    @JsonProperty
+    private String enableProxy;
 
     @JsonProperty
     public Duration getReadTimeout() {
         return this.readTimeout;
+    }
+
+    public boolean isProxyEnabled() {
+        return "true".equals(enableProxy);
     }
 }
--- src/main/java/uk/gov/pay/connector/service/ClientFactory.java
+++ src/main/java/uk/gov/pay/connector/service/ClientFactory.java
@@ -2,6 +2,7 @@ package uk.gov.pay.connector.service;
 
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.client.JerseyClientConfiguration;
+import io.dropwizard.client.proxy.ProxyConfiguration;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.util.Duration;
 import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
@@ -28,10 +29,42 @@ public class ClientFactory {
         Duration readTimeout = conf.getCustomJerseyClient().getReadTimeout();
         int readTimeoutInMillis = (int) (readTimeout.toMilliseconds());
 
-        return new JerseyClientBuilder(environment)
+        JerseyClientBuilder defaultClientBuilder = new JerseyClientBuilder(environment)
                 .using(connectorProvider)
                 .using(clientConfiguration)
-                .withProperty(ClientProperties.READ_TIMEOUT, readTimeoutInMillis)
-                .build(name);
+                .withProperty(ClientProperties.READ_TIMEOUT, readTimeoutInMillis);
+
+        // optionally set proxy; see comment below why this has to be done
+        if (conf.getCustomJerseyClient().isProxyEnabled()) {
+            defaultClientBuilder
+                    .withProperty(ClientProperties.PROXY_URI, proxyUrl(clientConfiguration.getProxyConfiguration()));
+        }
+
+        return defaultClientBuilder.build(name);
     }
+
+    /**
+     * Constructs the proxy URL required by JerseyClient property ClientProperties.PROXY_URI
+     * <p>
+     * <b>NOTE:</b> The reason for doing this is, Dropwizard jersey client doesn't seem to work as per
+     * http://www.dropwizard.io/0.9.2/docs/manual/configuration.html#proxy where just setting the proxy config in
+     * client configuration is only needed. But after several test, that doesn't seem to work, but by setting the
+     * native jersey proxy config as per this implementation seems to work
+     * <p>
+     * similar problem discussed in here -> https://groups.google.com/forum/#!topic/dropwizard-user/AbDSYfLB17M
+     * </p>
+     * </p>
+     *
+     * @param proxyConfig from config.yml
+     * @return proxy server URL
+     */
+    private String proxyUrl(ProxyConfiguration proxyConfig) {
+        return String.format("%s://%s:%s",
+                proxyConfig.getScheme(),
+                proxyConfig.getHost(),
+                proxyConfig.getPort()
+        );
+    }
+
 }
+
