d0e25a8c2ee80c25584b0d8e32c8283ceebaa879
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2016-03-08 19:13:12 +0000

    PP-596 Disable external DTD validation
        * Changed JAXB to use SAX parser to be able to set new properties.
        * Added features to disable DTD validations.
        * Set namespaceAware to true to be able to parse SOAP XMls.
    
    with @kakumara

---
+++ src/test/java/uk/gov/pay/connector/it/util/XMLUnmarshallerTest.java
@@ -0,0 +1,57 @@
+package uk.gov.pay.connector.it.util;
+
+import com.google.common.io.Resources;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockserver.integration.ClientAndServer;
+import uk.gov.pay.connector.service.worldpay.WorldpayCancelResponse;
+import uk.gov.pay.connector.util.XMLUnmarshaller;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.socket.PortFactory.findFreePort;
+import static org.mockserver.verify.VerificationTimes.exactly;
+
+public class XMLUnmarshallerTest {
+
+    private static final int MOCKSERVER_PORT = findFreePort();
+
+    private ClientAndServer mockServer;
+
+    @Before
+    public void setup() {
+        mockServer = startClientAndServer(MOCKSERVER_PORT);
+    }
+
+    @Test
+    public void shouldUnmarshallXmlIgnoringDTD() throws Exception {
+
+        String successPayload = Resources.toString(Resources.getResource("templates/it/worldpay-cancel-notfication-example-it-dtd-validation-disabled.xml"), Charset.defaultCharset())
+                .replace("{{transactionId}}", "MyTransactionId")
+                .replace("{{port}}", String.valueOf(MOCKSERVER_PORT));
+
+        mockServer
+                .when(request().withMethod("GET").withPath("/paymentService_v1.dtd"))
+                .respond(response().withStatusCode(200));
+
+        WorldpayCancelResponse response = XMLUnmarshaller.unmarshall(successPayload, WorldpayCancelResponse.class);
+
+        assertTrue(response.isCancelled());
+        assertThat(response.getErrorMessage(), is(nullValue()));
+
+        mockServer.verify(request().withPath("/paymentService_v1.dtd"), exactly(0));
+    }
+
+    @After
+    public void tearDown() {
+        mockServer.stop();
+    }
+}
