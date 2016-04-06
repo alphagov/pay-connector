af56dc46ab5497a46afa270157edce5ae5f59d4e
rahul-vinayak <rahul_vinayak@hotmail.com>
2016-04-06 14:31:23 +0100

    PP-565 Refactoring GatewayResponse
     - GatewayResponse is now an abstract class instead of an interface.
    This is required so that we can store the response status from the
    gateway.
    - Created an enum to represent the gateway statuses instead of relying
    on the isSuccessful and isInProgress booleans, which was error prone.
     - Refactored the tests

--- src/main/java/uk/gov/pay/connector/model/GatewayResponse.java
+++ src/main/java/uk/gov/pay/connector/model/GatewayResponse.java
@@ -1,9 +1,34 @@
 package uk.gov.pay.connector.model;
 
-public interface GatewayResponse {
+import static uk.gov.pay.connector.model.GatewayResponse.ResponseStatus.FAILED;
+import static uk.gov.pay.connector.model.GatewayResponse.ResponseStatus.IN_PROGRESS;
+import static uk.gov.pay.connector.model.GatewayResponse.ResponseStatus.SUCCEDED;
 
-    public Boolean isSuccessful();
-    public ErrorResponse getError();
-    // TODO gotta change this usage of boolean logics, may be with a String/Enum to represent the success or error or progress
-    public Boolean isInProgress();
+public abstract class GatewayResponse {
+
+
+    public enum ResponseStatus {
+        IN_PROGRESS,
+        SUCCEDED,
+        FAILED;
+    }
+    protected ResponseStatus responseStatus = null;
+
+    public abstract ErrorResponse getError();
+
+    public ResponseStatus getResponseStatus() {
+        return responseStatus;
+    }
+
+    public boolean isSuccessful() {
+        return responseStatus.equals(SUCCEDED);
+    }
+    public boolean isInProgress() {
+        return responseStatus.equals(IN_PROGRESS);
+    }
+    public boolean isFailed() {
+        return responseStatus.equals(FAILED);
+    }
 }
+
+
