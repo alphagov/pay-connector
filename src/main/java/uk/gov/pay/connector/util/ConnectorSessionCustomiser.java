3b50a4951731e22ac31b27d5c1fa37360df6e584
imranbohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-05-19 09:40:06 +0100

    PP-712 Configuring eclipselink session
    * Adding a eclipselink session customiser that configures the number of
    retrys and the duration between retries.
    * The number of retries is 0 based so for 1 retry we have to configure it
    to be 0.
    * The duration between retries is reduced to 2 seconds. This is
    5 seconds by default.
    * Also reducing the validation query timeout.

---
+++ src/main/java/uk/gov/pay/connector/util/ConnectorSessionCustomiser.java
@@ -0,0 +1,18 @@
+package uk.gov.pay.connector.util;
+
+import org.eclipse.persistence.config.SessionCustomizer;
+import org.eclipse.persistence.sessions.DatabaseLogin;
+import org.eclipse.persistence.sessions.Session;
+
+public class ConnectorSessionCustomiser implements SessionCustomizer {
+
+    private static final int QUERY_RETRY_ATTEMPT_COUNT_ZERO_BASED_INDEX = 0;
+    private static final int DELAY_BETWEEN_CONNECTION_ATTEMPTS_MILLIS = 2000;
+
+    @Override
+    public void customize(Session session) throws Exception {
+        DatabaseLogin datasourceLogin = (DatabaseLogin) session.getDatasourceLogin();
+        datasourceLogin.setQueryRetryAttemptCount(QUERY_RETRY_ATTEMPT_COUNT_ZERO_BASED_INDEX);
+        datasourceLogin.setDelayBetweenConnectionAttempts(DELAY_BETWEEN_CONNECTION_ATTEMPTS_MILLIS);
+    }
+}
