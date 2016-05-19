3338f82793b1b2a73faf8755e6c262613410d46e
Ian Maddison <ian@i.need.stronger.coffee>
2016-05-19 12:51:07 +0100

    PP-541 Change Confirmed to Success based on feedback from team. Remove Captured state and include capture in Success. We will introduce some basic settlement information in future.

--- src/main/java/uk/gov/pay/connector/model/api/ExternalChargeState.java
+++ src/main/java/uk/gov/pay/connector/model/api/ExternalChargeState.java
@@ -17,14 +17,12 @@ public enum ExternalChargeState {
     EXTERNAL_STARTED("started", false, null),
     EXTERNAL_SUBMITTED("submitted", false, null),
 
-    EXTERNAL_CONFIRMED("confirmed", true, true),
+    EXTERNAL_SUCCESS("success", true, true),
     EXTERNAL_FAILED_REJECTED("failed", true, false, "P0010", "Payment method rejected"),
     EXTERNAL_FAILED_EXPIRED("failed", true, false, "P0020", "Payment expired"),
     EXTERNAL_FAILED_CANCELLED("failed", true, false, "P0030", "Payment was cancelled by the user"),
     EXTERNAL_CANCELLED("cancelled", true, false, "P0040", "Payment was cancelled by the service"),
     EXTERNAL_ERROR_GATEWAY("error", true, false, "P0050", "Payment provider returned an error"),
-
-    EXTERNAL_CAPTURED("captured", true, true),
 
     ;
 
@@ -64,11 +62,6 @@ public enum ExternalChargeState {
     }
 
     public static List<ExternalChargeState> fromStatusString(String status) {
-        // temporary code to allow migration from Confirmed/Captured to Success 
-        if ("success".equals(status)) {
-            return Arrays.asList(EXTERNAL_CONFIRMED, EXTERNAL_CAPTURED);
-        }
-      
         List<ExternalChargeState> valid = stream(values()).filter(v -> v.getStatus().equals(status)).collect(Collectors.toList());
 
         if (valid.isEmpty()) {
