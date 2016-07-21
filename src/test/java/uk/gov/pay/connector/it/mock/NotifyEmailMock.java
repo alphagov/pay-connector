9214f5141abe9bb3733b682db5909a1b8c1999d4
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2016-07-21 12:38:11 +0100

    PP-446 add integration test for email personalisation
    Add test to chweck that email personalisation params
    are being correctly passed through.

--- src/test/java/uk/gov/pay/connector/it/mock/NotifyEmailMock.java
+++ src/test/java/uk/gov/pay/connector/it/mock/NotifyEmailMock.java
@@ -2,6 +2,8 @@ package uk.gov.pay.connector.it.mock;
 
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 
+import java.util.Map;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -22,6 +24,33 @@ public class NotifyEmailMock {
                 post(urlPathEqualTo("/notifications/email"))
                         .withRequestBody(
                                 matching(".*to*.*template.*")
+                        )
+                        .willReturn(
+                                responseDefBuilder
+                        )
+        );
+    }
+
+    public void responseWithEmailRequestResponseMatchingPersonalisation(
+            int statusCode, String payload, Map expectedPersonalisationParams, int timeout) {
+        ResponseDefinitionBuilder responseDefBuilder = aResponse()
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                .withStatus(statusCode)
+                .withBody(payload);
+
+        if (timeout >= 0) {
+            responseDefBuilder.withFixedDelay(timeout);
+        }
+
+        stubFor(
+                post(urlPathEqualTo("/notifications/email"))
+                        .withRequestBody(
+                                matching(".*amount*.*" + expectedPersonalisationParams.get("amount") +
+                                        "*.*serviceReference*.*" + expectedPersonalisationParams.get("serviceReference") +
+                                        "*.*description*.*" + expectedPersonalisationParams.get("description") +
+                                        "*.*serviceName*.*" + expectedPersonalisationParams.get("serviceName") +
+                                        "*.*customParagraph*.*" + expectedPersonalisationParams.get("customParagraph") + ".*"
+                                        )
                         )
                         .willReturn(
                                 responseDefBuilder
