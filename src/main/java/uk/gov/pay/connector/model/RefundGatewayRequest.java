e8b406651a493853a3e94687a6756504d9e7f073
David Heath <david.heath@digital.cabinet-office.gov.uk>
2016-08-24 12:58:05 +0100

    Pass RefundEntity to RefundGatewayRequest
    the `RefundEntity` is needed so that its external id can be passed
    through in the payload when generating a refund request.
    
    also modified the test fixtures to accomodate this change

--- src/main/java/uk/gov/pay/connector/model/RefundGatewayRequest.java
+++ src/main/java/uk/gov/pay/connector/model/RefundGatewayRequest.java
@@ -2,25 +2,28 @@ package uk.gov.pay.connector.model;
 
 import uk.gov.pay.connector.model.domain.ChargeEntity;
 import uk.gov.pay.connector.model.domain.GatewayAccountEntity;
+import uk.gov.pay.connector.model.domain.RefundEntity;
 
 public class RefundGatewayRequest implements GatewayRequest {
 
     private final GatewayAccountEntity gatewayAccountEntity;
     private final String amount;
     private final String transactionId;
+    private final String refundReference;
 
     private RefundGatewayRequest(ChargeEntity charge) {
-        this(charge, String.valueOf(charge.getAmount()));
+        this(charge, new RefundEntity(charge, charge.getAmount()));
     }
 
-    private RefundGatewayRequest(ChargeEntity charge, long partialAmount) {
-        this(charge, String.valueOf(partialAmount));
+    private RefundGatewayRequest(ChargeEntity charge, RefundEntity refundEntity) {
+        this(charge, String.valueOf(refundEntity.getAmount()), refundEntity.getExternalId());
     }
 
-    private RefundGatewayRequest(ChargeEntity charge, String amount) {
+    private RefundGatewayRequest(ChargeEntity charge, String amount, String refundReference) {
         this.transactionId = charge.getGatewayTransactionId();
         this.gatewayAccountEntity = charge.getGatewayAccount();
         this.amount = amount;
+        this.refundReference = refundReference;
     }
 
     public String getAmount() {
@@ -40,7 +43,7 @@ public class RefundGatewayRequest implements GatewayRequest {
         return new RefundGatewayRequest(charge);
     }
 
-    public static RefundGatewayRequest valueOf(ChargeEntity charge, long partialAmount) {
-        return new RefundGatewayRequest(charge, partialAmount);
+    public static RefundGatewayRequest valueOf(ChargeEntity charge, RefundEntity refundEntity) {
+        return new RefundGatewayRequest(charge, refundEntity);
     }
 }
--- src/test/java/uk/gov/pay/connector/model/domain/RefundEntityFixture.java
+++ src/test/java/uk/gov/pay/connector/model/domain/RefundEntityFixture.java
@@ -3,16 +3,16 @@ package uk.gov.pay.connector.model.domain;
 import static uk.gov.pay.connector.model.domain.ChargeEntityFixture.aValidChargeEntity;
 
 public class RefundEntityFixture {
-    private ChargeEntity chargeEntity = defaultChargeEntity();
     private Long amount = 500L;
     private RefundStatus status = RefundStatus.CREATED;
+    private GatewayAccountEntity gatewayAccountEntity = ChargeEntityFixture.defaultGatewayAccountEntity();
 
     public static RefundEntityFixture aValidRefundEntity() {
         return new RefundEntityFixture();
     }
 
     public RefundEntity build() {
-        RefundEntity refundEntity = new RefundEntity(chargeEntity, amount);
+        RefundEntity refundEntity = new RefundEntity(buildChargeEntity(), amount);
         refundEntity.setStatus(status);
         return refundEntity;
     }
@@ -27,15 +27,16 @@ public class RefundEntityFixture {
         return this;
     }
 
-    public ChargeEntity getChargeEntity() {
-        return chargeEntity;
+    public RefundEntityFixture withGatewayAccountEntity(GatewayAccountEntity gatewayAccountEntity) {
+        this.gatewayAccountEntity = gatewayAccountEntity;
+        return this;
     }
 
     public Long getAmount() {
         return amount;
     }
 
-    public ChargeEntity defaultChargeEntity() {
-        return aValidChargeEntity().build();
+    private ChargeEntity buildChargeEntity() {
+        return aValidChargeEntity().withGatewayAccountEntity(gatewayAccountEntity).build();
     }
 }
