8b7d995df1ae9e8c6291196ababf534af394056b
rahul-vinayak <rahul_vinayak@hotmail.com>
2016-09-29 16:08:05 +0100

    PP-1224 Ignore unknown properties for refund JSON request
    - added to make connector tolerant to accept an additional fields
    coming from Public API, to be removed at a later point
    
    with @PauloPortugal

--- src/main/java/uk/gov/pay/connector/model/RefundRequest.java
+++ src/main/java/uk/gov/pay/connector/model/RefundRequest.java
@@ -1,7 +1,11 @@
 package uk.gov.pay.connector.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+// FIXME this Json ignore is for backward compatibilty for Jira PP-1224, to be removed at a later state
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RefundRequest {
 
     @JsonProperty("amount")
