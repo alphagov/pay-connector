2b06823830235b8e91a66a4b6c8d9fd4d4e1acdb
Ian Maddison <ian@ianjm.com>
2017-01-25 20:09:46 +0000

    Remove newrelic. This is an old version of newrelic, is not used anywhere, and should not be stored as JARs in our github anyway.

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-if [ "${ENABLE_NEWRELIC}" == "yes" ]; then
-  NEWRELIC_JVM_FLAG="-javaagent:/app/newrelic/newrelic.jar"
-fi
-
 java -jar *-allinone.jar waitOnDependencies *.yaml && \
-java -jar *-allinone.jar db migrate *.yaml && \
-java ${NEWRELIC_JVM_FLAG} -jar *-allinone.jar server *.yaml
+  java -jar *-allinone.jar db migrate *.yaml && \
+  java -jar *-allinone.jar server *.yaml
