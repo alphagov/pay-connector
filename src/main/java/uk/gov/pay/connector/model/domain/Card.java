d5d83c5cb1d8e27794e38b092d00c1a82c4da6aa
imranbohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2017-01-25 14:36:16 +0000

    PP-1582 Ignore unknown json properties from payment details
     * As part of the 3DS work, we have to pass in the accept header
       and user agent header. To pass this from the frontend, we have to
       first let connector accept unknown properties.
     * This is temporary. Once frontend is passing the values, this code will be
       changed to remove the ignore and accept the specific headers.
    
    with @alexbishop1

--- src/main/java/uk/gov/pay/connector/model/domain/Card.java
+++ src/main/java/uk/gov/pay/connector/model/domain/Card.java
@@ -1,7 +1,9 @@
 package uk.gov.pay.connector.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Card {
 
     private String cardNo;
