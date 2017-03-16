c7bd9070ecc06092ccc0481acd71a078106f047f
David Heath <heathd@users.noreply.github.com>
2017-03-16 14:50:18 +0000

    PP-1776 Refactor ClientFactoryTest to not spin up postgres
    DropwizardAppWithPostgresRule spins up postgres in a docker container and runs
    migrations. We don't need any of that for these tests.
    
    Prior to this commit there was an elaborate use of paramterised tests in order
    to pass the `enableProxy` flag through to the Dropwizard app rule's constructor.
    This was based on the assumption that the only way to use the DropwizardAppRule
    was to hook it into jUnit's lifecycle by using a `@Rule` annotation. However,
    it turns out that it's quite straightforward to also use DropwizardTestSupport
    explicitly by calling `.before()` and doing that we can simplify the injection
    of the `enableProxy` flag.
    
    Prior to this commit we were spinning up and tearing down postgres twice for
    the two tests. With this change the tests should be about 10 seconds faster.

--- src/test/java/uk/gov/pay/connector/it/gatewayclient/ClientFactoryTest.java
+++ src/test/java/uk/gov/pay/connector/it/gatewayclient/ClientFactoryTest.java
@@ -1,5 +1,10 @@
 package uk.gov.pay.connector.it.gatewayclient;
 
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.DropwizardTestSupport;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import org.glassfish.jersey.client.ClientProperties;
 import org.junit.After;
 import org.junit.Before;
@@ -7,8 +12,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.verification.VerificationMode;
 import org.mockserver.integration.ClientAndProxy;
 import org.mockserver.integration.ClientAndServer;
+import uk.gov.pay.connector.app.ConnectorApp;
+import uk.gov.pay.connector.app.ConnectorConfiguration;
 import uk.gov.pay.connector.rules.DropwizardAppWithPostgresRule;
 import uk.gov.pay.connector.service.ClientFactory;
 import uk.gov.pay.connector.service.GatewayOperation;
@@ -20,6 +28,7 @@ import java.util.Collection;
 import static io.dropwizard.testing.ConfigOverride.config;
 import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.never;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
@@ -27,54 +36,38 @@ import static org.mockserver.socket.PortFactory.findFreePort;
 import static org.mockserver.verify.VerificationTimes.exactly;
 import static org.mockserver.verify.VerificationTimes.once;
 
-@RunWith(Parameterized.class)
 public class ClientFactoryTest {
 
-    @Rule
-    public DropwizardAppWithPostgresRule app;
-
+    DropwizardTestSupport<ConnectorConfiguration> app;
     private ClientAndProxy proxy;
     private ClientAndServer mockServer;
     private int serverPort = findFreePort();
     private int proxyPort = findFreePort();
     private String serverUrl = format("http://localhost:%s", serverPort);
-    private Boolean proxyEnabled;
-
-    @Parameterized.Parameters
-    public static Collection<Boolean> data() {
-        return Arrays.asList(Boolean.TRUE, Boolean.FALSE);
-    }
-
-    public ClientFactoryTest(Boolean proxyEnabled) {
-        this.proxyEnabled = proxyEnabled;
-        startAppWithProxy(proxyEnabled);
-    }
 
     @Before
-    public void before() throws Exception {
-        mockServer = startClientAndServer(serverPort);
+    public void setup() {
         proxy = new ClientAndProxy(proxyPort, "localhost", serverPort);
+        mockServer = startClientAndServer(serverPort);
     }
 
     @After
     public void after() throws Exception {
         proxy.stop();
         mockServer.stop();
+        app.after();
     }
 
-    /**
-     * DropwizardAppWithPostgresRule must be constructed during Test construction,
-     * hence this Parameterised Test and the conditional assertion below.
-     *
-     */
     @Test
-    public void shouldCreateAJerseyClientWithProxyEnabled() throws Exception {
+    public void shouldProxyRequestToTargetServer_whenProxyEnabled() throws Exception {
+        app = startAppWithProxy(true);
+
         mockServer
                 .when(request().withMethod("GET").withPath("/hello"))
                 .respond(response("world").withStatusCode(200));
 
 
-        Client client = new ClientFactory(app.getEnvironment(), app.getConf())
+        Client client = new ClientFactory(app.getEnvironment(), app.getConfiguration())
                 .createWithDropwizardClient("SANDBOX", GatewayOperation.AUTHORISE);
 
         client.target(serverUrl).path("hello").request().get();
@@ -82,18 +75,34 @@ public class ClientFactoryTest {
         mockServer.verify(request().withPath("/hello"), once());
         assertEquals(90000, client.getConfiguration().getProperty(ClientProperties.READ_TIMEOUT));
 
-        if (proxyEnabled) {
-            proxy.verify(request().withPath("/hello"), once());
-        } else {
-            proxy.verify(request().withPath("/hello"), exactly(0));
-        }
-
+        proxy.verify(request().withPath("/hello"), once());
     }
 
-    private void startAppWithProxy(boolean proxyEnabled) {
-        app = new DropwizardAppWithPostgresRule(
-                config("customJerseyClient.enableProxy", String.valueOf(proxyEnabled)),
-                config("jerseyClient.proxy.port", String.valueOf(proxyPort)));
+    @Test
+    public void shouldNotProxyRequestToTargetServer_whenProxyDisabled() throws Exception {
+        app = startAppWithProxy(false);
+
+        mockServer
+                .when(request().withMethod("GET").withPath("/hello"))
+                .respond(response("world").withStatusCode(200));
+
+        Client client = new ClientFactory(app.getEnvironment(), app.getConfiguration())
+                .createWithDropwizardClient("SANDBOX", GatewayOperation.AUTHORISE);
+
+        client.target(serverUrl).path("hello").request().get();
+
+        mockServer.verify(request().withPath("/hello"), once());
+        assertEquals(90000, client.getConfiguration().getProperty(ClientProperties.READ_TIMEOUT));
+
+        proxy.verify(request().withPath("/hello"), exactly(0));
     }
 
+    private DropwizardTestSupport<ConnectorConfiguration> startAppWithProxy(boolean proxyEnabled) {
+        DropwizardTestSupport<ConnectorConfiguration> app = new DropwizardTestSupport<>(ConnectorApp.class,
+                ResourceHelpers.resourceFilePath("config/test-it-config.yaml"),
+                ConfigOverride.config("customJerseyClient.enableProxy", String.valueOf(proxyEnabled)),
+                ConfigOverride.config("jerseyClient.proxy.port", String.valueOf(proxyPort)));
+        app.before();
+        return app;
+    }
 }
