adf2c058397f572b3ae808ddb8a45bc21a5dfdd2
Christophe Kalista <christophe.kalista@gmail.com>
2017-04-24 15:44:35 +0100

    PP-1494 Argument check ensuring the passphrase is not blank
    Added argument check ensuring the passphrase is not blank for EPDQ message
    payment signature.

--- src/main/java/uk/gov/pay/connector/service/epdq/EpdqSha512SignatureGenerator.java
+++ src/main/java/uk/gov/pay/connector/service/epdq/EpdqSha512SignatureGenerator.java
@@ -1,6 +1,7 @@
 package uk.gov.pay.connector.service.epdq;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
 
 import java.util.List;
@@ -10,6 +11,10 @@ public class EpdqSha512SignatureGenerator implements SignatureGenerator {
 
     @Override
     public String sign(List<NameValuePair> params, String passphrase) {
+        if (StringUtils.isBlank(passphrase)) {
+            throw new IllegalArgumentException("Passphrase must not be blank.");
+        }
+
         StringJoiner input = new StringJoiner(passphrase, "", passphrase);
         params.forEach(param -> input.add(param.getName() + "=" + param.getValue()));
         return DigestUtils.sha512Hex(input.toString());
