5767e8d9834bacabc1168f467685a9cc382cfbad
David Heath <heathd@users.noreply.github.com>
2017-05-05 17:30:15 +0100

    Allow JAVA_OPTS to be specified

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 java -jar *-allinone.jar waitOnDependencies *.yaml && \
   java -jar *-allinone.jar db migrate *.yaml && \
-  java -jar *-allinone.jar server *.yaml
+  java $JAVA_OPTS -jar *-allinone.jar server *.yaml
