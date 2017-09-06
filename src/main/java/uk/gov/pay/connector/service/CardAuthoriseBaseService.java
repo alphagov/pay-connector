9bc3eb3ec2325c0f9e8fc63b33f13368dde57712
Mark Bridgett <mark.bridgett@digital.cabinet-office.gov.uk>
2017-09-06 11:22:03 +0100

    PP-2495 AWS x-ray on application stack (Java)

--- src/main/java/uk/gov/pay/connector/service/CardAuthoriseBaseService.java
+++ src/main/java/uk/gov/pay/connector/service/CardAuthoriseBaseService.java
@@ -40,6 +40,30 @@ public abstract class CardAuthoriseBaseService<T extends AuthorisationDetails> e
 
 
     public GatewayResponse doAuthorise(String chargeId, T gatewayAuthRequest) {
+        return chargeDao.findByExternalId(chargeId).map(chargeEntity -> {
+            Supplier<GatewayResponse> authorisationSupplier = () -> {
+                recorder.beginSegment("pay-connector");
+                //segment.putService("component","authorisation-request");
+                ChargeEntity preOperationResponse;
+                try {
+
+                    try {
+                        preOperationResponse = preOperation(chargeEntity, gatewayAuthRequest);
+                        if (preOperationResponse.hasStatus(ChargeStatus.AUTHORISATION_ABORTED)) {
+                            throw new ConflictRuntimeException(chargeEntity.getExternalId(), "configuration mismatch");
+                        }
+                    } catch (OptimisticLockException e) {
+                        throw new ConflictRuntimeException(chargeEntity.getExternalId());
+                    }
+
+                    GatewayResponse<BaseAuthoriseResponse> operationResponse = operation(preOperationResponse, gatewayAuthRequest);
+
+                    return postOperation(preOperationResponse, gatewayAuthRequest, operationResponse);
+
+                } finally {
+                    recorder.endSegment();
+                }
+            };
 
         Supplier authorisationSupplier = () -> {
             ChargeEntity charge;
