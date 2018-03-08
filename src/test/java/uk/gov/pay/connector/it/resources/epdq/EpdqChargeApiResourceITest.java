e221633fa928a097f11d956b8269bf553fbcaff2
chrisgrimble <chris.grimble@digital.cabinet-office.gov.uk>
2018-03-08 15:50:26 +0000

    PP-3450 EPDQ 3Ds add html to getCharge for frontend
    When we make a 3Ds authorisation request to EPDQ we get back a block of
    html. Frontend calls the getCharge for frontend endpoint and this now
    needs to include the html.
    
    - Add html the response from connector.
    - Update tests.
    - Do not add paRequest, issureUrl or htmlOut to response if they have
    not been set.
    
    with @kakumara

---
+++ src/test/java/uk/gov/pay/connector/it/resources/epdq/EpdqChargeApiResourceITest.java
@@ -0,0 +1,26 @@
+package uk.gov.pay.connector.it.resources.epdq;
+
+import org.junit.Test;
+import uk.gov.pay.connector.it.base.ChargingITestBase;
+
+import static org.hamcrest.core.Is.is;
+import static uk.gov.pay.connector.model.domain.ChargeStatus.AUTHORISATION_3DS_REQUIRED;
+
+public class EpdqChargeApiResourceITest extends ChargingITestBase {
+    public EpdqChargeApiResourceITest() {
+        super("epdq");
+    }
+
+    @Test
+    public void shouldSuccessfully_authorise3ds() throws Exception {
+        String externalChargeId = createNewChargeWithNoTransactionId(AUTHORISATION_3DS_REQUIRED);
+        String chargeId = externalChargeId.replace("charge-", "");
+        String expectedHtml = "someHtml";
+        app.getDatabaseTestHelper().updateCharge3dsDetails(Long.valueOf(chargeId), null, null, expectedHtml);
+
+        connectorRestApi.withChargeId(externalChargeId).getFrontendCharge()
+                .statusCode(200)
+                .body("status", is(AUTHORISATION_3DS_REQUIRED.toString()))
+                .body("auth_3ds_data.htmlOut", is(expectedHtml));
+    }
+}
