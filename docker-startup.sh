35866dfe8c3c49fc0785ef39e6d49bd11cc735dc
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-03-27 14:47:06 +0100

    PP3322 - Connector has standard startup script
    Right now the startup script cannot run database migrations, it should
    have this functionality inside
    
    Once this is merged and pay-scripts has been updated I will remove
    docker startup with db migrations
    
    solo @tlwr

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
-java -jar *-allinone.jar waitOnDependencies *.yaml && \
+
+set -eu
+RUN_MIGRATION=${RUN_MIGRATION:-false}
+RUN_APP=${RUN_APP:-true}
+
+java -jar *-allinone.jar waitOnDependencies *.yaml
+
+if [ "$RUN_MIGRATION" == "true" ]; then
+  java -jar *-allinone.jar db migrate *.yaml
+fi
+
+if [ "$RUN_APP" == "true" ]; then
   java $JAVA_OPTS -jar *-allinone.jar server *.yaml
+fi
+
+exit 0
