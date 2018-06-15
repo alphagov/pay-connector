345e4906d8698ddc6376a8f34b2df23e764ac314
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2018-06-15 11:55:55 +0100

    Use common pay pact modules

--- src/test/java/uk/gov/pay/connector/pact/TransactionsApiContractTest.java
+++ src/test/java/uk/gov/pay/connector/pact/TransactionsApiContractTest.java
@@ -10,6 +10,7 @@ import au.com.dius.pact.provider.junit.target.TestTarget;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
+import uk.gov.pay.commons.testing.pact.providers.PayPactRunner;
 import uk.gov.pay.connector.model.domain.ChargeStatus;
 import uk.gov.pay.connector.rules.DropwizardAppWithPostgresRule;
 import uk.gov.pay.connector.util.DatabaseTestHelper;
@@ -48,7 +49,7 @@ public class TransactionsApiContractTest {
 
     private void setUpCharges(int numberOfCharges, String accountID, ZonedDateTime createdDate){
         for (int i = 0; i < numberOfCharges; i++) {
-            long chargeId = chargeIdCounter++;
+            Long chargeId = Integer.toUnsignedLong(i);
             dbHelper.addCharge(chargeId, Long.toString(chargeId), accountID, 100L, ChargeStatus.CREATED,"aReturnUrl","aTransactionId", "aReference", createdDate, "test@test.com@");
             dbHelper.updateChargeCardDetails(chargeId, "visa", "0001", "aName", "08/23", "aFirstAddress", "aSecondLine", "aPostCode", "aCity", "aCounty", "aCountry");
         }
