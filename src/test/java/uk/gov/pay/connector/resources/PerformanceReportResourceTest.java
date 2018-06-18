3c87e5c4d11d6950a259884f561f3c45ef8bb1f0
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-06-18 15:26:52 +0100

    PP-3870: Add performance report endpoints tests
    Adds tests for
    - performance report
    - gateway account segmented performance report
    
    This just tests serialisation/deserialisation of the entities, doesn't
    test JPA interaction.
    
    solo @tlwr

---
+++ src/test/java/uk/gov/pay/connector/resources/PerformanceReportResourceTest.java
@@ -0,0 +1,84 @@
+package uk.gov.pay.connector.resources;
+
+import java.math.BigDecimal;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.gov.pay.connector.dao.PerformanceReportDao;
+import uk.gov.pay.connector.model.domain.report.PerformanceReportEntity;
+
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PerformanceReportResourceTest {
+
+    private final Long totalVolume = 100000000000000L;
+    private final BigDecimal totalAmount = new BigDecimal("10000000000000000");
+    private final BigDecimal averageAmount = new BigDecimal("100");
+
+    @Mock
+    private PerformanceReportDao mockPerformanceReportDao;
+
+    @Mock
+    private PerformanceReportEntity noTransactionsPerformanceReportEntity,
+                                    someTransactionsPerformanceReportEntity;
+
+    private PerformanceReportResource resource;
+
+    @Before
+    public void setUp() {
+        noTransactionsPerformanceReportEntity = new PerformanceReportEntity(
+            0L,
+            BigDecimal.ZERO,
+            BigDecimal.ZERO
+        );
+        someTransactionsPerformanceReportEntity = new PerformanceReportEntity(
+            totalVolume,
+            totalAmount,
+            averageAmount
+        );
+
+        resource = new PerformanceReportResource(mockPerformanceReportDao);
+    }
+
+    @Test
+    public void emptyPerformanceReportSerialisesCorrectly() {
+        given(mockPerformanceReportDao.aggregateNumberAndValueOfPayments())
+                .willReturn(noTransactionsPerformanceReportEntity);
+
+        Response result = resource.getPerformanceReport();
+
+        assertThat(result.getStatus(), is(OK.getStatusCode()));
+
+        ImmutableMap<String, Object> unserializedResponse = (ImmutableMap<String, Object>) result.getEntity();
+
+        assertThat(unserializedResponse.get("total_volume"), is(0L));
+        assertThat(unserializedResponse.get("total_amount"), is(BigDecimal.ZERO));
+        assertThat(unserializedResponse.get("average_amount"), is(BigDecimal.ZERO));
+    }
+
+    @Test
+    public void nonEmptyPerformanceReportSerialisesCorrectly() {
+        given(mockPerformanceReportDao.aggregateNumberAndValueOfPayments())
+                .willReturn(someTransactionsPerformanceReportEntity);
+
+        Response result = resource.getPerformanceReport();
+
+        assertThat(result.getStatus(), is(OK.getStatusCode()));
+
+        ImmutableMap<String, Object> unserializedResponse = (ImmutableMap<String, Object>) result.getEntity();
+
+        assertThat(unserializedResponse.get("total_volume"), is(totalVolume));
+        assertThat(unserializedResponse.get("total_amount"), is(totalAmount));
+        assertThat(unserializedResponse.get("average_amount"), is(averageAmount));
+    }
+}
