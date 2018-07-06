bb53883c7af34695488a1e3cf2b6684c21ea7fb3
Krishna Bottla <krishna.bottla@digital.cabinet-office.gov.uk>
2018-07-06 16:00:02 +0100

    BAU : Adde missing Level import
    @kbottla

--- src/test/java/uk/gov/pay/connector/util/ApplicationStartupApplicationStartupDependentResourceCheckerTest.java
+++ src/test/java/uk/gov/pay/connector/util/ApplicationStartupApplicationStartupDependentResourceCheckerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.pay.connector.util;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
