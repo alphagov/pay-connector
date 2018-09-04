a4fbb0e013c522260755327ed1075b01c31d234e
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-09-04 11:27:42 +0100

    bau: Add java options to profile cpu
    @oswaldquek

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,7 +4,7 @@ set -eu
 RUN_MIGRATION=${RUN_MIGRATION:-false}
 RUN_APP=${RUN_APP:-true}
 
-java $JAVA_OPTS -jar *-allinone.jar waitOnDependencies *.yaml
+java -agentlib:hprof=cpu=samples,interval=500,depth=10 -jar *-allinone.jar waitOnDependencies *.yaml
 
 if [ "$RUN_MIGRATION" == "true" ]; then
   java $JAVA_OPTS -jar *-allinone.jar db migrate *.yaml
