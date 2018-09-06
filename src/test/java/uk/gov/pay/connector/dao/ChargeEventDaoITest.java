fcf0fd4f330e3b9095527288d0830a102628d80b
george racu <george.racu@digital.cabinet-office.gov.uk>
2018-09-06 10:46:36 +0100

    PP-4108 Added test for checking the event is persisted
    - Checks that an event is persisted when updating status
    - Removed unused fields and unnecessary declaration for throwing exceptions
    - Extracted local variables into constants

--- src/test/java/uk/gov/pay/connector/dao/ChargeEventDaoITest.java
+++ src/test/java/uk/gov/pay/connector/dao/ChargeEventDaoITest.java
@@ -21,23 +21,30 @@ import static java.util.stream.Collectors.toList;
 import static org.exparity.hamcrest.date.ZonedDateTimeMatchers.within;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static uk.gov.pay.connector.model.domain.ChargeStatus.AUTHORISATION_READY;
 import static uk.gov.pay.connector.model.domain.ChargeStatus.AUTHORISATION_SUCCESS;
+import static uk.gov.pay.connector.model.domain.ChargeStatus.AWAITING_CAPTURE_REQUEST;
 import static uk.gov.pay.connector.model.domain.ChargeStatus.CAPTURE_READY;
 import static uk.gov.pay.connector.model.domain.ChargeStatus.ENTERING_CARD_DETAILS;
 
 public class ChargeEventDaoITest extends DaoITestBase {
 
+    private static final long CHARGE_ID = 56735L;
+    private static final String EXTERNAL_CHARGE_ID = "charge456";
+
+    private static final String TRANSACTION_ID = "345654";
+    private static final String TRANSACTION_ID_2 = "345655";
+    private static final String TRANSACTION_ID_3 = "345656";
+
     private ChargeDao chargeDao;
     private ChargeEventDao chargeEventDao;
-    private DatabaseFixtures.TestCardDetails defaultTestCardDetails;
     private DatabaseFixtures.TestAccount defaultTestAccount;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         chargeDao = env.getInstance(ChargeDao.class);
         chargeEventDao = env.getInstance(ChargeEventDao.class);
-        defaultTestCardDetails = new DatabaseFixtures(databaseTestHelper).validTestCardDetails();
         this.defaultTestAccount = DatabaseFixtures
                 .withDatabaseTestHelper(databaseTestHelper)
                 .aTestAccount()
@@ -45,25 +52,18 @@ public class ChargeEventDaoITest extends DaoITestBase {
     }
 
     @Test
-    public void persistChargeEventOfChargeEntity_succeeds() throws Exception {
-
-        Long chargeId = 56735L;
-        String externalChargeId = "charge456";
-
-        String transactionId = "345654";
-        String transactionId2 = "345655";
-        String transactionId3 = "345656";
+    public void persistChargeEventOfChargeEntity_succeeds() {
 
         DatabaseFixtures
                 .withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
                 .withTestAccount(defaultTestAccount)
-                .withChargeId(chargeId)
-                .withExternalChargeId(externalChargeId)
-                .withTransactionId(transactionId)
+                .withChargeId(CHARGE_ID)
+                .withExternalChargeId(EXTERNAL_CHARGE_ID)
+                .withTransactionId(TRANSACTION_ID)
                 .insert();
 
-        Optional<ChargeEntity> charge = chargeDao.findById(chargeId);
+        Optional<ChargeEntity> charge = chargeDao.findById(CHARGE_ID);
         ChargeEntity entity = charge.get();
         entity.setStatus(ENTERING_CARD_DETAILS);
 
@@ -72,21 +72,64 @@ public class ChargeEventDaoITest extends DaoITestBase {
         //move status to AUTHORISED
         entity.setStatus(AUTHORISATION_READY);
         entity.setStatus(AUTHORISATION_SUCCESS);
-        entity.setGatewayTransactionId(transactionId2);
+        entity.setGatewayTransactionId(TRANSACTION_ID_2);
 
         chargeEventDao.persistChargeEventOf(entity, Optional.empty());
 
         entity.setStatus(CAPTURE_READY);
-        entity.setGatewayTransactionId(transactionId3);
+        entity.setGatewayTransactionId(TRANSACTION_ID_3);
 
         chargeEventDao.persistChargeEventOf(entity, Optional.empty());
 
-        List<ChargeEventEntity> events = chargeDao.findById(chargeId).get().getEvents();
+        List<ChargeEventEntity> events = chargeDao.findById(CHARGE_ID).get().getEvents();
 
         assertThat(events, hasSize(3));
         assertThat(events, shouldIncludeStatus(ENTERING_CARD_DETAILS));
         assertThat(events, shouldIncludeStatus(AUTHORISATION_SUCCESS));
         assertThat(events, shouldIncludeStatus(CAPTURE_READY));
+        assertDateMatch(events.get(0).getUpdated());
+    }
+
+    @Test
+    public void shouldPersistEventForStatus_awaitingCaptureRequest() {
+
+        DatabaseFixtures
+                .withDatabaseTestHelper(databaseTestHelper)
+                .aTestCharge()
+                .withTestAccount(defaultTestAccount)
+                .withChargeId(CHARGE_ID)
+                .withExternalChargeId(EXTERNAL_CHARGE_ID)
+                .withTransactionId(TRANSACTION_ID)
+                .withDelayedCapture(true)
+                .insert();
+
+        Optional<ChargeEntity> charge = chargeDao.findById(CHARGE_ID);
+        assertThat(charge.isPresent(), is(true));
+        ChargeEntity entity = charge.get();
+        entity.setStatus(ENTERING_CARD_DETAILS);
+
+        chargeEventDao.persistChargeEventOf(entity, Optional.empty());
+
+        //move status to AUTHORISED
+        entity.setStatus(AUTHORISATION_READY);
+        entity.setStatus(AUTHORISATION_SUCCESS);
+        entity.setGatewayTransactionId(TRANSACTION_ID_2);
+
+        chargeEventDao.persistChargeEventOf(entity, Optional.empty());
+
+        entity.setStatus(AWAITING_CAPTURE_REQUEST);
+        entity.setGatewayTransactionId(TRANSACTION_ID_3);
+
+        chargeEventDao.persistChargeEventOf(entity, Optional.empty());
+
+        final Optional<ChargeEntity> maybeChargeEntity = chargeDao.findById(CHARGE_ID);
+        assertThat(maybeChargeEntity.isPresent(), is(true));
+        List<ChargeEventEntity> events = maybeChargeEntity.get().getEvents();
+
+        assertThat(events, hasSize(3));
+        assertThat(events, shouldIncludeStatus(ENTERING_CARD_DETAILS));
+        assertThat(events, shouldIncludeStatus(AUTHORISATION_SUCCESS));
+        assertThat(events, shouldIncludeStatus(AWAITING_CAPTURE_REQUEST));
         assertDateMatch(events.get(0).getUpdated());
     }
 
