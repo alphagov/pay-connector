df1f7ad6760d73f4f3b48af4de9f9803f9eb6e52
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-11-23 15:09:13 +0000

    PP-4399: Deal with exceptions
    @oswaldquek @georgeracu

--- src/main/java/uk/gov/pay/connector/gateway/stripe/handler/StripeCancelHandler.java
+++ src/main/java/uk/gov/pay/connector/gateway/stripe/handler/StripeCancelHandler.java
@@ -6,6 +6,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.pay.connector.app.StripeGatewayConfig;
+import uk.gov.pay.connector.gateway.model.GatewayError;
 import uk.gov.pay.connector.gateway.model.request.CancelGatewayRequest;
 import uk.gov.pay.connector.gateway.model.response.BaseCancelResponse;
 import uk.gov.pay.connector.gateway.model.response.BaseResponse;
@@ -14,15 +15,20 @@ import uk.gov.pay.connector.gateway.stripe.DownstreamException;
 import uk.gov.pay.connector.gateway.stripe.GatewayClientException;
 import uk.gov.pay.connector.gateway.stripe.GatewayException;
 import uk.gov.pay.connector.gateway.stripe.StripeGatewayClient;
+import uk.gov.pay.connector.gateway.stripe.json.StripeErrorResponse;
 import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
 
+import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.UUID;
 
 import static java.lang.String.format;
 import static java.nio.charset.Charset.defaultCharset;
 import static java.util.Collections.singletonList;
 import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED_TYPE;
+import static uk.gov.pay.connector.gateway.model.GatewayError.genericGatewayError;
+import static uk.gov.pay.connector.gateway.model.GatewayError.unexpectedStatusCodeFromGateway;
 import static uk.gov.pay.connector.gateway.stripe.util.StripeAuthUtil.getAuthHeaderValue;
 
 public class StripeCancelHandler {
@@ -64,13 +70,26 @@ public class StripeCancelHandler {
             }).build();
             
         } catch (GatewayClientException e) {
-            e.printStackTrace();
-        } catch (GatewayException e) {
-            e.printStackTrace();
-        } catch (DownstreamException e) {
-            e.printStackTrace();
-        }
+            
+            Response response = e.getResponse();
+            StripeErrorResponse stripeErrorResponse = response.readEntity(StripeErrorResponse.class);
+            String errorId = UUID.randomUUID().toString();
+            logger.error("Cancel failed for gateway transaction id {}. Failure code from Stripe: {}, failure message from Stripe: {}. ErrorId: {}. Response code from Stripe: {}",
+                    request.getTransactionId(), stripeErrorResponse.getError().getCode(), stripeErrorResponse.getError().getMessage(), errorId, response.getStatus());
+            GatewayError gatewayError = genericGatewayError(stripeErrorResponse.getError().getMessage());
+            return responseBuilder.withGatewayError(gatewayError).build();
 
-        return null;
+        } catch (GatewayException e) {
+            
+            return responseBuilder.withGatewayError(GatewayError.of(e)).build();
+            
+        } catch (DownstreamException e) {
+            
+            String errorId = UUID.randomUUID().toString();
+            logger.error("Cancel failed for transaction id {}. Reason: {}. Status code from Stripe: {}. ErrorId: {}",
+                    request.getTransactionId(), e.getMessage(), e.getStatusCode(), errorId);
+            GatewayError gatewayError = unexpectedStatusCodeFromGateway("An internal server error occurred. ErrorId: " + errorId);
+            return responseBuilder.withGatewayError(gatewayError).build();
+        }
     }
 }
