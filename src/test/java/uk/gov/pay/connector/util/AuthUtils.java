fced131ac81044d31247462d0cb1060d03c8b6c9
Silvia Mandalà <sil.mandala@gmail.com>
2018-11-23 11:21:30 +0000

    PP-4314 Use fixtures rather than static methods to build apple pay test data

--- src/test/java/uk/gov/pay/connector/util/AuthUtils.java
+++ src/test/java/uk/gov/pay/connector/util/AuthUtils.java
@@ -1,11 +1,6 @@
 package uk.gov.pay.connector.util;
 
-import uk.gov.pay.connector.applepay.AppleDecryptedPaymentData;
-import uk.gov.pay.connector.applepay.api.ApplePaymentInfo;
 import uk.gov.pay.connector.gateway.model.Auth3dsDetails;
-import uk.gov.pay.connector.gateway.model.PayersCardType;
-
-import java.time.LocalDate;
 
 public class AuthUtils {
     public static Auth3dsDetails buildAuth3dsDetails() {
@@ -13,40 +8,4 @@ public class AuthUtils {
         auth3dsDetails.setPaResponse("sample-pa-response");
         return auth3dsDetails;
     }
-
-
-    public static class ApplePay {
-        private static AppleDecryptedPaymentData buildDecryptedPaymentData(String cardHolderName, String email, String lastFourDigitsCardNumber, AppleDecryptedPaymentData.PaymentData paymentData) {
-            return new AppleDecryptedPaymentData(
-                    new ApplePaymentInfo(
-                            lastFourDigitsCardNumber,
-                            "visa",
-                            PayersCardType.DEBIT,
-                            cardHolderName,
-                            email
-                    ),
-                    "4818528840010767",
-                    LocalDate.of(2023, 12, 31),
-                    "643",
-                    10L,
-                    "040010030273",
-                    "3DSecure",
-                    paymentData
-            );
-        }
-
-        public static AppleDecryptedPaymentData buildDecryptedPaymentData(String cardHolderName, String email, String lastFourDigitsCardNumber) {
-            return buildDecryptedPaymentData(cardHolderName, email, lastFourDigitsCardNumber, new AppleDecryptedPaymentData.PaymentData(
-                    "Ao/fzpIAFvp1eB9y8WVDMAACAAA=",
-                    "7"
-            ));
-        }
-        public static AppleDecryptedPaymentData buildDecryptedMinimalPaymentData(String lastFourDigitsCardNumber) {
-            return buildDecryptedPaymentData(null, null, lastFourDigitsCardNumber, new AppleDecryptedPaymentData.PaymentData(
-                    "Ao/fzpIAFvp1eB9y8WVDMAACAAA=",
-                    null
-            ));
-        }
-    }
-
 }
