dbd5138a7894a62cc018ee1e1a162e84e21ac3db
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-04 14:33:58 +0000

    PP-4584: Remove proxy environment variables
    Let's just delegate the setting of system properties to the JAVA_OPTS
    environment variable.

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,10 +4,6 @@ set -eu
 RUN_MIGRATION=${RUN_MIGRATION:-false}
 RUN_APP=${RUN_APP:-true}
 
-[ -z "${http_proxy:-}" ] || JAVA_OPTS="${JAVA_OPTS:-} -Dhttp.proxyHost=${http_proxy}"
-[ -z "${https_proxy:-}" ] || JAVA_OPTS="${JAVA_OPTS:-} -Dhttps.proxyHost=${https_proxy}"
-[ -z "${java_http_non_proxy_hosts:-}" ] || JAVA_OPTS="${JAVA_OPTS:-} -Dhttp.nonProxyHosts=${java_http_non_proxy_hosts}"
-
 java $JAVA_OPTS -jar *-allinone.jar waitOnDependencies *.yaml
 
 if [ "$RUN_MIGRATION" == "true" ]; then
