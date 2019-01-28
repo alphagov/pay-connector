724f6d13f80c36bc81edcc4594233d60dd8ccffb
danworth <dan.worth@digital.cabinet-office.gov.uk>
2019-01-28 08:53:29 +0000

    PP-4638 Add Googlepay to Worldpay Auth Flow
    - Create Worldpay authorisation template for Googlepay.
    - Add `Googlepay` to `WalletTypes`.
    - Add tests for correct creation of template.

--- src/main/java/uk/gov/pay/connector/wallets/WalletType.java
+++ src/main/java/uk/gov/pay/connector/wallets/WalletType.java
@@ -4,11 +4,14 @@ import uk.gov.pay.connector.gateway.model.OrderRequestType;
 import uk.gov.pay.connector.gateway.templates.TemplateBuilder;
 
 import static uk.gov.pay.connector.gateway.model.OrderRequestType.AUTHORISE_APPLE_PAY;
+import static uk.gov.pay.connector.gateway.model.OrderRequestType.AUTHORISE_GOOGLE_PAY;
 import static uk.gov.pay.connector.gateway.worldpay.WorldpayOrderRequestBuilder.AUTHORISE_APPLE_PAY_ORDER_TEMPLATE_BUILDER;
+import static uk.gov.pay.connector.gateway.worldpay.WorldpayOrderRequestBuilder.AUTHORISE_GOOGLE_PAY_ORDER_TEMPLATE_BUILDER;
 
 public enum WalletType {
     
-    APPLE_PAY(AUTHORISE_APPLE_PAY_ORDER_TEMPLATE_BUILDER, AUTHORISE_APPLE_PAY);
+    APPLE_PAY(AUTHORISE_APPLE_PAY_ORDER_TEMPLATE_BUILDER, AUTHORISE_APPLE_PAY),
+    GOOGLE_PAY(AUTHORISE_GOOGLE_PAY_ORDER_TEMPLATE_BUILDER, AUTHORISE_GOOGLE_PAY);
     
     private final TemplateBuilder worldPayTemplate;
     private final OrderRequestType orderRequestType;
--- src/main/java/uk/gov/pay/connector/wallets/googlepay/api/GooglePayAuthRequest.java
+++ src/main/java/uk/gov/pay/connector/wallets/googlepay/api/GooglePayAuthRequest.java
@@ -1,12 +1,14 @@
 package uk.gov.pay.connector.wallets.googlepay.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import uk.gov.pay.connector.wallets.WalletType;
 import uk.gov.pay.connector.wallets.model.WalletPaymentInfo;
+import uk.gov.pay.connector.wallets.model.WalletTemplateData;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-public class GooglePayAuthRequest {
+public class GooglePayAuthRequest implements WalletTemplateData {
 
     @NotNull @Valid private final WalletPaymentInfo paymentInfo;
     @NotNull @Valid private final EncryptedPaymentData encryptedPaymentData;
@@ -23,5 +25,15 @@ public class GooglePayAuthRequest {
 
     public EncryptedPaymentData getEncryptedPaymentData() {
         return encryptedPaymentData;
+    }
+
+    @Override
+    public String getLastDigitsCardNumber() {
+        return getPaymentInfo().getLastDigitsCardNumber();
+    }
+
+    @Override
+    public WalletType getWalletType() {
+        return WalletType.GOOGLE_PAY;
     }
 }
