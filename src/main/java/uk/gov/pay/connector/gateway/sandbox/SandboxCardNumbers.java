c9e22919a381899788048191df55f0e53eae1d9d
danworth <dan.worth@digital.cabinet-office.gov.uk>
2019-01-30 10:12:34 +0000

    PP-4638 Tidy up SandboxCardNumbers
    - Replace `APPLE` with `WALLET` for re-use across all wallet types.
    - Use `Set` rather than `List` interface for efficiency.
    - Remove `.equals()` and add cards to appropriate set to be included in existing
    `.contains()` check.
    - Reorder for better clarity.

--- src/main/java/uk/gov/pay/connector/gateway/sandbox/SandboxCardNumbers.java
+++ src/main/java/uk/gov/pay/connector/gateway/sandbox/SandboxCardNumbers.java
@@ -1,9 +1,9 @@
 package uk.gov.pay.connector.gateway.sandbox;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -12,15 +12,64 @@ import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.AUTHORISATIO
 
 public class SandboxCardNumbers {
 
+    private static final String GOOD_CARD_PREPAID_NON_CORPORATE = "4000160000000004";
+    private static final String GOOD_WALLET_LAST_DIGITS_CARD_NUMBER = "4242";
+    private static final String GOOD_CORPORATE_PREPAID_UNKNOWN_CREDIT_CARD = "5101180000000007";
+    private static final String GOOD_CORPORATE_PREPAID_UNKNOWN_DEBIT_CARD = "5200828282828210";
+    private static final String GOOD_NON_CORPORATE_NON_PREPAID = "4000020000000000";
+    private static final String DECLINED_WALLET_LAST_DIGITS_CARD_NUMBER = "0002";
+    private static final String DECLINED_CARD_NUMBER = "4000000000000002";
+    private static final String CVC_ERROR_WALLET_LAST_DIGITS_CARD_NUMBER = "0127";
+    private static final String CVC_ERROR_CARD_NUMBER = "4000000000000127";
+    private static final String EXPIRED_WALLET_LAST_DIGITS_CARD_NUMBER = "0069";
+    private static final String EXPIRED_CARD_NUMBER = "4000000000000069";
+    private static final String PROCESSING_ERROR_WALLET_LAST_DIGITS_CARD_NUMBER = "0119";
+    private static final String PROCESSING_ERROR_CARD_NUMBER = "4000000000000119";
+
+    private static final Set<String> GOOD_CARDS = ImmutableSet.of(
+            "4444333322221111",
+            "4242424242424242",
+            "4917610000000000003",
+            "4000056655665556",
+            "5105105105105100",
+            "371449635398431",
+            "3566002020360505",
+            "6011000990139424",
+            "36148900647913",
+            GOOD_WALLET_LAST_DIGITS_CARD_NUMBER,
+            GOOD_CARD_PREPAID_NON_CORPORATE,
+            GOOD_NON_CORPORATE_NON_PREPAID);
+
+    private static final Set<String> GOOD_CORPORATE_CARDS = ImmutableSet.of(
+            "4988080000000000",
+            "4000620000000007",
+            "4293189100000008",
+            GOOD_CORPORATE_PREPAID_UNKNOWN_CREDIT_CARD);
+
+    private static final Set<String> GOOD_CORPORATE_PREPAID_DEBIT_CARD = ImmutableSet.of(
+            "4131840000000003",
+            "4000180000000002",
+            GOOD_CORPORATE_PREPAID_UNKNOWN_DEBIT_CARD);
+
+    private static final Set<String> REJECTED_CARDS = ImmutableSet.of(
+            DECLINED_CARD_NUMBER,
+            DECLINED_WALLET_LAST_DIGITS_CARD_NUMBER,
+            EXPIRED_CARD_NUMBER,
+            EXPIRED_WALLET_LAST_DIGITS_CARD_NUMBER,
+            CVC_ERROR_CARD_NUMBER,
+            CVC_ERROR_WALLET_LAST_DIGITS_CARD_NUMBER);
+
+    private static Map<String, CardError> ERROR_CARDS = Stream.of(
+            PROCESSING_ERROR_CARD_NUMBER,
+            PROCESSING_ERROR_WALLET_LAST_DIGITS_CARD_NUMBER)
+            .collect(toMap(
+                    Function.identity(),
+                    cardNumber -> new CardError(AUTHORISATION_ERROR, "This transaction could be not be processed.")));
+
     public static boolean isValidCard(String cardNumber) {
         return GOOD_CARDS.contains(cardNumber) ||
                 GOOD_CORPORATE_CARDS.contains(cardNumber) ||
-                GOOD_APPLE_PAY_LAST_DIGITS_CARD_NUMBER.equals(cardNumber) ||
-                GOOD_CORPORATE_PREPAID_DEBIT_CARD.contains(cardNumber) ||
-                GOOD_CORPORATE_PREPAID_UNKNOWN_CREDIT_CARD.equals(cardNumber) ||
-                GOOD_CARD_PREPAID_NON_CORPORATE.equals(cardNumber) ||
-                GOOD_CORPORATE_PREPAID_UNKNOWN_DEBIT_CARD.equals(cardNumber) ||
-                GOOD_NON_COPORATE_NON_PREPAID.equals(cardNumber);
+                GOOD_CORPORATE_PREPAID_DEBIT_CARD.contains(cardNumber);
     }
 
     public static boolean isRejectedCard(String cardNumber) {
@@ -34,45 +83,4 @@ public class SandboxCardNumbers {
     public static CardError cardErrorFor(String cardNumber) {
         return ERROR_CARDS.get(cardNumber);
     }
-
-    private static final List<String> GOOD_CARDS = ImmutableList.of(
-            "4444333322221111",
-            "4242424242424242",
-            "4917610000000000003",
-            "4000056655665556",
-            "5105105105105100",
-            "371449635398431",
-            "3566002020360505",
-            "6011000990139424",
-            "36148900647913");
-    private static final String GOOD_CARD_PREPAID_NON_CORPORATE = "4000160000000004";
-    private static final String GOOD_APPLE_PAY_LAST_DIGITS_CARD_NUMBER = "4242";
-    private static final List<String> GOOD_CORPORATE_CARDS = ImmutableList.of(
-            "4988080000000000",
-            "4000620000000007",
-            "4293189100000008");
-    private static final List<String> GOOD_CORPORATE_PREPAID_DEBIT_CARD = ImmutableList.of(
-            "4131840000000003",
-            "4000180000000002");
-    private static final String GOOD_CORPORATE_PREPAID_UNKNOWN_CREDIT_CARD = "5101180000000007";
-    private static final String GOOD_CORPORATE_PREPAID_UNKNOWN_DEBIT_CARD = "5200828282828210";
-    private static final String GOOD_NON_COPORATE_NON_PREPAID = "4000020000000000";
-    private static final String DECLINED_APPLE_PAY_LAST_DIGITS_CARD_NUMBER = "0002";
-    private static final String DECLINED_CARD_NUMBER = "4000000000000002";
-    private static final String CVC_ERROR_APPLE_PAY_LAST_DIGITS_CARD_NUMBER = "0127";
-    private static final String CVC_ERROR_CARD_NUMBER = "4000000000000127";
-    private static final String EXPIRED_APPLE_PAY_LAST_DIGITS_CARD_NUMBER = "0069";
-    private static final String EXPIRED_CARD_NUMBER = "4000000000000069";
-    private static final String PROCESSING_ERROR_APPLE_PAY_LAST_DIGITS_CARD_NUMBER = "0119";
-    private static final String PROCESSING_ERROR_CARD_NUMBER = "4000000000000119";
-
-    private static Map<String, CardError> ERROR_CARDS = Stream.of(
-            PROCESSING_ERROR_CARD_NUMBER,
-            PROCESSING_ERROR_APPLE_PAY_LAST_DIGITS_CARD_NUMBER)
-            .collect(toMap(
-                    Function.identity(),
-                    cardNumber -> new CardError(AUTHORISATION_ERROR, "This transaction could be not be processed.")));
-
-    private static final List<String>REJECTED_CARDS = ImmutableList.of(DECLINED_CARD_NUMBER, DECLINED_APPLE_PAY_LAST_DIGITS_CARD_NUMBER, EXPIRED_CARD_NUMBER, EXPIRED_APPLE_PAY_LAST_DIGITS_CARD_NUMBER, CVC_ERROR_CARD_NUMBER,
-            CVC_ERROR_APPLE_PAY_LAST_DIGITS_CARD_NUMBER);
 }
