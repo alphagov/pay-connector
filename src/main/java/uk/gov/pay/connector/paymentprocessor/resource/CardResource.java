a45801854e54e28dc9b1b2a81db2eba6a39ec0d5
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-03-18 15:50:01 +0000

    bau: Temporarily log apple payload
    Unfortunately since the keys were rotated on 15/3/2019 apple pay no longer
    works. I need a payload to test this locally.

--- src/main/java/uk/gov/pay/connector/paymentprocessor/resource/CardResource.java
+++ src/main/java/uk/gov/pay/connector/paymentprocessor/resource/CardResource.java
@@ -65,6 +65,7 @@ public class CardResource {
     public Response authoriseCharge(@PathParam("chargeId") String chargeId, 
                                     @NotNull @Valid ApplePayAuthRequest applePayAuthRequest) {
         logger.info("Received encrypted payload for charge with id {} ", chargeId);
+        logger.info("ApplePayAuthRequest.EncryptedPaymentData: {}", applePayAuthRequest.getEncryptedPaymentData().toString());
         return applePayService.authorise(chargeId, applePayAuthRequest);
     }
 
