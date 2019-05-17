82a081fb5e74cbfbd78255df4db7fc8b4f3d3549
David Heath <david.heath@digital.cabinet-office.gov.uk>
2019-05-17 17:17:16 +0100

    Hacking state transitions graph
    deleted all ready states
    
    created event objects representing each event type
    
    trying to catalogue all of the events that occur exhaustively so that we can
    describe the event schemas and think about what data is emitted

--- src/main/java/uk/gov/pay/connector/common/model/domain/PaymentGatewayStateTransitions.java
+++ src/main/java/uk/gov/pay/connector/common/model/domain/PaymentGatewayStateTransitions.java
@@ -58,82 +58,107 @@ public class PaymentGatewayStateTransitions {
         graph = buildGraph();
     }
 
-    private static ImmutableValueGraph<ChargeStatus, String> buildGraph() {
-        MutableValueGraph<ChargeStatus, String> graph = ValueGraphBuilder
+    private static class Event {}
+    private static class SalientEvent extends Event {}
+    private static class BoringEvent extends Event {}
+    
+    private static class ExpiredEvent extends SalientEvent {}
+    
+    private static class UserVistedPaymentPageEvent extends BoringEvent  {}
+    
+    private static class DependentServiceCancelledEvent extends SalientEvent  {}
+    
+    private static class AuthAbortDueTo3dsMisconfigurationEvent extends SalientEvent {}
+    private static class UserCancelledEvent extends SalientEvent {}
+    private static class AuthorisationSuccessEvent extends SalientEvent {}
+    private static class AuthorisationRejectedEvent extends SalientEvent {}
+    private static class GatewayErrorDuringAuthorisation extends SalientEvent {}
+    private static class GatewayTimeoutDuringAuthorisation extends SalientEvent {}
+    private static class UnexpectedGatewayErrorDuringAuthorisation extends SalientEvent {}
+    private static class GatewayRequiresThreeDSecure extends SalientEvent {}
+    private static class GatewayResponseCancelledEvent extends Event {}
+    private static class EpdqWaitingExternal extends Event {}
+    private static class AuthSuccess extends SalientEvent {}
+    private static class AuthRejected extends SalientEvent {}
+    private static class GatewayError extends SalientEvent {}
+    private static class UserApprovedPaymentCapture extends SalientEvent {}
+    private static class UserApprovedPaymentForCaptureAwaitingDependentServiceConfirmationEvent extends SalientEvent {}
+    private static class DependentServiceRequestedCapture extends Event { }
+    private static class CaptureErrorEvent extends Event { }
+    private static class CapturedEvent extends Event { }
+    private static class SubmittedForCaptureEvent extends Event { }
+    private static class CaptureFailedQueingForRetryEvent extends Event { }
+    private static class SubmittedForCancellationEvent extends Event { }
+    private static class CancelFailedEvent extends Event { }
+    private static class DependentServiceCancellationRequestSubmitted extends Event { }
+    private static class DependentServiceCancellationRequestError extends Event { }
+    private static class DependentServiceCancellationRequestSucceeded extends Event { }
+    private static class UserCancellationSubmitted extends Event { }
+    private static class UserCancellationError extends Event { }
+    
+    
+    private static ImmutableValueGraph<ChargeStatus, Event> buildGraph() {
+        MutableValueGraph<ChargeStatus, Event> graph = ValueGraphBuilder
                 .directed()
                 .build();
 
-        graph.putEdgeValue(CREATED, EXPIRED, "ChargeExpiryService");
-        graph.putEdgeValue(ENTERING_CARD_DETAILS, EXPIRED, "ChargeExpiryService");
-        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, EXPIRED, "ChargeExpiryService");
-        graph.putEdgeValue(EXPIRE_CANCEL_READY, EXPIRED, "ChargeExpiryService");
-        graph.putEdgeValue(EXPIRE_CANCEL_SUBMITTED, EXPIRED, "ChargeExpiryService");
-        graph.putEdgeValue(AUTHORISATION_3DS_READY, EXPIRED, "ChargeExpiryService");
+        graph.putEdgeValue(CREATED, EXPIRED, new ExpiredEvent());
+        graph.putEdgeValue(ENTERING_CARD_DETAILS, EXPIRED, new ExpiredEvent());
+        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, EXPIRED, new ExpiredEvent());
+        graph.putEdgeValue(EXPIRE_CANCEL_READY, EXPIRED, new ExpiredEvent());
+        graph.putEdgeValue(EXPIRE_CANCEL_SUBMITTED, EXPIRED, new ExpiredEvent());
+        graph.putEdgeValue(AUTHORISATION_3DS_READY, EXPIRED, new ExpiredEvent());
 
-        graph.putEdgeValue(CREATED, ENTERING_CARD_DETAILS, "frontend: POST /frontend/charges/{chargeId}/status");
-        graph.putEdgeValue(CREATED, SYSTEM_CANCELLED, "");
-        graph.putEdgeValue(ENTERING_CARD_DETAILS, AUTHORISATION_READY, "user submitted card details");
-        graph.putEdgeValue(ENTERING_CARD_DETAILS, AUTHORISATION_ABORTED, "");
-        graph.putEdgeValue(ENTERING_CARD_DETAILS, USER_CANCELLED, "user clicked cancel");
-        graph.putEdgeValue(ENTERING_CARD_DETAILS, SYSTEM_CANCELLED, "");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_ABORTED, "");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_SUCCESS, "Gateway response: AUTHORISED");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_REJECTED, "Gateway response: REJECTED");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_ERROR, "Gateway error");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_TIMEOUT, "Gateway timeout");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_UNEXPECTED_ERROR, "Gateway response: unexpected error");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_3DS_REQUIRED, "Gateway response: 3ds required");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_CANCELLED, "Gateway response: cancelled");
-        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_SUBMITTED, "Epdq only: WAITING_EXTERNAL(50)");
-        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_SUCCESS, "");
-        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_REJECTED, "");
-        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_ERROR, "");
-        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_3DS_REQUIRED, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, AUTHORISATION_3DS_READY, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, AUTHORISATION_REJECTED, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, AUTHORISATION_CANCELLED, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, EXPIRE_CANCEL_READY, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, USER_CANCELLED, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_SUCCESS, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_REJECTED, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_ERROR, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_READY, EXPIRE_CANCEL_READY, "");
-        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_CANCELLED, "");
-        graph.putEdgeValue(AUTHORISATION_SUCCESS, CAPTURE_APPROVED, "");
-        graph.putEdgeValue(AUTHORISATION_SUCCESS, CAPTURE_READY, "");
-        graph.putEdgeValue(AUTHORISATION_SUCCESS, SYSTEM_CANCEL_READY, "");
-        graph.putEdgeValue(AUTHORISATION_SUCCESS, USER_CANCEL_READY, "");
-        graph.putEdgeValue(AUTHORISATION_SUCCESS, EXPIRE_CANCEL_READY, "");
-        graph.putEdgeValue(AUTHORISATION_SUCCESS, AWAITING_CAPTURE_REQUEST, "");
+        graph.putEdgeValue(CREATED, ENTERING_CARD_DETAILS, new UserVistedPaymentPageEvent());
+        graph.putEdgeValue(CREATED, SYSTEM_CANCELLED, new DependentServiceCancelledEvent());
+        graph.putEdgeValue(ENTERING_CARD_DETAILS, AUTHORISATION_ABORTED, new AuthAbortDueTo3dsMisconfigurationEvent());
+        graph.putEdgeValue(ENTERING_CARD_DETAILS, USER_CANCELLED, new UserCancelledEvent());
+        graph.putEdgeValue(ENTERING_CARD_DETAILS, SYSTEM_CANCELLED, new DependentServiceCancelledEvent());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_ABORTED, new AuthAbortDueTo3dsMisconfigurationEvent());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_SUCCESS, new AuthorisationSuccessEvent());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_REJECTED, new AuthorisationRejectedEvent());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_ERROR, new GatewayErrorDuringAuthorisation());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_TIMEOUT, new GatewayTimeoutDuringAuthorisation());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_UNEXPECTED_ERROR, new UnexpectedGatewayErrorDuringAuthorisation());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_3DS_REQUIRED, new GatewayRequiresThreeDSecure());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_CANCELLED, new GatewayResponseCancelledEvent());
+        graph.putEdgeValue(AUTHORISATION_READY, AUTHORISATION_SUBMITTED, new EpdqWaitingExternal());
+        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_SUCCESS, new AuthSuccess());
+        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_REJECTED, new AuthRejected());
+        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_ERROR, new GatewayError());
+        graph.putEdgeValue(AUTHORISATION_SUBMITTED, AUTHORISATION_3DS_REQUIRED, new GatewayRequiresThreeDSecure());
+        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, AUTHORISATION_REJECTED, new AuthRejected());
+        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, AUTHORISATION_CANCELLED, new GatewayResponseCancelledEvent());
+        graph.putEdgeValue(AUTHORISATION_3DS_REQUIRED, USER_CANCELLED, new UserCancelledEvent());
+        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_SUCCESS, new AuthSuccess());
+        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_REJECTED, new AuthRejected());
+        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_ERROR, new GatewayError());
+        graph.putEdgeValue(AUTHORISATION_3DS_READY, AUTHORISATION_CANCELLED, new GatewayResponseCancelledEvent());
+        graph.putEdgeValue(AUTHORISATION_SUCCESS, CAPTURE_APPROVED, new UserApprovedPaymentCapture());
+        graph.putEdgeValue(AUTHORISATION_SUCCESS, AWAITING_CAPTURE_REQUEST, new UserApprovedPaymentForCaptureAwaitingDependentServiceConfirmationEvent());
 
-        graph.putEdgeValue(AWAITING_CAPTURE_REQUEST, CAPTURE_APPROVED, "");
-        graph.putEdgeValue(AWAITING_CAPTURE_REQUEST, SYSTEM_CANCEL_READY, "");
-        graph.putEdgeValue(AWAITING_CAPTURE_REQUEST, EXPIRE_CANCEL_READY, "");
+        graph.putEdgeValue(AWAITING_CAPTURE_REQUEST, CAPTURE_APPROVED, new DependentServiceRequestedCapture());
+        graph.putEdgeValue(CAPTURE_APPROVED, CAPTURE_ERROR, new CaptureErrorEvent());
+        graph.putEdgeValue(CAPTURE_APPROVED_RETRY, CAPTURE_ERROR, new CaptureErrorEvent()); 
+        graph.putEdgeValue(CAPTURE_APPROVED_RETRY, CAPTURED, new CapturedEvent());
+        graph.putEdgeValue(CAPTURE_READY, CAPTURE_SUBMITTED, new SubmittedForCaptureEvent());
+        graph.putEdgeValue(CAPTURE_READY, CAPTURE_APPROVED_RETRY, new CaptureFailedQueingForRetryEvent());
+        graph.putEdgeValue(CAPTURE_READY, CAPTURED, new CapturedEvent());
+        graph.putEdgeValue(CAPTURE_SUBMITTED, CAPTURED, new CapturedEvent());
+        graph.putEdgeValue(EXPIRE_CANCEL_READY, EXPIRE_CANCEL_SUBMITTED, new SubmittedForCancellationEvent());
+        graph.putEdgeValue(EXPIRE_CANCEL_READY, EXPIRE_CANCEL_FAILED, new CancelFailedEvent());
 
-        graph.putEdgeValue(CAPTURE_APPROVED, CAPTURE_READY, "");
-        graph.putEdgeValue(CAPTURE_APPROVED, CAPTURE_ERROR, "");
-        graph.putEdgeValue(CAPTURE_APPROVED_RETRY, CAPTURE_READY, "");
-        graph.putEdgeValue(CAPTURE_APPROVED_RETRY, CAPTURE_ERROR, "");
-        graph.putEdgeValue(CAPTURE_APPROVED_RETRY, CAPTURED, "");
-        graph.putEdgeValue(CAPTURE_READY, CAPTURE_SUBMITTED, "");
-        graph.putEdgeValue(CAPTURE_READY, CAPTURE_ERROR, "");
-        graph.putEdgeValue(CAPTURE_READY, CAPTURE_APPROVED_RETRY, "");
-        graph.putEdgeValue(CAPTURE_READY, CAPTURED, "received CAPTURED notification while about to retry");
-        graph.putEdgeValue(CAPTURE_SUBMITTED, CAPTURED, "");
-        graph.putEdgeValue(EXPIRE_CANCEL_READY, EXPIRE_CANCEL_SUBMITTED, "");
-        graph.putEdgeValue(EXPIRE_CANCEL_READY, EXPIRE_CANCEL_FAILED, "");
-
-        graph.putEdgeValue(EXPIRE_CANCEL_SUBMITTED, EXPIRE_CANCEL_FAILED, "");
-        graph.putEdgeValue(SYSTEM_CANCEL_READY, SYSTEM_CANCEL_SUBMITTED, "");
-        graph.putEdgeValue(SYSTEM_CANCEL_READY, SYSTEM_CANCEL_ERROR, "");
-        graph.putEdgeValue(SYSTEM_CANCEL_READY, SYSTEM_CANCELLED, "");
-        graph.putEdgeValue(SYSTEM_CANCEL_SUBMITTED, SYSTEM_CANCEL_ERROR, "");
-        graph.putEdgeValue(SYSTEM_CANCEL_SUBMITTED, SYSTEM_CANCELLED, "");
-        graph.putEdgeValue(USER_CANCEL_READY, USER_CANCEL_SUBMITTED, "");
-        graph.putEdgeValue(USER_CANCEL_READY, USER_CANCEL_ERROR, "");
-        graph.putEdgeValue(USER_CANCEL_READY, USER_CANCELLED, "");
-        graph.putEdgeValue(USER_CANCEL_SUBMITTED, USER_CANCEL_ERROR, "");
-        graph.putEdgeValue(USER_CANCEL_SUBMITTED, USER_CANCELLED, "");
+        graph.putEdgeValue(EXPIRE_CANCEL_SUBMITTED, EXPIRE_CANCEL_FAILED, new CancelFailedEvent());
+        graph.putEdgeValue(SYSTEM_CANCEL_READY, SYSTEM_CANCEL_SUBMITTED, new DependentServiceCancellationRequestSubmitted());
+        graph.putEdgeValue(SYSTEM_CANCEL_READY, SYSTEM_CANCEL_ERROR, new DependentServiceCancellationRequestError());
+        graph.putEdgeValue(SYSTEM_CANCEL_READY, SYSTEM_CANCELLED, new DependentServiceCancellationRequestSucceeded());
+        graph.putEdgeValue(SYSTEM_CANCEL_SUBMITTED, SYSTEM_CANCEL_ERROR, new DependentServiceCancellationRequestError());
+        graph.putEdgeValue(SYSTEM_CANCEL_SUBMITTED, SYSTEM_CANCELLED, new DependentServiceCancellationRequestSucceeded());
+        graph.putEdgeValue(USER_CANCEL_READY, USER_CANCEL_SUBMITTED, new UserCancellationSubmitted());
+        graph.putEdgeValue(USER_CANCEL_READY, USER_CANCEL_ERROR, new UserCancellationError());
+        graph.putEdgeValue(USER_CANCEL_READY, USER_CANCELLED, new UserCancelledEvent());
+        graph.putEdgeValue(USER_CANCEL_SUBMITTED, USER_CANCEL_ERROR, new UserCancellationError());
+        graph.putEdgeValue(USER_CANCEL_SUBMITTED, USER_CANCELLED, new UserCancelledEvent());
 
         return ImmutableValueGraph.copyOf(graph);
     }
@@ -160,4 +185,5 @@ public class PaymentGatewayStateTransitions {
     private boolean isValidTransitionImpl(ChargeStatus state, ChargeStatus targetState) {
         return graph.edgeValueOrDefault(state, targetState, null) != null;
     }
+
 }
