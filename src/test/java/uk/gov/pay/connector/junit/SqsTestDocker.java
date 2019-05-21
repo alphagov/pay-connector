89ce1b2742c6f15e3fef0e13ed8dd545da65ed11
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-05-21 11:23:02 +0100

    PP-5166: Use `waitingFor` to simplify healthcheck
    Makes use of SQS `GET` actions + test containers `waitingFor` to wait
    for the container to become available.

--- src/test/java/uk/gov/pay/connector/junit/SqsTestDocker.java
+++ src/test/java/uk/gov/pay/connector/junit/SqsTestDocker.java
@@ -5,14 +5,10 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.ListQueuesResult;
-import com.google.common.base.Stopwatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 /**
  * Start a Generic Test container with SQS docker image and creates queues if required.
@@ -27,7 +23,6 @@ final class SqsTestDocker {
     public static void initialise(String... queues) {
         try {
             createContainer();
-            waitForSQSContainerToStart();
             createQueues(queues);
         } catch (Exception e) {
             logger.error("Exception initialising SQS Container - {}", e.getMessage());
@@ -37,27 +32,11 @@ final class SqsTestDocker {
 
     private static void createContainer() {
         if (sqsContainer == null) {
-            // wait at least 3 minutes for container to startup to avoid timeout exception. Default is 60 seconds
             sqsContainer = new GenericContainer("roribio16/alpine-sqs")
-                    .withStartupTimeout(Duration.ofMinutes(3));
+                    .withExposedPorts(9324)
+                    .waitingFor(Wait.forHttp("/?Action=GetQueueUrl&QueueName=default"));
 
             sqsContainer.start();
-        }
-    }
-
-    private static void waitForSQSContainerToStart() throws InterruptedException {
-        Stopwatch timer = Stopwatch.createStarted();
-
-        boolean succeeded;
-        for (succeeded = false; !succeeded && timer.elapsed(TimeUnit.SECONDS) < 15L;
-             succeeded = checkSQSAvailability()) {
-            Thread.sleep(500L);
-        }
-
-        if (!succeeded) {
-            throw new RuntimeException("SQS Container did not start in 15 seconds.");
-        } else {
-            logger.info("SQS docker container started in {}.", timer.elapsed(TimeUnit.MILLISECONDS));
         }
     }
 
@@ -76,30 +55,6 @@ final class SqsTestDocker {
 
     private static String getEndpoint() {
         return "http://localhost:" + sqsContainer.getMappedPort(9324);
-    }
-
-    /**
-     * Checks for `default` queue which indicates SQS server is started and ready to use
-     *
-     * @return true if `default` queue exists and false otherwise
-     */
-    private static boolean checkSQSAvailability() {
-        AmazonSQS amazonSQS = getSqsClient();
-
-        try {
-            ListQueuesResult result = amazonSQS.listQueues();
-
-            for (String queueUrl : result.getQueueUrls()) {
-                if (queueUrl.contains("/queue/default"))
-                    return true;
-            }
-        }
-        // exceptions (ignored) are thrown if container is not ready yet
-        catch (Exception ignored) {
-
-        }
-
-        return false;
     }
 
     private static AmazonSQS getSqsClient() {
