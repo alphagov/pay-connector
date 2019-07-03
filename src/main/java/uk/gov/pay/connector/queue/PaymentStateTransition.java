559657aa0283089756275b3ba1c4748e1dfea5a3
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2019-07-03 13:02:12 +0100

    PP-5385 State transition implementation
    * implement delayed methods for DelayedQueue
    * add chargeEventId attribute
    * verify that `getDelay` and `compareTo` are
      respected on the `DelayQueue`
    
    with @sfount

--- src/main/java/uk/gov/pay/connector/queue/PaymentStateTransition.java
+++ src/main/java/uk/gov/pay/connector/queue/PaymentStateTransition.java
@@ -4,13 +4,32 @@ import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
 public class PaymentStateTransition implements Delayed {
+    private final long chargeEventId;
+    private final Long readTime;
+
+    private long delayDurationInMilliseconds = 100L;
+
+    public PaymentStateTransition(long chargeEventId) {
+        this.chargeEventId = chargeEventId;
+        this.readTime = System.currentTimeMillis() + delayDurationInMilliseconds;
+    }
+
     @Override
     public long getDelay(TimeUnit unit) {
-        return 0;
+        long diff = readTime - System.currentTimeMillis();
+        return unit.convert(diff, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public int compareTo(Delayed o) {
-        return 0;
+        return (int) (readTime - ((PaymentStateTransition) o).readTime);
+    }
+
+    public long getChargeEventId() {
+        return chargeEventId;
+    }
+
+    public long getDelayDurationInMilliseconds() {
+        return delayDurationInMilliseconds;
     }
 }
