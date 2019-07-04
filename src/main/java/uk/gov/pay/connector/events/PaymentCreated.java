c12f7002b06e0377b0597052a0e485a80f1d81d1
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-07-04 18:38:36 +0100

    PP-5385: restructure payment event -> factory
    * Have lowest level extended event class define `EventDetails` from a
    a `ChargeEventEntity`
    * Add Factory to build event object given an event type and
    `EventDetails` object

--- src/main/java/uk/gov/pay/connector/events/PaymentCreated.java
+++ src/main/java/uk/gov/pay/connector/events/PaymentCreated.java
@@ -1,20 +1,31 @@
 package uk.gov.pay.connector.events;
 
 import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
+import uk.gov.pay.connector.chargeevent.model.domain.ChargeEventEntity;
+import uk.gov.pay.connector.events.eventdetails.EventDetails;
 import uk.gov.pay.connector.events.eventdetails.PaymentCreatedEventDetails;
 
 import java.time.ZonedDateTime;
 
 public class PaymentCreated extends PaymentEvent {
 
-    public PaymentCreated(String resourceExternalId, PaymentCreatedEventDetails eventDetails, ZonedDateTime timestamp) {
-        super(resourceExternalId, eventDetails, timestamp);
+    private PaymentCreated(String resourceExternalId, PaymentCreatedEventDetails paymentCreatedEventDetails, ZonedDateTime timestamp) {
+        super(resourceExternalId, paymentCreatedEventDetails, timestamp);
     }
 
-    public static PaymentCreated from(ChargeEntity charge) {
+    // @TODO(sfount) add factory to create event given
+    //               - ChargeEvent
+    //               - Event Class type
+    // @TODO(sfount) make all events work magically with factory
+    // @TODO(sfount) submit PR
+    // @TODO(sfount) rebase feature branch with this PR
+    // @TODO(sfount) instantiate event through factory after having polled state transition queue
+    // @TODO(sfount) emit the event - profit
+    public static PaymentCreated from(ChargeEventEntity event) {
         return new PaymentCreated(
-                charge.getExternalId(), 
-                PaymentCreatedEventDetails.from(charge),
-                charge.getCreatedDate());
+                event.getChargeEntity().getExternalId(),
+                PaymentCreatedEventDetails.from(event.getChargeEntity()),
+                event.getUpdated()
+        );
     }
 }
