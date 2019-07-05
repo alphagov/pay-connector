66aa39e0909bedc7d461b1d65e21c5db2d4a1d2e
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-07-05 16:11:12 +0100

    PP-5386: expose payment event type given state transition graph
    Co-authored-by: Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>

--- src/test/java/uk/gov/pay/connector/common/model/domain/PaymentGatewayStateTransitionsTest.java
+++ src/test/java/uk/gov/pay/connector/common/model/domain/PaymentGatewayStateTransitionsTest.java
@@ -3,20 +3,25 @@ package uk.gov.pay.connector.common.model.domain;
 import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Test;
 import uk.gov.pay.connector.charge.model.domain.ChargeStatus;
+import uk.gov.pay.connector.events.CaptureAbandonedAfterTooManyRetries;
 import uk.gov.pay.connector.events.CaptureError;
 import uk.gov.pay.connector.events.CaptureSubmitted;
+import uk.gov.pay.connector.events.Event;
 import uk.gov.pay.connector.events.PaymentExpired;
 import uk.gov.pay.connector.events.UnspecifiedEvent;
 
 import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
 import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.AUTHORISATION_READY;
+import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.CAPTURE_APPROVED;
+import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.CAPTURE_ERROR;
 import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.CAPTURE_READY;
 import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.CAPTURE_SUBMITTED;
 import static uk.gov.pay.connector.charge.model.domain.ChargeStatus.CREATED;
@@ -47,5 +52,23 @@ public class PaymentGatewayStateTransitionsTest {
     public void isValidTransition_deniesTransitionWithInvalidEvent() {
         assertThat(PaymentGatewayStateTransitions.isValidTransition(CAPTURE_READY, CAPTURE_SUBMITTED, new CaptureSubmitted("a", ZonedDateTime.now())), is(true));
         assertThat(PaymentGatewayStateTransitions.isValidTransition(CAPTURE_READY, CAPTURE_SUBMITTED, new CaptureError("a", ZonedDateTime.now())), is(false));
+    }
+
+    @Test
+    public void getEventTransitionFor_returnsEventForModelledTypedEvent() {
+        Optional<Class<Event>> eventClassType = transitions.getEventForTransition(CAPTURE_APPROVED, CAPTURE_ERROR);
+        assertThat(eventClassType.get(), is(CaptureAbandonedAfterTooManyRetries.class));
+    }
+
+    @Test
+    public void getEventTransitionFor_returnsEmptyForInvalidTransition() {
+        Optional<Class<Event>> eventClassType = transitions.getEventForTransition(CAPTURE_APPROVED, CREATED);
+        assertThat(eventClassType, is(Optional.empty()));
+    }
+
+    @Test
+    public void getEventTransitionFor_returnsEmptyForUnmodelledEvent() {
+        Optional<Class<Event>> eventClassType = transitions.getEventForTransition(CAPTURE_APPROVED, CAPTURE_READY);
+        assertThat(eventClassType, is(Optional.empty()));
     }
 }
