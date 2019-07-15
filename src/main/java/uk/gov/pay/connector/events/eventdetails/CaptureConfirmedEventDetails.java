4869f6781977ff79226b4d4bc7909ac89c0e6aa3
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-07-15 14:40:51 +0100

    PP-5386: update CaptureConfirmed event for factory pattern
    * build capture confirmed event details for charge event
    * build cpature confirmed event from charge event

---
+++ src/main/java/uk/gov/pay/connector/events/eventdetails/CaptureConfirmedEventDetails.java
@@ -0,0 +1,24 @@
+package uk.gov.pay.connector.events.eventdetails;
+
+import uk.gov.pay.connector.chargeevent.model.domain.ChargeEventEntity;
+
+import java.time.ZonedDateTime;
+
+public class CaptureConfirmedEventDetails extends EventDetails {
+    private final ZonedDateTime gatewayEventDate;
+
+    private CaptureConfirmedEventDetails(ZonedDateTime gatewayEventDate) {
+        this.gatewayEventDate = gatewayEventDate;
+    }
+
+    public static CaptureConfirmedEventDetails from(ChargeEventEntity chargeEvent) {
+        return new CaptureConfirmedEventDetails(
+                chargeEvent.getGatewayEventDate()
+                .orElseGet(null)
+        );
+    }
+
+    public ZonedDateTime getGatewayEventDate() {
+        return gatewayEventDate;
+    }
+}
