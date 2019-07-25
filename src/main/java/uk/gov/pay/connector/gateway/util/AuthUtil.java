a158795e8c71d4a81df3d9d1ade898c522749f30
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-07-25 16:59:09 +0100

    PP-5209: specify `Stripe-Version` on all requests
    * introduce stripe version header to all Stripe requests
    * specify version as string in code, no reason to allow environment
    configuration for now
    * this will allow soft upgrades before commiting to a default API
    version on the platform Stripe configuration

--- src/main/java/uk/gov/pay/connector/gateway/util/AuthUtil.java
+++ src/main/java/uk/gov/pay/connector/gateway/util/AuthUtil.java
@@ -14,7 +14,9 @@ import static uk.gov.pay.connector.gatewayaccount.model.GatewayAccount.CREDENTIA
 import static uk.gov.pay.connector.gatewayaccount.model.GatewayAccount.CREDENTIALS_USERNAME;
 
 public class AuthUtil {
-    
+    private static final String STRIPE_VERSION_HEADER = "Stripe-Version";
+    private static final String STRIPE_API_VERSION = "2019-05-16";
+
     private static String encode(String username, String password) {
         return "Basic " + Base64.encodeAsString(username + ":" + password);
     }
@@ -22,9 +24,12 @@ public class AuthUtil {
     public static Map<String, String> getStripeAuthHeader(StripeGatewayConfig stripeGatewayConfig, boolean isLiveAccount) {
         StripeAuthTokens authTokens = stripeGatewayConfig.getAuthTokens();
         String value = format("Bearer %s", isLiveAccount ? authTokens.getLive() : authTokens.getTest());
-        return ImmutableMap.of(AUTHORIZATION, value);
+        return ImmutableMap.of(
+                AUTHORIZATION, value,
+                STRIPE_VERSION_HEADER, STRIPE_API_VERSION
+        );
     }
-    
+
     public static Map<String, String> getGatewayAccountCredentialsAsAuthHeader(GatewayAccountEntity gae) {
         String value = encode(gae.getCredentials().get(CREDENTIALS_USERNAME), gae.getCredentials().get(CREDENTIALS_PASSWORD));
         return ImmutableMap.of(AUTHORIZATION, value);
