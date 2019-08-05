8cd93a336ca678dd570d4656357ef12ee72bd7fc
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-05 21:49:31 +0100

    Removes unused imports

--- src/main/java/uk/gov/pay/connector/charge/model/telephone/Supplemental.java
+++ src/main/java/uk/gov/pay/connector/charge/model/telephone/Supplemental.java
@@ -1,6 +1,5 @@
 package uk.gov.pay.connector.charge.model.telephone;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
