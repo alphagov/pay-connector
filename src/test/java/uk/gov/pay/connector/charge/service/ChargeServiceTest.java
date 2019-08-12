2620c159321073d51fbc8762fd09f54651033a4d
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-12 15:36:22 +0100

    PP-5402 Creates tests to check whether a TelephoneChargeResponse has been created for both success and failed cases

--- src/test/java/uk/gov/pay/connector/charge/service/ChargeServiceTest.java
+++ src/test/java/uk/gov/pay/connector/charge/service/ChargeServiceTest.java
@@ -30,6 +30,7 @@ import uk.gov.pay.connector.charge.model.domain.ChargeStatus;
 import uk.gov.pay.connector.charge.model.telephone.PaymentOutcome;
 import uk.gov.pay.connector.charge.model.telephone.Supplemental;
 import uk.gov.pay.connector.charge.model.telephone.TelephoneChargeCreateRequest;
+import uk.gov.pay.connector.charge.model.telephone.TelephoneChargeResponse;
 import uk.gov.pay.connector.chargeevent.dao.ChargeEventDao;
 import uk.gov.pay.connector.chargeevent.model.domain.ChargeEventEntity;
 import uk.gov.pay.connector.common.exception.ConflictRuntimeException;
@@ -433,7 +434,6 @@ public class ChargeServiceTest {
         assertThat(tokenEntity.getChargeEntity().getId(), is(CHARGE_ENTITY_ID));
         assertThat(tokenEntity.getToken(), is(notNullValue()));
     }
-
     
     @Test
     public void shouldCreateATelephoneChargeForSuccess() {
@@ -531,6 +531,79 @@ public class ChargeServiceTest {
         assertThat(createdChargeEntity.getLanguage(), is(SupportedLanguage.ENGLISH));
     }
 
+    @Test
+    public void shouldCreateATelephoneChargeResponseForSuccess() {
+
+        PaymentOutcome paymentOutcome = new PaymentOutcome("success");
+        
+        TelephoneChargeCreateRequest telephoneChargeCreateRequest = telephoneRequestBuilder
+                .paymentOutcome(paymentOutcome)
+                .build();
+        
+        TelephoneChargeResponse chargeResponse = service.createTelephoneCharge(telephoneChargeCreateRequest, GATEWAY_ACCOUNT_ID, mockedUriInfo).get();
+
+        ArgumentCaptor<ChargeEntity> chargeEntityArgumentCaptor = forClass(ChargeEntity.class);
+        verify(mockedChargeDao).persist(chargeEntityArgumentCaptor.capture());
+        
+        assertThat(chargeResponse.getAmount(), is(100L));
+        assertThat(chargeResponse.getReference(), is("Some reference"));
+        assertThat(chargeResponse.getDescription(), is("Some description"));
+        assertThat(chargeResponse.getCreatedDate(), is("2018-02-21T16:04:25Z"));
+        assertThat(chargeResponse.getAuthorisedDate(), is("2018-02-21T16:05:33Z"));
+        assertThat(chargeResponse.getAuthCode(), is("666"));
+        assertThat(chargeResponse.getPaymentOutcome().getStatus(), is("success"));
+        assertThat(chargeResponse.getCardType(), is("visa"));
+        assertThat(chargeResponse.getNameOnCard(), is("Jane Doe"));
+        assertThat(chargeResponse.getEmailAddress(), is("jane.doe@example.com"));
+        assertThat(chargeResponse.getCardExpiry(), is("01/19"));
+        assertThat(chargeResponse.getLastFourDigits(), is("1234"));
+        assertThat(chargeResponse.getFirstSixDigits(), is("123456"));
+        assertThat(chargeResponse.getTelephoneNumber(), is("+447700900796"));
+        assertThat(chargeResponse.getPaymentId(), is("hu20sqlact5260q2nanm0q8u93"));
+        assertThat(chargeResponse.getState().getStatus(), is("success"));
+        assertThat(chargeResponse.getState().getFinished(), is(true));
+        assertThat(chargeResponse.getState().getMessage(), is("created"));
+    }
+
+    @Test
+    public void shouldCreateATelephoneChargeResponseForFailure() {
+
+        Supplemental supplemental = new Supplemental("ECKOH01234", "textual message describing error code");
+        PaymentOutcome paymentOutcome = new PaymentOutcome("failed", "P0010", supplemental);
+
+        TelephoneChargeCreateRequest telephoneChargeCreateRequest = telephoneRequestBuilder
+                .paymentOutcome(paymentOutcome)
+                .build();
+
+        TelephoneChargeResponse chargeResponse = service.createTelephoneCharge(telephoneChargeCreateRequest, GATEWAY_ACCOUNT_ID, mockedUriInfo).get();
+
+        ArgumentCaptor<ChargeEntity> chargeEntityArgumentCaptor = forClass(ChargeEntity.class);
+        verify(mockedChargeDao).persist(chargeEntityArgumentCaptor.capture());
+
+        assertThat(chargeResponse.getAmount(), is(100L));
+        assertThat(chargeResponse.getReference(), is("Some reference"));
+        assertThat(chargeResponse.getDescription(), is("Some description"));
+        assertThat(chargeResponse.getCreatedDate(), is("2018-02-21T16:04:25Z"));
+        assertThat(chargeResponse.getAuthorisedDate(), is("2018-02-21T16:05:33Z"));
+        assertThat(chargeResponse.getAuthCode(), is("666"));
+        assertThat(chargeResponse.getPaymentOutcome().getStatus(), is("failed"));
+        assertThat(chargeResponse.getPaymentOutcome().getCode(), is("P0010"));
+        assertThat(chargeResponse.getPaymentOutcome().getSupplemental().getErrorCode(), is("ECKOH01234"));
+        assertThat(chargeResponse.getPaymentOutcome().getSupplemental().getErrorMessage(), is("textual message describing error code"));
+        assertThat(chargeResponse.getCardType(), is("visa"));
+        assertThat(chargeResponse.getNameOnCard(), is("Jane Doe"));
+        assertThat(chargeResponse.getEmailAddress(), is("jane.doe@example.com"));
+        assertThat(chargeResponse.getCardExpiry(), is("01/19"));
+        assertThat(chargeResponse.getLastFourDigits(), is("1234"));
+        assertThat(chargeResponse.getFirstSixDigits(), is("123456"));
+        assertThat(chargeResponse.getTelephoneNumber(), is("+447700900796"));
+        assertThat(chargeResponse.getPaymentId(), is("hu20sqlact5260q2nanm0q8u93"));
+        assertThat(chargeResponse.getState().getStatus(), is("failed"));
+        assertThat(chargeResponse.getState().getFinished(), is(false));
+        assertThat(chargeResponse.getState().getMessage(), is("created"));
+        assertThat(chargeResponse.getState().getCode(), is("P0010"));
+    }
+    
     @Test
     public void shouldCreateAResponse() throws Exception {
         ChargeResponse response = service.create(requestBuilder.build(), GATEWAY_ACCOUNT_ID, mockedUriInfo).get();
