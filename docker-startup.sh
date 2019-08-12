c3b56abd7ead0ba59c3bab47c57b80ee5601ad8b
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-08-12 12:29:05 +0100

    Remove support for custom CA certificates
    We no longer use TLS in end to end tests with selfsigned certificates

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,16 +4,6 @@ set -eu
 RUN_MIGRATION=${RUN_MIGRATION:-false}
 RUN_APP=${RUN_APP:-true}
 
-if [ -n "${CERTS_PATH:-}" ]; then
-  i=0
-  truststore_pass=changeit
-  for cert in "$CERTS_PATH"/*; do
-    [ -f "$cert" ] || continue
-    echo "Adding $cert to default truststore"
-    keytool -importcert -noprompt -cacerts -storepass "$truststore_pass" -file "$cert" -alias custom$((i++))
-  done
-fi
-
 java $JAVA_OPTS -jar *-allinone.jar waitOnDependencies *.yaml
 
 if [ "$RUN_MIGRATION" == "true" ]; then
