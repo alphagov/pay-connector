bf96518631300330ef77d95f728ac304a66dd9eb
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-17 20:19:52 +0100

    PP-5402-Sets the generic type to the relevant annotation interface

--- src/main/java/uk/gov/pay/connector/charge/validation/telephone/CardLastFourDigitsValidator.java
+++ src/main/java/uk/gov/pay/connector/charge/validation/telephone/CardLastFourDigitsValidator.java
@@ -5,7 +5,7 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.HashSet;
 import java.util.regex.Pattern;
 
-public class CardLastFourDigitsValidator implements ConstraintValidator<ValidCardExpiryDate, String> {
+public class CardLastFourDigitsValidator implements ConstraintValidator<ValidCardLastFourDigits, String> {
 
     private Pattern pattern = Pattern.compile("\\d{4}");
 
