d02029b80c0d6aa6c99f4ced97241fcca71c5bde
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-27 17:25:43 +0100

    PP-5402 Adds unit test for CardTypeValidatorTest

--- src/main/java/uk/gov/pay/connector/app/ConnectorApp.java
+++ src/main/java/uk/gov/pay/connector/app/ConnectorApp.java
@@ -131,6 +131,8 @@ public class ConnectorApp extends Application<ConnectorConfiguration> {
         environment.jersey().register(injector.getInstance(DiscrepancyResource.class));
 
         environment.jersey().register(ChargeIdMDCLoggingFeature.class);
+        
+        environment.jersey().register(environment.getValidator());
 
         if(configuration.getCaptureProcessConfig().getBackgroundProcessingEnabled()) {
             setupSchedulers(environment, injector);
---
+++ src/test/java/uk/gov/pay/connector/charge/validation/telephone/CardTypeValidatorTest.java
@@ -0,0 +1,73 @@
+package uk.gov.pay.connector.charge.validation.telephone;
+
+import io.dropwizard.setup.Environment;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import uk.gov.pay.connector.app.ConnectorApp;
+import uk.gov.pay.connector.app.ConnectorConfiguration;
+import uk.gov.pay.connector.charge.model.telephone.PaymentOutcome;
+import uk.gov.pay.connector.charge.model.telephone.TelephoneChargeCreateRequest;
+import uk.gov.pay.connector.rules.DropwizardAppRule;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static uk.gov.pay.connector.util.NumberMatcher.isNumber;
+
+public class CardTypeValidatorTest {
+    
+    @ClassRule
+    public static final DropwizardAppRule<ConnectorConfiguration> app =
+            new DropwizardAppRule<>(ConnectorApp.class, resourceFilePath("config/test-config.yaml"));
+    
+    
+    private static TelephoneChargeCreateRequest.ChargeBuilder telephoneRequestBuilder = new TelephoneChargeCreateRequest.ChargeBuilder();
+    
+    @BeforeClass
+    public static void setUpValidator() {
+        telephoneRequestBuilder = telephoneRequestBuilder
+                .amount(1200L)
+                .description("Some description")
+                .reference("Some reference")
+                .createdDate("2018-02-21T16:04:25Z")
+                .authorisedDate("2018-02-21T16:05:33Z")
+                .authCode("666")
+                .processorId("1PROC")
+                .providerId("1PROV")
+                .cardExpiry("01/99")
+                .lastFourDigits("1234")
+                .firstSixDigits("123456")
+                .nameOnCard("Jane Doe")
+                .emailAddress("jane_doe@example.com")
+                .telephoneNumber("+447700900796")
+                .paymentOutcome(new PaymentOutcome("success"));
+    }
+    
+    @Test
+    public void failsValidationForInvalidCardType() {
+        Validator validator = app.getEnvironment().getValidator();
+        
+        
+        TelephoneChargeCreateRequest telephoneChargeCreateRequest = telephoneRequestBuilder
+                .cardType("bad-card")
+                .build();
+
+        
+        Set<ConstraintViolation<TelephoneChargeCreateRequest>> constraintViolations = validator.validate(telephoneChargeCreateRequest);
+
+        assertThat(constraintViolations.size(), isNumber(1));
+        assertThat(constraintViolations.iterator().next().getMessage(), is("Card type must be either master-card, visa, maestro, diners-club or american-express"));
+        
+    }
+    
+
+}
