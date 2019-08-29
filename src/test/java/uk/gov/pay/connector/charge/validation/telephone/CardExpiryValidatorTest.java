e712e683dbd017f07fa19ef250409fc6e482fd3c
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-29 22:49:12 +0100

    PP-5443 Sends only required fields in CardExpiryValidatorTest, CardTypeValidatorTest and PaymentOutcomeValidatorTest

--- src/test/java/uk/gov/pay/connector/charge/validation/telephone/CardExpiryValidatorTest.java
+++ src/test/java/uk/gov/pay/connector/charge/validation/telephone/CardExpiryValidatorTest.java
@@ -30,17 +30,11 @@ public class CardExpiryValidatorTest {
                 .amount(1200L)
                 .description("Some description")
                 .reference("Some reference")
-                .createdDate("2018-02-21T16:04:25Z")
-                .authorisedDate("2018-02-21T16:05:33Z")
-                .authCode("666")
                 .processorId("1PROC")
                 .providerId("1PROV")
                 .lastFourDigits("1234")
                 .firstSixDigits("123456")
                 .cardType("visa")
-                .nameOnCard("Jane Doe")
-                .emailAddress("jane_doe@example.com")
-                .telephoneNumber("+447700900796")
                 .paymentOutcome(new PaymentOutcome("success"));
     }
 
