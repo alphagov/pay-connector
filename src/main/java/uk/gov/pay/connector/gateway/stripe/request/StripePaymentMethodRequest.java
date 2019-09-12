0d6085b08b62e4bfbe751fc6dd9b311c9266e47a
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2019-09-12 16:24:58 +0100

    PP-5585 Add serialisation tests
    Serialisation tests for payment methods and payment intents request

--- src/main/java/uk/gov/pay/connector/gateway/stripe/request/StripePaymentMethodRequest.java
+++ src/main/java/uk/gov/pay/connector/gateway/stripe/request/StripePaymentMethodRequest.java
@@ -1,28 +1,27 @@
 package uk.gov.pay.connector.gateway.stripe.request;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.message.BasicNameValuePair;
 import uk.gov.pay.connector.app.StripeGatewayConfig;
+import uk.gov.pay.connector.gateway.model.AuthCardDetails;
 import uk.gov.pay.connector.gateway.model.OrderRequestType;
 import uk.gov.pay.connector.gateway.model.request.CardAuthorisationGatewayRequest;
 import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class StripePaymentMethodRequest extends StripeRequest {
-    private String cvc;
-    private String expiryMonth;
-    private String expiryYear;
-    private String cardNo;
-
+    private final AuthCardDetails authCardDetails;
+    
     public StripePaymentMethodRequest(
             GatewayAccountEntity gatewayAccount,
-            String idempotencyKey, StripeGatewayConfig stripeGatewayConfig,
-            String cvc, String expiryMonth, String expiryYear, String cardNo)
+            String idempotencyKey,
+            StripeGatewayConfig stripeGatewayConfig,
+            AuthCardDetails authCardDetails)
     {
         super(gatewayAccount, idempotencyKey, stripeGatewayConfig);
-        this.cvc = cvc;
-        this.expiryMonth = expiryMonth;
-        this.expiryYear = expiryYear;
-        this.cardNo = cardNo;
+        this.authCardDetails = authCardDetails;
     }
     
     public static StripePaymentMethodRequest of(CardAuthorisationGatewayRequest request, StripeGatewayConfig config) {
@@ -30,21 +29,31 @@ public class StripePaymentMethodRequest extends StripeRequest {
                 request.getGatewayAccount(),
                 request.getChargeExternalId(),
                 config,
-                request.getAuthCardDetails().getCvc(),
-                request.getAuthCardDetails().expiryMonth(),
-                request.getAuthCardDetails().expiryYear(),
-                request.getAuthCardDetails().getCardNo()
+                request.getAuthCardDetails()
         );
     }
 
     @Override
     protected Map<String, String> params() {
-        return Map.of(
-                "card[cvc]", cvc,
-                "card[exp_month]", expiryMonth,
-                "card[exp_year]", expiryYear,
-                "card[number]", cardNo,
-                "type", "card");
+        Map<String, String> localParams = new HashMap<>();
+        localParams.put("card[exp_month]", authCardDetails.expiryMonth());
+        localParams.put("card[exp_year]", authCardDetails.expiryYear());
+        localParams.put("card[number]", authCardDetails.getCardNo());
+        localParams.put("card[cvc]", authCardDetails.getCvc());
+        localParams.put("card[name]", authCardDetails.getCardHolder());
+        localParams.put("type", "card");
+
+        authCardDetails.getAddress().ifPresent(address -> {
+            localParams.put("card[address_line1]", address.getLine1());
+            if (StringUtils.isNotBlank(address.getLine2())) {
+                localParams.put("card[address_line2]", address.getLine2());
+            }
+            localParams.put("card[address_city]", address.getCity());
+            localParams.put("card[address_country]", address.getCountry());
+            localParams.put("card[address_zip]", address.getPostcode());
+        });
+        
+        return Map.copyOf(localParams);
     }
 
     @Override
@@ -55,5 +64,10 @@ public class StripePaymentMethodRequest extends StripeRequest {
     @Override
     protected OrderRequestType orderRequestType() {
         return OrderRequestType.AUTHORISE;
+    }
+
+    @Override
+    protected String idempotencyKeyType() {
+        return "payment_method";
     }
 }
---
+++ src/test/java/uk/gov/pay/connector/gateway/stripe/request/StripePaymentIntentRequestTest.java
@@ -0,0 +1,84 @@
+package uk.gov.pay.connector.gateway.stripe.request;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.pay.connector.app.StripeAuthTokens;
+import uk.gov.pay.connector.app.StripeGatewayConfig;
+import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
+import uk.gov.pay.connector.gateway.model.AuthCardDetails;
+import uk.gov.pay.connector.gateway.model.request.CardAuthorisationGatewayRequest;
+import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
+
+import java.net.URI;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StripePaymentIntentRequestTest {
+    private final String stripeConnectAccountId = "stripeConnectAccountId";
+    private final String chargeExternalId = "payChargeExternalId";
+    private final String stripeBaseUrl = "stripeUrl";
+
+    private StripePaymentIntentRequest stripePaymentIntentRequest;
+
+    @Mock
+    ChargeEntity charge;
+    @Mock
+    GatewayAccountEntity gatewayAccount;
+    @Mock
+    StripeAuthTokens stripeAuthTokens;
+    @Mock
+    StripeGatewayConfig stripeGatewayConfig;
+    private String paymentMethodId = "123abc";
+    private String frontendUrl = "frontendUrl";
+    private Long amount = 100L;
+
+    @Before
+    public void setUp() {
+        when(gatewayAccount.getCredentials()).thenReturn(ImmutableMap.of("stripe_account_id", stripeConnectAccountId));
+        when(charge.getGatewayAccount()).thenReturn(gatewayAccount);
+        when(charge.getExternalId()).thenReturn(chargeExternalId);
+        when(charge.getAmount()).thenReturn(amount);
+        when(stripeGatewayConfig.getUrl()).thenReturn(stripeBaseUrl);
+        when(stripeGatewayConfig.getAuthTokens()).thenReturn(stripeAuthTokens);
+        when(stripeGatewayConfig.getUrl()).thenReturn(stripeBaseUrl);
+
+        CardAuthorisationGatewayRequest authorisationGatewayRequest = new CardAuthorisationGatewayRequest(charge, new AuthCardDetails());
+
+        stripePaymentIntentRequest = StripePaymentIntentRequest.of(authorisationGatewayRequest, paymentMethodId, stripeGatewayConfig, frontendUrl);
+    }
+
+    @Test
+    public void shouldHaveCorrectParametersWithAddress() {
+        
+        String payload = stripePaymentIntentRequest.getGatewayOrder().getPayload();
+        assertThat(payload, containsString("payment_method=" + paymentMethodId));
+        assertThat(payload, containsString("amount=" + amount));
+        assertThat(payload, containsString("confirmation_method=automatic"));
+        assertThat(payload, containsString("capture_method=manual"));
+        assertThat(payload, containsString("currency=GBP"));
+        assertThat(payload, containsString("transfer_group=" + chargeExternalId));
+        assertThat(payload, containsString("on_behalf_of=" + stripeConnectAccountId));
+        assertThat(payload, containsString("confirm=true"));
+        assertThat(payload, containsString("return_url=" + frontendUrl + "%2Fcard_details%2F" + chargeExternalId + "%2F3ds_required_in"));
+    }
+
+    @Test
+    public void createsCorrectIdempotencyKey() {
+        assertThat(
+                stripePaymentIntentRequest.getHeaders().get("Idempotency-Key"),
+                is("payment_intent" + chargeExternalId));
+    }
+
+    @Test
+    public void shouldCreateCorrectUrl() {
+        assertThat(stripePaymentIntentRequest.getUrl(), is(URI.create(stripeBaseUrl + "/v1/payment_intents")));
+    }
+}
