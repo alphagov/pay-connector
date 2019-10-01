f24af60d812576ed1d336d752c3d189f4f2f6cb1
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-10-01 11:50:52 +0100

    PP-5670: Specify the consumer when running contract test
    Currently when a consumer build is kicked off, the provider contract test will
    run in its entirety. For example, in a frontend build, the connector provider
    contract tests will run as well. However, what is run is:
    
    frontend->connector
    ledger->connector
    publicapi->connector
    selfservice->connector
    
    We really only want to run frontend->connector in this case.
    
    This commit will allow a dynamic creation of the test suite to run based on a
    CONSUMER system property. This will mean shorter run times when a provider
    contract test is run as part of a specific consumer build.

--- src/test/java/uk/gov/pay/connector/pact/ContractTestSuite.java
+++ src/test/java/uk/gov/pay/connector/pact/ContractTestSuite.java
@@ -1,15 +1,41 @@
 package uk.gov.pay.connector.pact;
 
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runners.AllTests;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@RunWith(Suite.class)
+import java.util.Map;
 
-@Suite.SuiteClasses({
-        QueueMessageContractTest.class,
-        TransactionsApiContractTest.class,
-        FrontendContractTest.class
-})
+import static java.lang.String.format;
+
+@RunWith(AllTests.class)
 public class ContractTestSuite {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContractTestSuite.class);
+
+    private static Map<String, JUnit4TestAdapter> map = Map.of(
+            "frontend", new JUnit4TestAdapter(FrontendContractTest.class),
+            "ledger", new JUnit4TestAdapter(QueueMessageContractTest.class),
+            "publicapi", new JUnit4TestAdapter(PublicApiContractTest.class),
+            "selfservice", new JUnit4TestAdapter(SelfServiceContractTest.class));
+
+    public static TestSuite suite() {
+        String consumer = System.getProperty("CONSUMER");
+        TestSuite suite = new TestSuite();
+        
+        if (consumer == null || consumer.isBlank()) {
+            LOGGER.info("Running all contract tests.");
+            map.forEach((key, value) -> suite.addTest(value));
+        } else if (map.containsKey(consumer)) {
+            LOGGER.info("Running {}-connector contract tests only.", consumer);
+            suite.addTest(map.get(consumer));
+        } else {
+            throw new RuntimeException(format("Error running provider contract tests. ${CONSUMER} system property was %s.", consumer));
+        }
+        
+        return suite;
+    }
 }
---
+++ src/test/java/uk/gov/pay/connector/pact/SelfServiceContractTest.java
@@ -0,0 +1,15 @@
+package uk.gov.pay.connector.pact;
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
+import org.junit.runner.RunWith;
+
+@RunWith(PactRunner.class)
+@Provider("connector")
+@PactBroker(scheme = "https", host = "pact-broker-test.cloudapps.digital", tags = {"${PACT_CONSUMER_TAG}", "test"},
+        authentication = @PactBrokerAuth(username = "${PACT_BROKER_USERNAME}", password = "${PACT_BROKER_PASSWORD}"),
+        consumers = {"selfservice"})
+public class SelfServiceContractTest extends ContractTest {
+}
