2a0171e73f6347fd5b656696cc8d5de6255b4fac
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2019-10-03 14:56:43 +0100

    PP-5672 Always authorise with payment intents
    All issues have been ironed out with using payment intents, so
    fully switch to using them to create and authorise payments.
    All downstream operations are still backwards compatible with
    both payment intents and charges. I will switch them over in
    subsequent PRs.

--- src/main/java/uk/gov/pay/connector/gateway/stripe/handler/StripeAuthoriseHandler.java
+++ src/main/java/uk/gov/pay/connector/gateway/stripe/handler/StripeAuthoriseHandler.java
@@ -1,9 +1,6 @@
 package uk.gov.pay.connector.gateway.stripe.handler;
 
 import com.google.inject.Inject;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.pay.connector.app.ConnectorConfiguration;
@@ -11,41 +8,23 @@ import uk.gov.pay.connector.app.StripeGatewayConfig;
 import uk.gov.pay.connector.gateway.AuthoriseHandler;
 import uk.gov.pay.connector.gateway.GatewayClient;
 import uk.gov.pay.connector.gateway.GatewayException;
-import uk.gov.pay.connector.gateway.GatewayOrder;
 import uk.gov.pay.connector.gateway.model.GatewayError;
-import uk.gov.pay.connector.gateway.model.OrderRequestType;
 import uk.gov.pay.connector.gateway.model.request.CardAuthorisationGatewayRequest;
 import uk.gov.pay.connector.gateway.model.response.BaseResponse;
 import uk.gov.pay.connector.gateway.model.response.GatewayResponse;
-import uk.gov.pay.connector.gateway.stripe.Stripe3dsSourceAuthorisationResponse;
 import uk.gov.pay.connector.gateway.stripe.StripeAuthorisationResponse;
 import uk.gov.pay.connector.gateway.stripe.json.StripeAuthorisationFailedResponse;
-import uk.gov.pay.connector.gateway.stripe.json.StripeCharge;
 import uk.gov.pay.connector.gateway.stripe.json.StripeErrorResponse;
-import uk.gov.pay.connector.gateway.stripe.json.StripeSourcesResponse;
-import uk.gov.pay.connector.gateway.stripe.json.StripeTokenResponse;
-import uk.gov.pay.connector.gateway.stripe.request.StripeAuthoriseRequest;
 import uk.gov.pay.connector.gateway.stripe.request.StripePaymentIntentRequest;
 import uk.gov.pay.connector.gateway.stripe.request.StripePaymentMethodRequest;
-import uk.gov.pay.connector.gateway.stripe.response.Stripe3dsSourceResponse;
 import uk.gov.pay.connector.gateway.stripe.response.StripePaymentIntentResponse;
 import uk.gov.pay.connector.gateway.stripe.response.StripePaymentMethodResponse;
-import uk.gov.pay.connector.gateway.util.AuthUtil;
-import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
 import uk.gov.pay.connector.util.JsonObjectMapper;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED_TYPE;
 import static javax.ws.rs.core.Response.Status.Family.CLIENT_ERROR;
 import static javax.ws.rs.core.Response.Status.Family.SERVER_ERROR;
 import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 import static uk.gov.pay.connector.gateway.model.GatewayError.gatewayConnectionError;
-import static uk.gov.pay.connector.gateway.model.response.BaseAuthoriseResponse.AuthoriseStatus.AUTHORISED;
 
 public class StripeAuthoriseHandler implements AuthoriseHandler {
 
@@ -73,11 +52,13 @@ public class StripeAuthoriseHandler implements AuthoriseHandler {
                 .GatewayResponseBuilder
                 .responseBuilder();
         try {
-            if (usePaymentIntents(request)) {
-                return authoriseWithPaymentIntent(request);
-            } else {
-                return authoriseWithCharge(request);
-            }
+            StripePaymentMethodResponse stripePaymentMethodResponse = createPaymentMethod(request);
+            StripePaymentIntentResponse stripePaymentIntentResponse = createPaymentIntent(request, stripePaymentMethodResponse.getId());
+
+            return GatewayResponse
+                    .GatewayResponseBuilder
+                    .responseBuilder()
+                    .withResponse(StripeAuthorisationResponse.of(stripePaymentIntentResponse)).build();
         } catch (GatewayException.GatewayErrorException e) {
             String chargeExternalId = request.getChargeExternalId();
 
@@ -104,144 +85,6 @@ public class StripeAuthoriseHandler implements AuthoriseHandler {
             logger.error("GatewayException occurred for charge external id {}, error:\n {}", request.getChargeExternalId(), e);
             return responseBuilder.withGatewayError(e.toGatewayError()).build();
         }
-    }
-
-    private GatewayResponse authoriseWithCharge(CardAuthorisationGatewayRequest request)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        GatewayResponse.GatewayResponseBuilder<BaseResponse> responseBuilder = GatewayResponse
-                .GatewayResponseBuilder
-                .responseBuilder();
-        StripeTokenResponse tokenResponse = createToken(request);
-        StripeSourcesResponse stripeSourcesResponse = createSource(request, tokenResponse.getId());
-        if (stripeSourcesResponse.require3ds()) {
-            String source3dsResponse = create3dsSource(request, stripeSourcesResponse.getId());
-            Stripe3dsSourceResponse sourceResponse = jsonObjectMapper.getObject(source3dsResponse, Stripe3dsSourceResponse.class);
-
-            Stripe3dsSourceAuthorisationResponse response = new Stripe3dsSourceAuthorisationResponse(sourceResponse);
-
-            if(AUTHORISED.equals(response.authoriseStatus())){
-                StripeAuthorisationResponse stripeAuthResponse = createCharge(request, response.getTransactionId());
-                return responseBuilder.withResponse(stripeAuthResponse).build();
-            }
-
-            return responseBuilder.withResponse(new Stripe3dsSourceAuthorisationResponse(sourceResponse)).build();
-        } else {
-            StripeAuthorisationResponse stripeAuthResponse = createCharge(request, stripeSourcesResponse.getId());
-            return responseBuilder.withResponse(stripeAuthResponse).build();
-        }
-    }
-
-    private boolean usePaymentIntents(CardAuthorisationGatewayRequest request) {
-        return request.getGatewayAccount().getIntegrationVersion3ds() == 2;
-    }
-
-    private String create3dsSource(CardAuthorisationGatewayRequest request, String sourceId)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        GatewayAccountEntity gatewayAccount = request.getGatewayAccount();
-        return postToStripe(
-                "/v1/sources",
-                threeDSecurePayload(request, sourceId),
-                gatewayAccount.isLive(),
-                gatewayAccount,
-                OrderRequestType.STRIPE_CREATE_3DS_SOURCE).getEntity();
-    }
-
-    private StripeAuthorisationResponse createCharge(CardAuthorisationGatewayRequest request, String sourceId)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        String jsonResponse = client.postRequestFor(StripeAuthoriseRequest.of(sourceId, request, stripeGatewayConfig)).getEntity();
-        final StripeCharge createChargeResponse = jsonObjectMapper.getObject(jsonResponse, StripeCharge.class);
-        return StripeAuthorisationResponse.of(createChargeResponse);
-    }
-
-    private StripeSourcesResponse createSource(CardAuthorisationGatewayRequest request, String tokenId)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        GatewayAccountEntity gatewayAccount = request.getGatewayAccount();
-        String jsonResponse = postToStripe(
-                "/v1/sources",
-                sourcesPayload(tokenId),
-                gatewayAccount.isLive(),
-                gatewayAccount,
-                OrderRequestType.STRIPE_CREATE_SOURCE).getEntity();
-        return jsonObjectMapper.getObject(jsonResponse, StripeSourcesResponse.class);
-    }
-
-    private StripeTokenResponse createToken(CardAuthorisationGatewayRequest request)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        GatewayAccountEntity gatewayAccount = request.getGatewayAccount();
-        String jsonResponse = postToStripe(
-                "/v1/tokens",
-                tokenPayload(request),
-                gatewayAccount.isLive(),
-                gatewayAccount,
-                OrderRequestType.STRIPE_TOKEN).getEntity();
-        return jsonObjectMapper.getObject(jsonResponse, StripeTokenResponse.class);
-    }
-
-    private GatewayClient.Response postToStripe(String path,
-                                                String payload,
-                                                boolean isLiveAccount,
-                                                GatewayAccountEntity gatewayAccountEntity,
-                                                OrderRequestType orderRequestType)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        return client.postRequestFor(
-                URI.create(stripeGatewayConfig.getUrl() + path),
-                gatewayAccountEntity,
-                new GatewayOrder(orderRequestType, payload, APPLICATION_FORM_URLENCODED_TYPE),
-                AuthUtil.getStripeAuthHeader(stripeGatewayConfig, isLiveAccount)
-        );
-    }
-
-    private String sourcesPayload(String token) {
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("type", "card"));
-        params.add(new BasicNameValuePair("token", token));
-        params.add(new BasicNameValuePair("usage", "single_use"));
-        return URLEncodedUtils.format(params, UTF_8);
-    }
-
-    private String threeDSecurePayload(CardAuthorisationGatewayRequest request, String sourceId) {
-        String frontend3dsIncomingUrl = String.format("%s/card_details/%s/3ds_required_in", frontendUrl, request.getChargeExternalId());
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("type", "three_d_secure"));
-        params.add(new BasicNameValuePair("amount", request.getAmount()));
-        params.add(new BasicNameValuePair("currency", "GBP"));
-        params.add(new BasicNameValuePair("redirect[return_url]", frontend3dsIncomingUrl));
-        params.add(new BasicNameValuePair("three_d_secure[card]", sourceId));
-        return URLEncodedUtils.format(params, UTF_8);
-    }
-
-    private String tokenPayload(CardAuthorisationGatewayRequest request) {
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("card[cvc]", request.getAuthCardDetails().getCvc()));
-        params.add(new BasicNameValuePair("card[exp_month]", request.getAuthCardDetails().expiryMonth()));
-        params.add(new BasicNameValuePair("card[exp_year]", request.getAuthCardDetails().expiryYear()));
-        params.add(new BasicNameValuePair("card[number]", request.getAuthCardDetails().getCardNo()));
-        params.add(new BasicNameValuePair("card[name]", request.getAuthCardDetails().getCardHolder()));
-
-        request.getAuthCardDetails().getAddress().ifPresent(address -> {
-            params.add(new BasicNameValuePair("card[address_line1]", address.getLine1()));
-
-            if (StringUtils.isNotBlank(address.getLine2())) {
-                params.add(new BasicNameValuePair("card[address_line2]", address.getLine2()));
-            }
-
-            params.add(new BasicNameValuePair("card[address_city]", address.getCity()));
-            params.add(new BasicNameValuePair("card[address_country]", address.getCountry()));
-            params.add(new BasicNameValuePair("card[address_zip]", address.getPostcode()));
-        });
-
-        return URLEncodedUtils.format(params, UTF_8);
-    }
-
-    private GatewayResponse authoriseWithPaymentIntent(CardAuthorisationGatewayRequest request)
-            throws GatewayException.GenericGatewayException, GatewayException.GatewayConnectionTimeoutException, GatewayException.GatewayErrorException {
-        StripePaymentMethodResponse stripePaymentMethodResponse = createPaymentMethod(request);
-        StripePaymentIntentResponse stripePaymentIntentResponse = createPaymentIntent(request, stripePaymentMethodResponse.getId());
-
-        return GatewayResponse
-                .GatewayResponseBuilder
-                .responseBuilder()
-                .withResponse(StripeAuthorisationResponse.of(stripePaymentIntentResponse)).build();
     }
 
     private StripePaymentIntentResponse createPaymentIntent(CardAuthorisationGatewayRequest request, String paymentMethodId)
--- src/test/java/uk/gov/pay/connector/it/resources/stripe/StripeResourceAuthorizeIT.java
+++ src/test/java/uk/gov/pay/connector/it/resources/stripe/StripeResourceAuthorizeIT.java
@@ -3,12 +3,9 @@ package uk.gov.pay.connector.it.resources.stripe;
 import com.amazonaws.util.json.Jackson;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
-import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import com.google.common.collect.ImmutableMap;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.RandomUtils;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -18,7 +15,6 @@ import uk.gov.pay.commons.model.ErrorIdentifier;
 import uk.gov.pay.connector.app.ConnectorApp;
 import uk.gov.pay.connector.charge.model.domain.ChargeStatus;
 import uk.gov.pay.connector.gateway.PaymentGatewayName;
-import uk.gov.pay.connector.gateway.model.PayersCardType;
 import uk.gov.pay.connector.junit.DropwizardConfig;
 import uk.gov.pay.connector.junit.DropwizardJUnitRunner;
 import uk.gov.pay.connector.junit.DropwizardTestContext;
@@ -28,26 +24,18 @@ import uk.gov.pay.connector.util.DatabaseTestHelper;
 import uk.gov.pay.connector.util.RestAssuredClient;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.findAll;
-import static com.github.tomakehurst.wiremock.client.WireMock.matching;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.emptyMap;
 import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
-import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.eclipse.jetty.http.HttpStatus.BAD_REQUEST_400;
-import static org.eclipse.jetty.http.HttpStatus.INTERNAL_SERVER_ERROR_500;
 import static org.eclipse.jetty.http.HttpStatus.NO_CONTENT_204;
 import static org.eclipse.jetty.http.HttpStatus.OK_200;
 import static org.hamcrest.Matchers.contains;
@@ -77,7 +65,6 @@ public class StripeResourceAuthorizeIT {
     private static final String EXP_YEAR = "99";
     private static final String CARD_NUMBER = "4242424242424242";
     private static final String AMOUNT = "6234";
-    private static final String DESCRIPTION = "Test description";
     private static final String CARD_TYPE = "CREDIT"; 
 
     private static final String ADDRESS_LINE_1 = "The Money Pool";
@@ -114,33 +101,10 @@ public class StripeResourceAuthorizeIT {
         databaseTestHelper = testContext.getDatabaseTestHelper();
         accountId = String.valueOf(RandomUtils.nextInt());
 
-        stripeMockClient.mockCreateToken();
-        stripeMockClient.mockCreateSource();
-        stripeMockClient.mockCreateCharge();
-
+        
         connectorRestApiClient = new RestAssuredClient(testContext.getPort(), accountId);
     }
-
-    @Test
-    public void cardAuthorisationFailureShouldReturnBadRequest() {
-        stripeMockClient.mockAuthorisationFailed();
-
-        addGatewayAccount(ImmutableMap.of("stripe_account_id", stripeAccountId));
-
-        String externalChargeId = addCharge();
-
-        given().port(testContext.getPort())
-                .contentType(JSON)
-                .body(validAuthorisationDetails)
-                .post(authoriseChargeUrlFor(externalChargeId))
-                .then()
-                .statusCode(BAD_REQUEST_400)
-                .body("message", contains("This transaction was declined."))
-                .body("error_identifier", is(ErrorIdentifier.GENERIC.toString()));
-
-        assertFrontendChargeStatusIs(externalChargeId, AUTHORISATION_REJECTED.toString());
-    }
-
+    
     @Test
     public void cardAuthorisationWithPaymentIntentsFailureShouldReturnBadRequest() {
         stripeMockClient.mockAuthorisationFailedWithPaymentIntents();
@@ -160,40 +124,9 @@ public class StripeResourceAuthorizeIT {
 
         assertFrontendChargeStatusIs(externalChargeId, AUTHORISATION_REJECTED.toString());
     }
-
-
+    
     @Test
     public void authoriseCharge() {
-        addGatewayAccount(ImmutableMap.of("stripe_account_id", stripeAccountId));
-
-        String externalChargeId = addCharge();
-
-        given().port(testContext.getPort())
-                .contentType(JSON)
-                .body(validAuthorisationDetails)
-                .post(authoriseChargeUrlFor(externalChargeId))
-                .then()
-                .body("status", is(AUTHORISATION_SUCCESS.toString()))
-                .statusCode(OK_200);
-
-        verify(postRequestedFor(urlEqualTo("/v1/tokens"))
-                .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED))
-                .withRequestBody(equalTo(constructExpectedTokensRequestBody())));
-
-        verify(postRequestedFor(urlEqualTo("/v1/sources"))
-                .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED))
-                .withRequestBody(equalTo(constructExpectedSourcesRequestBody())));
-
-        verify(postRequestedFor(urlEqualTo("/v1/charges"))
-                .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED)));
-
-        List<LoggedRequest> requests = findAll(postRequestedFor(urlMatching("/v1/charges")));
-        assertThat(requests).hasSize(1);
-        assertExpectedAuthoriseRequestBody(requests.get(0), externalChargeId);
-    }
-
-    @Test
-    public void authoriseChargeWithPaymentIntents() {
         stripeMockClient.mockCreatePaymentMethod();
         stripeMockClient.mockCreatePaymentIntent();
         addGatewayAccountWith3DS2Enabled(ImmutableMap.of("stripe_account_id", stripeAccountId));
@@ -219,6 +152,8 @@ public class StripeResourceAuthorizeIT {
 
     @Test
     public void shouldAuthoriseChargeWithoutBillingAddress() {
+        stripeMockClient.mockCreatePaymentMethod();
+        stripeMockClient.mockCreatePaymentIntent();
         addGatewayAccount(ImmutableMap.of("stripe_account_id", stripeAccountId));
 
         String externalChargeId = addCharge();
@@ -230,29 +165,20 @@ public class StripeResourceAuthorizeIT {
                 .then()
                 .body("status", is(AUTHORISATION_SUCCESS.toString()))
                 .statusCode(OK_200);
-
-        verify(postRequestedFor(urlEqualTo("/v1/tokens"))
-                .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED))
-                .withRequestBody(equalTo(constructExpectedTokensRequestBodyWithoutBillingAddress())));
-
-        verify(postRequestedFor(urlEqualTo("/v1/sources"))
-                .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED))
-                .withRequestBody(matching(constructExpectedSourcesRequestBody())));
-
-        verify(postRequestedFor(urlEqualTo("/v1/charges"))
+        
+        verify(postRequestedFor(urlEqualTo("/v1/payment_methods"))
                 .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED)));
 
-        List<LoggedRequest> requests = findAll(postRequestedFor(urlMatching("/v1/charges")));
-        assertThat(requests).hasSize(1);
-        assertExpectedAuthoriseRequestBody(requests.get(0), externalChargeId);
+        verify(postRequestedFor(urlEqualTo("/v1/payment_intents"))
+                .withHeader("Content-Type", equalTo(APPLICATION_FORM_URLENCODED)));
+
     }
 
     @Test
     public void shouldRespondAs3dsRequired_whenAuthorisationRequires3ds() {
         addGatewayAccount(ImmutableMap.of("stripe_account_id", stripeAccountId));
-
-        stripeMockClient.mockCreateSourceWithThreeDSecureRequired();
-        stripeMockClient.mockCreate3dsSource();
+        stripeMockClient.mockCreatePaymentMethod();
+        stripeMockClient.mockCreatePaymentIntentRequiring3DS();
 
         String externalChargeId = addCharge();
         given().port(testContext.getPort())
@@ -264,31 +190,6 @@ public class StripeResourceAuthorizeIT {
                 .statusCode(OK_200);
 
         assertFrontendChargeStatusIs(externalChargeId, AUTHORISATION_3DS_REQUIRED.toString());
-
-        List<LoggedRequest> requests = findAll(postRequestedFor(urlMatching("/v1/tokens")));
-        assertThat(requests).hasSize(1);
-        requests = findAll(postRequestedFor(urlMatching("/v1/sources")));
-        assertThat(requests).hasSize(2);
-        requests = findAll(postRequestedFor(urlMatching("/v1/charges")));
-        assertThat(requests).hasSize(0);
-    }
-
-    @Test
-    public void invalidAuthCredentialsShouldReturnAnInternalServerError() {
-        stripeMockClient.mockUnauthorizedResponse();
-
-        addGatewayAccount(ImmutableMap.of("stripe_account_id", stripeAccountId));
-
-        String externalChargeId = addCharge();
-
-        given().port(testContext.getPort())
-                .contentType(JSON)
-                .body(validAuthorisationDetails)
-                .post(authoriseChargeUrlFor(externalChargeId))
-                .then()
-                .statusCode(INTERNAL_SERVER_ERROR_500)
-                .body("message", contains("There was an internal server error authorising charge_external_id: " + externalChargeId))
-                .body("error_identifier", is(ErrorIdentifier.GENERIC.toString()));
     }
 
     @Test
@@ -399,52 +300,7 @@ public class StripeResourceAuthorizeIT {
                 .build());
     }
 
-    private String constructExpectedSourcesRequestBody() {
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("type", "card"));
-        params.add(new BasicNameValuePair("token", "tok_1DJfnpHj08j2jFuBPMcHN1F8")); //This comes from resources/stripe/create_token_response.json
-        params.add(new BasicNameValuePair("usage", "single_use"));
-        return URLEncodedUtils.format(params, UTF_8);
-    }
-
     private String captureChargeUrlFor(String chargeId) {
         return "/v1/frontend/charges/{chargeId}/capture".replace("{chargeId}", chargeId);
-    }
-
-    private void assertExpectedAuthoriseRequestBody(LoggedRequest request, String chargeExternalId) {
-        assertThat(request.getBodyAsString()).contains("amount=" + AMOUNT);
-        assertThat(request.getBodyAsString()).contains("on_behalf_of=" + stripeAccountId);
-        assertThat(request.getBodyAsString()).contains("transfer_group=" +  chargeExternalId);
-        assertThat(request.getBodyAsString()).contains("currency=GBP");
-        assertThat(request.getBodyAsString()).contains("source=src_1DT9bn2eZvKYlo2Cg5okt8WC");
-        assertThat(request.getBodyAsString()).contains("capture=false");
-        assertThat(request.getBodyAsString()).contains("description=Test+description");
-    }
-
-    private String constructExpectedTokensRequestBody() {
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("card[cvc]", CVC));
-        params.add(new BasicNameValuePair("card[exp_month]", EXP_MONTH));
-        params.add(new BasicNameValuePair("card[exp_year]", EXP_YEAR));
-        params.add(new BasicNameValuePair("card[number]", CARD_NUMBER));
-        params.add(new BasicNameValuePair("card[name]", CARD_HOLDER_NAME));
-
-        params.add(new BasicNameValuePair("card[address_line1]", ADDRESS_LINE_1));
-        params.add(new BasicNameValuePair("card[address_line2]", ADDRESS_LINE_2));
-        params.add(new BasicNameValuePair("card[address_city]", ADDRESS_CITY));
-        params.add(new BasicNameValuePair("card[address_country]", ADDRESS_COUNTRY_GB));
-        params.add(new BasicNameValuePair("card[address_zip]", ADDRESS_POSTCODE));
-
-        return URLEncodedUtils.format(params, UTF_8);
-    }
-
-    private String constructExpectedTokensRequestBodyWithoutBillingAddress() {
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("card[cvc]", CVC));
-        params.add(new BasicNameValuePair("card[exp_month]", EXP_MONTH));
-        params.add(new BasicNameValuePair("card[exp_year]", EXP_YEAR));
-        params.add(new BasicNameValuePair("card[number]", CARD_NUMBER));
-        params.add(new BasicNameValuePair("card[name]", CARD_HOLDER_NAME));
-        return URLEncodedUtils.format(params, UTF_8);
     }
 }
