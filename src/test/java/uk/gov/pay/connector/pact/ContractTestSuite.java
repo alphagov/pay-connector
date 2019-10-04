cd2b992b4adfb6c531ef9b36f822f2ff7a0a39a1
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-10-04 10:19:58 +0100

    PP-5670: Update pay java commons lib

--- src/test/java/uk/gov/pay/connector/pact/ContractTestSuite.java
+++ src/test/java/uk/gov/pay/connector/pact/ContractTestSuite.java
@@ -1,22 +1,21 @@
 package uk.gov.pay.connector.pact;
 
+import com.google.common.collect.ImmutableSetMultimap;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
 import uk.gov.pay.commons.testing.pact.provider.CreateTestSuite;
 
-import java.util.Map;
-
 @RunWith(AllTests.class)
 public class ContractTestSuite {
     
     public static TestSuite suite() {
-        Map<String, JUnit4TestAdapter> map = Map.of(
+        ImmutableSetMultimap<String, JUnit4TestAdapter> consumerToJUnitTest = ImmutableSetMultimap.of(
                 "frontend", new JUnit4TestAdapter(FrontendContractTest.class),
                 "ledger", new JUnit4TestAdapter(QueueMessageContractTest.class),
                 "publicapi", new JUnit4TestAdapter(PublicApiContractTest.class),
                 "selfservice", new JUnit4TestAdapter(SelfServiceContractTest.class));
-        return CreateTestSuite.create(map);
+        return CreateTestSuite.create(consumerToJUnitTest);
     }
 }
