5105ff203b4095f1052ea209549df8fc7df93e4d
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-10-07 15:02:32 +0100

    PP-561 Return Card Type In V2 Endpoint
    - Returns the `card_type` in the getCharges v2 endpoint

--- src/main/java/uk/gov/pay/connector/charge/service/ChargeService.java
+++ src/main/java/uk/gov/pay/connector/charge/service/ChargeService.java
@@ -362,6 +362,7 @@ public class ChargeService {
         if (chargeEntity.getCardDetails() != null) {
             persistedCard = chargeEntity.getCardDetails().toCard();
             persistedCard.setCardBrand(findCardBrandLabel(chargeEntity.getCardDetails().getCardBrand()).orElse(""));
+            persistedCard.setCardType(chargeEntity.getCardDetails().getCardType());
         }
 
         ChargeResponse.Auth3dsData auth3dsData = null;
