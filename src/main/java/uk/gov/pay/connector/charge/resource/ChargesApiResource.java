3195a9e9d4f31f6907db8e0de4743894ba7e0b74
Krishna Bottla <krishna.bottla@digital.cabinet-office.gov.uk>
2019-12-03 18:05:45 +0000

    PP-5425 Remove /v2 search endpoint
    - Removes `/v2/api/accounts/{accountId}/charges` endpoint and relevant tests, as the endpoint is no longer used by selfservice

--- src/main/java/uk/gov/pay/connector/charge/resource/ChargesApiResource.java
+++ src/main/java/uk/gov/pay/connector/charge/resource/ChargesApiResource.java
@@ -166,56 +166,6 @@ public class ChargesApiResource {
                 }); // always the first page if its missing
     }
 
-    @GET
-    @Path("/v2/api/accounts/{accountId}/charges")
-    @Timed
-    @Produces(APPLICATION_JSON)
-    public Response getChargesJsonV2(@PathParam(ACCOUNT_ID) Long accountId,
-                                     @QueryParam(EMAIL_KEY) String email,
-                                     @QueryParam(REFERENCE_KEY) String reference,
-                                     @QueryParam(CARDHOLDER_NAME_KEY) String cardHolderName,
-                                     @QueryParam(LAST_DIGITS_CARD_NUMBER_KEY) String lastDigitsCardNumber,
-                                     @QueryParam(FIRST_DIGITS_CARD_NUMBER_KEY) String firstDigitsCardNumber,
-                                     @QueryParam(PAYMENT_STATES_KEY) CommaDelimitedSetParameter paymentStates,
-                                     @QueryParam(REFUND_STATES_KEY) CommaDelimitedSetParameter refundStates,
-                                     @QueryParam(CARD_BRAND_KEY) List<String> cardBrands,
-                                     @QueryParam(FROM_DATE_KEY) String fromDate,
-                                     @QueryParam(TO_DATE_KEY) String toDate,
-                                     @QueryParam(PAGE) Long pageNumber,
-                                     @QueryParam(DISPLAY_SIZE) Long displaySize,
-                                     @Context UriInfo uriInfo) {
-
-        List<Pair<String, String>> inputDatePairMap = ImmutableList.of(Pair.of(FROM_DATE_KEY, fromDate), Pair.of(TO_DATE_KEY, toDate));
-        List<Pair<String, Long>> nonNegativePairMap = ImmutableList.of(Pair.of(PAGE, pageNumber), Pair.of(DISPLAY_SIZE, displaySize));
-        //Client using v2 API will have the feature flag enabled by default
-        boolean isFeatureTransactionsEnabled = true;
-
-        return ApiValidators
-                .validateQueryParams(inputDatePairMap, nonNegativePairMap) //TODO - improvement, get the entire searchparam object into the validateQueryParams
-                .map(ResponseUtil::badRequestResponse)
-                .orElseGet(() -> {
-                    SearchParams searchParams = new SearchParams()
-                            .withGatewayAccountId(accountId)
-                            .withEmailLike(email)
-                            .withCardHolderNameLike(cardHolderName != null ? CardHolderName.of(cardHolderName) : null)
-                            .withLastDigitsCardNumber(LastDigitsCardNumber.ofNullable(lastDigitsCardNumber))
-                            .withFirstDigitsCardNumber(FirstDigitsCardNumber.ofNullable(firstDigitsCardNumber))
-                            .withReferenceLike(reference != null ? ServicePaymentReference.of(reference) : null)
-                            .withCardBrands(removeBlanks(cardBrands))
-                            .withFromDate(parseDate(fromDate))
-                            .withToDate(parseDate(toDate))
-                            .withDisplaySize(displaySize != null ? displaySize : configuration.getTransactionsPaginationConfig().getDisplayPageSize())
-                            .withPage(pageNumber != null ? pageNumber : 1)
-                            .withTransactionType(inferTransactionTypeFrom(toList(paymentStates), toList(refundStates)))
-                            .addExternalChargeStatesV2(toList(paymentStates))
-                            .addExternalRefundStates(toList(refundStates));
-
-                    return gatewayAccountDao.findById(accountId)
-                            .map(gatewayAccount -> listCharges(searchParams, isFeatureTransactionsEnabled, uriInfo))
-                            .orElseGet(() -> notFoundResponse(format("account with id %s not found", accountId)));
-                }); // always the first page if its missing
-    }
-
     private List<String> toList(CommaDelimitedSetParameter commaDelimitedSetParameter) {
         if (commaDelimitedSetParameter != null) {
             return commaDelimitedSetParameter.stream().collect(Collectors.toList());
