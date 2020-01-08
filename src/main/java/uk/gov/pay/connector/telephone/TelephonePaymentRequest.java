de47aa50406d2946c3bfa15a14dcf6695dd7950e
Stephen Daly <stephencdaly@gmail.com>
2020-01-08 16:11:03 +0000

    Add telephone payments resource

---
+++ src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentRequest.java
@@ -0,0 +1,17 @@
+package uk.gov.pay.connector.telephone;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TelephonePaymentRequest {
+    
+    @JsonProperty("stripe_id")
+    private final String stripeId;
+
+    public TelephonePaymentRequest(String stripeChargeId) {
+        this.stripeId = stripeChargeId;
+    }
+
+    public String getStripeId() {
+        return stripeId;
+    }
+}
---
+++ src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentsResource.java
@@ -0,0 +1,25 @@
+package uk.gov.pay.connector.telephone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/v1/api/ivr/")
+public class TelephonePaymentsResource {
+    
+    private static final Logger logger = LoggerFactory.getLogger(TelephonePaymentsResource.class);
+    
+    @POST
+    @Consumes(APPLICATION_JSON)
+    @Path("/create-payment")
+    public Response createPayment(TelephonePaymentRequest telephonePaymentRequest) {
+        logger.info("Received request to create a telephone payment for Stripe id " + telephonePaymentRequest.getStripeId());
+        return Response.ok().build();
+    }
+}
