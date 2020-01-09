501292ff71207777db6b1573c1057400b85c3f06
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2020-01-09 16:01:46 +0000

    update telephone payments resource

--- src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentsResource.java
+++ src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentsResource.java
@@ -3,6 +3,7 @@ package uk.gov.pay.connector.telephone;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.gov.pay.connector.charge.model.telephone.PaymentOutcome;
 import uk.gov.pay.connector.charge.model.telephone.TelephoneChargeCreateRequest;
 import uk.gov.pay.connector.charge.service.ChargeService;
 
@@ -33,6 +34,8 @@ public class TelephonePaymentsResource {
         var request = builder.withAmount(1337L)
                 .withProviderId(telephonePaymentRequest.getStripeId())
                 .withReference("test telephone payment")
+                .withProcessorId("processor_id")
+                .withPaymentOutcome(new PaymentOutcome("payment_outcome"))
                 .build();
         chargeService.create(request, telephonePaymentRequest.getAccountId());
         return Response.ok().build();
