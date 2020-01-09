29ba44c0e179f47445fb72d2790ab659104816ee
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2020-01-09 11:53:01 +0000

    try with existing telephone payments infrastructure

--- src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentRequest.java
+++ src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentRequest.java
@@ -3,18 +3,26 @@ package uk.gov.pay.connector.telephone;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class TelephonePaymentRequest {
-    
+
+    @JsonProperty("account_id")
+    private Long accountId;
+
     @JsonProperty("stripe_id")
     private String stripeId;
 
     public TelephonePaymentRequest() {
     }
 
-    public TelephonePaymentRequest(String stripeChargeId) {
+    public TelephonePaymentRequest(Long accountId, String stripeChargeId) {
+        this.accountId = accountId;
         this.stripeId = stripeChargeId;
     }
 
     public String getStripeId() {
         return stripeId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
     }
 }
--- src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentsResource.java
+++ src/main/java/uk/gov/pay/connector/telephone/TelephonePaymentsResource.java
@@ -1,7 +1,10 @@
 package uk.gov.pay.connector.telephone;
 
+import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.gov.pay.connector.charge.model.telephone.TelephoneChargeCreateRequest;
+import uk.gov.pay.connector.charge.service.ChargeService;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -14,12 +17,24 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 public class TelephonePaymentsResource {
     
     private static final Logger logger = LoggerFactory.getLogger(TelephonePaymentsResource.class);
+    private ChargeService chargeService;
     
+    @Inject
+    TelephonePaymentsResource(ChargeService chargeService) {
+        this.chargeService = chargeService;
+    }
+
     @POST
     @Consumes(APPLICATION_JSON)
     @Path("/create-payment")
     public Response createPayment(TelephonePaymentRequest telephonePaymentRequest) {
         logger.info("Received request to create a telephone payment for Stripe id " + telephonePaymentRequest.getStripeId());
+        var builder = new TelephoneChargeCreateRequest.Builder();
+        var request = builder.withAmount(1337L)
+                .withProviderId(telephonePaymentRequest.getStripeId())
+                .withReference("test telephone payment")
+                .build();
+        chargeService.create(request, telephonePaymentRequest.getAccountId());
         return Response.ok().build();
     }
 }
