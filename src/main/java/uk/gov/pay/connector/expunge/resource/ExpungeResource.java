a1d433f35d9b75fd024cf97e1fa7b71879c8a5f0
Krish <krishna.bottla@gmail.com>
2020-02-05 13:34:56 +0000

    PP-6097 Add ExpungeResource
    - Added new resource ExpungeResource (POST /v1/tasks/expunge) which can be invoked by scheduler or manually to expunge charges of given batch size
    - `ChargeExpungeService` skeleton, which gets a charge to expunge and does nothing. Parity checking and expunging charge to be covered in PP-6098
    - Relevant tests

---
+++ src/main/java/uk/gov/pay/connector/expunge/resource/ExpungeResource.java
@@ -0,0 +1,33 @@
+package uk.gov.pay.connector.expunge.resource;
+
+import uk.gov.pay.connector.expunge.service.ChargeExpungeService;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
+import static javax.ws.rs.core.Response.status;
+
+@Path("/")
+public class ExpungeResource {
+
+    private ChargeExpungeService chargeExpungeService;
+
+    @Inject
+    public ExpungeResource(ChargeExpungeService chargeExpungeService) {
+        this.chargeExpungeService = chargeExpungeService;
+    }
+
+    @POST
+    @Path("/v1/tasks/expunge")
+    @Produces(APPLICATION_JSON)
+    public Response expungeCharges(@QueryParam("number_of_charges_to_expire") Integer noOfChargesToExpunge) {
+        chargeExpungeService.expunge(noOfChargesToExpunge);
+        return status(OK).build();
+    }
+}
