35a3ae4228e4c7758e7376b3deedc370507d83c1
Krish <krishna.bottla@gmail.com>
2020-04-15 10:58:48 +0100

    PP-6267 Add PayoutReconcileQueue
    - Added PayoutReconcileQueue which willbe used to interact (send/receive messages and other ops) with payout reconcile SQS queue
    - Added relevant tests

---
+++ src/main/java/uk/gov/pay/connector/queue/payout/PayoutReconcileMessage.java
@@ -0,0 +1,33 @@
+package uk.gov.pay.connector.queue.payout;
+
+import uk.gov.pay.connector.queue.QueueMessage;
+
+public class PayoutReconcileMessage {
+    private Payout payout;
+    private QueueMessage queueMessage;
+
+    private PayoutReconcileMessage(Payout payout, QueueMessage queueMessage) {
+        this.payout = payout;
+        this.queueMessage = queueMessage;
+    }
+
+    public static PayoutReconcileMessage of(Payout payout, QueueMessage queueMessage) {
+        return new PayoutReconcileMessage(payout, queueMessage);
+    }
+
+    public String getGatewayPayoutId() {
+        return payout.getGatewayPayoutId();
+    }
+
+    public String getConnectAccountId() {
+        return payout.getConnectAccountId();
+    }
+
+    public String getQueueMessageReceiptHandle() {
+        return queueMessage.getReceiptHandle();
+    }
+
+    public Object getQueueMessageId() {
+        return queueMessage.getMessageId();
+    }
+}
