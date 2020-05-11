326c4230b95bc6761f933d7bc094070e0d6f3399
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-05-11 18:28:08 +0100

    PP-6346: stripe transfer records pay transaction id for reconciliation
    Require a `charge` or `refund` external ID when making a Stripe
    transfer so that these can be used to simply and clearly reconcile
    transactions with bank payouts.

--- src/main/java/uk/gov/pay/connector/gateway/stripe/request/StripeTransferRequest.java
+++ src/main/java/uk/gov/pay/connector/gateway/stripe/request/StripeTransferRequest.java
@@ -9,17 +9,20 @@ import java.util.Map;
 public abstract class StripeTransferRequest extends StripeRequest {
     protected String amount;
     protected String stripeChargeId;
+    protected String reconciliationTransactionId;
 
     protected StripeTransferRequest(
             String amount,
             GatewayAccountEntity gatewayAccount,
             String stripeChargeId,
             String idempotencyKey,
-            StripeGatewayConfig stripeGatewayConfig
+            StripeGatewayConfig stripeGatewayConfig,
+            String reconciliationTransactionId
     ) {
         super(gatewayAccount, idempotencyKey, stripeGatewayConfig);
         this.stripeChargeId = stripeChargeId;
         this.amount = amount;
+        this.reconciliationTransactionId = reconciliationTransactionId;
     }
 
     public String urlPath() {
@@ -31,7 +34,8 @@ public abstract class StripeTransferRequest extends StripeRequest {
         return Map.of(
                 "amount", amount,
                 "currency", "GBP",
-                "metadata[stripe_charge_id]", stripeChargeId
+                "metadata[stripe_charge_id]", stripeChargeId,
+                "metadata[reconciliation_transaction_id]", reconciliationTransactionId
         );
     }
     
