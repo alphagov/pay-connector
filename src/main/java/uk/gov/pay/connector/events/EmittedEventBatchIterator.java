252e68a2e080b106a9c299c990ec512f6dcf492d
David Heath <david.heath@digital.cabinet-office.gov.uk>
2020-05-23 15:17:27 +0100

    Refactor iteration into separate iterator class
    The iteration over batches of events is somewhat complicated and entangled
    with other logic. By pulling it out into a separate iterator class we can
    more clearly see which parts of the logic have to do with iteration, counters,
    ranges etc.
    
    The remaining business logic of actually emitting the events is then much clearer to see.
    
    Some small bugs/issues were found during this refactoring due to the mockist
    style of testing. These will be cleaned up in a subsequent commit.

---
+++ src/main/java/uk/gov/pay/connector/events/EmittedEventBatchIterator.java
@@ -0,0 +1,124 @@
+package uk.gov.pay.connector.events;
+
+import uk.gov.pay.connector.app.config.EmittedEventSweepConfig;
+import uk.gov.pay.connector.events.dao.EmittedEventDao;
+
+import java.time.ZonedDateTime;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+class EmittedEventBatchIterator implements Iterator<EmittedEventBatchIterator.EventBatch> {
+    private final EmittedEventDao emittedEventDao;
+    private final EmittedEventSweepConfig sweepConfig;
+    private Long currentBatchStartId;
+    private final int batchSize;
+    private final ZonedDateTime batchStartTime;
+    private EventBatch currentBatch;
+    private Optional<Long> maybeMaximumIdOfEventsEligibleForReEmission;
+    private ZonedDateTime cutoffDate;
+
+    EmittedEventBatchIterator(EmittedEventDao emittedEventDao,
+                              EmittedEventSweepConfig sweepConfig,
+                              Long startId,
+                              int batchSize,
+                              ZonedDateTime batchStartTime) {
+        this.emittedEventDao = emittedEventDao;
+        this.sweepConfig = sweepConfig;
+        this.batchSize = batchSize;
+        this.batchStartTime = batchStartTime;
+        currentBatch = getFirstBatch(startId);
+    }
+    
+    @Override
+    public boolean hasNext() {
+        return currentBatch != null && (! currentBatch.isEmpty());
+    }
+
+    @Override
+    public EventBatch next() {
+        final EventBatch batchToReturn = currentBatch;
+
+        if ((!currentBatch.isEmpty()) && currentBatch.getEndId().isPresent()) {
+            currentBatch = getNextBatch(currentBatch.getEndId().get());
+        }
+
+        return batchToReturn;
+    }
+
+    Optional<Long> getMaximumIdOfEventsEligibleForReEmission() {
+        return maybeMaximumIdOfEventsEligibleForReEmission;
+    }
+    
+    Long getCurrentBatchStartId() {
+        return currentBatchStartId;
+    }
+
+    private EventBatch getFirstBatch(Long startFromId) {
+        cutoffDate = getCutoffDateForProcessingNotEmittedEvents(batchStartTime);
+        maybeMaximumIdOfEventsEligibleForReEmission = emittedEventDao.findNotEmittedEventMaxIdOlderThan(cutoffDate, batchStartTime);
+        return getNextBatch(startFromId);
+    }
+
+    private EventBatch getNextBatch(Long startFromId) {
+        final List<EmittedEventEntity> emittedEventEntities = maybeMaximumIdOfEventsEligibleForReEmission
+                .map(maxId ->
+                        emittedEventDao.findNotEmittedEventsOlderThan(cutoffDate, batchSize, startFromId, maxId, batchStartTime))
+                .orElse(List.of());
+        this.currentBatchStartId = startFromId;
+        return new EventBatch(emittedEventEntities, startFromId);
+    }
+
+    private ZonedDateTime getCutoffDateForProcessingNotEmittedEvents(ZonedDateTime batchStartTime) {
+        int notEmittedEventMaxAgeInSeconds = sweepConfig.getNotEmittedEventMaxAgeInSeconds();
+        return batchStartTime.minusSeconds(notEmittedEventMaxAgeInSeconds);
+    }
+    
+
+    public static class EventBatch {
+        private final List<EmittedEventEntity> events;
+        private final Long startId;
+
+        EventBatch(List<EmittedEventEntity> events, Long startId) {
+            this.events = events;
+            this.startId = startId;
+        }
+
+        public List<EmittedEventEntity> getEvents() {
+            return events;
+        }
+
+        public Long getStartId() {
+            return startId;
+        }
+
+        public Optional<Long> getEndId() {
+            return idOfLastInBatch();
+        }
+        
+        public boolean isEmpty() {
+            return events.isEmpty();
+        }
+
+        public Optional<ZonedDateTime> oldestEventDate() {
+            return events.stream()
+                    .map(EmittedEventEntity::getEventDate).min(ZonedDateTime::compareTo);
+        }
+        
+        public Optional<EmittedEventEntity> last() {
+            if (isEmpty()) {
+                return Optional.empty();
+            } else {
+                return Optional.of(events.get(events.size() - 1));
+            }
+        }
+        
+        public Optional<Long> idOfLastInBatch() {
+            return last().map(EmittedEventEntity::getId);
+        }
+        
+        public int getSize() {
+            return events.size();
+        }
+    }
+}
