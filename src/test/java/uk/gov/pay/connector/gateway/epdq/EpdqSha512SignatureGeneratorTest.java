55c1c93d447589b949830cb2b399840a58f52e8b
Alex Bishop <alexbishop1@users.noreply.github.com>
2020-07-01 13:50:53 +0100

    PP-6712 EpdqSha512SignatureGenerator: be smarter with streams
    Be smarter with streams in EpdqSha512SignatureGenerator to avoid
    both an intermediate list and an intermediate StringJoiner.

--- src/test/java/uk/gov/pay/connector/gateway/epdq/EpdqSha512SignatureGeneratorTest.java
+++ src/test/java/uk/gov/pay/connector/gateway/epdq/EpdqSha512SignatureGeneratorTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class EpdqSha512SignatureGeneratorTest {
 
