ad2a5197a55a13c5893ba738a055a3d03dbd0d90
Krish <krishna.bottla@gmail.com>
2020-09-03 18:22:56 +0100

    PP-7077 Get refunds from Ledger
    - Added service method to get refund transactions for payment from Ledger.
      This is going to be used to calculate refundability when refunds have been expunged from connector

---
+++ src/main/java/uk/gov/pay/connector/paritycheck/RefundTransactionsForPayment.java
@@ -0,0 +1,23 @@
+package uk.gov.pay.connector.paritycheck;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RefundTransactionsForPayment {
+
+    String parentTransactionId;
+    List<LedgerTransaction> transactions;
+
+    public String getParentTransactionId() {
+        return parentTransactionId;
+    }
+
+    public List<LedgerTransaction> getTransactions() {
+        return transactions;
+    }
+}
