9ab6be90bda7823af9fe3c7c8020ffcad088ae92
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2020-10-02 12:39:04 +0100

    PP-7208 add json ignore unknown properties - Adding this property to relevant classes ensures that no breakage or   downtime when introducing new fields to payload on other backend   systems.

--- src/main/java/uk/gov/pay/connector/client/ledger/model/CardDetails.java
+++ src/main/java/uk/gov/pay/connector/client/ledger/model/CardDetails.java
@@ -1,6 +1,7 @@
 package uk.gov.pay.connector.client.ledger.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -10,6 +11,7 @@ import java.util.Objects;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CardDetails {
 
     private String cardholderName;
