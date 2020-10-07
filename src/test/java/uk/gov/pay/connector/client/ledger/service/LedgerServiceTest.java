ec0cd92fa27e3a042141d85269435b60845f333e
Stephen Daly <stephencdaly@gmail.com>
2020-10-07 15:55:46 +0100

    PP-7253 Add consumer pact tests with ledger as provider
    Add configuration for running pact consumer tests.
    
    Replace unit tests that use templates with pact tests with ledger as the
    provider.

--- src/test/java/uk/gov/pay/connector/client/ledger/service/LedgerServiceTest.java
+++ src/test/java/uk/gov/pay/connector/client/ledger/service/LedgerServiceTest.java
@@ -1,7 +1,5 @@
 package uk.gov.pay.connector.client.ledger.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +7,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.pay.connector.app.ConnectorConfiguration;
 import uk.gov.pay.connector.client.ledger.exception.GetRefundsForPaymentException;
 import uk.gov.pay.connector.client.ledger.exception.LedgerException;
-import uk.gov.pay.connector.client.ledger.model.LedgerTransaction;
 import uk.gov.pay.connector.client.ledger.model.RefundTransactionsForPayment;
 
 import javax.ws.rs.ProcessingException;
@@ -18,27 +15,17 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
-import java.util.List;
-import java.util.Optional;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.http.HttpStatus.SC_OK;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static uk.gov.pay.connector.util.TestTemplateResourceLoader.LEDGER_GET_REFUNDS_FOR_PAYMENT;
-import static uk.gov.pay.connector.util.TestTemplateResourceLoader.LEDGER_PAYMENT_TRANSACTION;
-import static uk.gov.pay.connector.util.TestTemplateResourceLoader.LEDGER_REFUND_TRANSACTION;
-import static uk.gov.pay.connector.util.TestTemplateResourceLoader.load;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LedgerServiceTest {
-
-    private ObjectMapper objectMapper = new ObjectMapper();
+    
     private LedgerService ledgerService;
     private Response mockResponse;
 
@@ -58,65 +45,6 @@ public class LedgerServiceTest {
 
         when(mockResponse.getStatus()).thenReturn(SC_OK);
         ledgerService = new LedgerService(mockClient, mockConnectorConfiguration);
-    }
-
-    @Test
-    public void getTransaction_shouldSerialiseLedgerPaymentTransactionCorrectly() throws JsonProcessingException {
-        when(mockResponse.readEntity(LedgerTransaction.class)).thenReturn(objectMapper.readValue(load(LEDGER_PAYMENT_TRANSACTION), LedgerTransaction.class));
-
-        String externalId = "external-id";
-        Optional<LedgerTransaction> mayBeTransaction = ledgerService.getTransaction("external-id");
-
-        assertThat(mayBeTransaction.isPresent(), is(true));
-        LedgerTransaction transaction = mayBeTransaction.get();
-        assertThat(transaction.getTransactionId(), is(externalId));
-        assertThat(transaction.getAmount(), is(12000L));
-        assertThat(transaction.getGatewayAccountId(), is("3"));
-        assertThat(transaction.getExternalMetaData(), is(notNullValue()));
-    }
-
-    @Test
-    public void getTransaction_shouldSerialiseLedgerRefundTransactionCorrectly() throws JsonProcessingException {
-        when(mockResponse.readEntity(LedgerTransaction.class))
-                .thenReturn(objectMapper.readValue(load(LEDGER_REFUND_TRANSACTION), LedgerTransaction.class));
-
-        String externalId = "external-id";
-        Optional<LedgerTransaction> mayBeTransaction = ledgerService.getTransaction("external-id");
-
-        assertThat(mayBeTransaction.isPresent(), is(true));
-        LedgerTransaction transaction = mayBeTransaction.get();
-        assertThat(transaction.getTransactionId(), is(externalId));
-        assertThat(transaction.getAmount(), is(1000L));
-        assertThat(transaction.getGatewayTransactionId(), is("re_1H6zDTHj08j2jFuBtG6maiHK11"));
-        assertThat(transaction.getParentTransactionId(), is("64pcdagc9c13vgi7n904aio3n9"));
-        assertThat(transaction.getRefundedBy(), is("refunded-by-external-id"));
-        assertThat(transaction.getRefundedByUserEmail(), is("test@example.org"));
-        assertThat(transaction.getCreatedDate(), is("2020-07-20T13:39:38.940Z"));
-        assertThat(transaction.getState().getStatus(), is("success"));
-    }
-
-    @Test
-    public void getRefundsFromLedgerShouldSerialiseResponseCorrectly() throws JsonProcessingException {
-        when(mockResponse.readEntity(RefundTransactionsForPayment.class)).
-                thenReturn(objectMapper.readValue(load(LEDGER_GET_REFUNDS_FOR_PAYMENT), RefundTransactionsForPayment.class));
-
-        String externalId = "650516the13q5jpfo435f1m1fm";
-        RefundTransactionsForPayment refundTransactionsForPayment =
-                ledgerService.getRefundsForPayment(152L, externalId);
-
-        assertThat(refundTransactionsForPayment.getParentTransactionId(), is(externalId));
-
-        List<LedgerTransaction> transactions = refundTransactionsForPayment.getTransactions();
-
-        assertThat(transactions.get(0).getTransactionId(), is("nklfm1pk9flpu91j815kp2835o"));
-        assertThat(transactions.get(0).getGatewayAccountId(), is("152"));
-        assertThat(transactions.get(0).getAmount(), is(100L));
-        assertThat(transactions.get(0).getState().getStatus(), is("success"));
-
-        assertThat(transactions.get(1).getTransactionId(), is("migtkmlt6gvm16sim5h0p7oeje"));
-        assertThat(transactions.get(1).getAmount(), is(110L));
-        assertThat(transactions.get(1).getGatewayAccountId(), is("152"));
-        assertThat(transactions.get(1).getState().getStatus(), is("failed"));
     }
 
     @Test(expected = GetRefundsForPaymentException.class)
