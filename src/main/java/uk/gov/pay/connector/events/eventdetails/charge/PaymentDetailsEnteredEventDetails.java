041897dcbfd4a2370f218d454bd87a493ea73bbd
Krish <krishna.bottla@gmail.com>
2020-10-14 10:08:06 +0100

    PP-7276 Add UserEmailCollected pact state
    - Add new state for `user email collected` event
    - Also removed email from `payment details entered` event as email is included in separate event

--- src/main/java/uk/gov/pay/connector/events/eventdetails/charge/PaymentDetailsEnteredEventDetails.java
+++ src/main/java/uk/gov/pay/connector/events/eventdetails/charge/PaymentDetailsEnteredEventDetails.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 public class PaymentDetailsEnteredEventDetails extends EventDetails {
 
     private final Long corporateSurcharge;
-    private final String email;
     private final String cardType;
     private final String cardBrand;
     private final String cardBrandLabel;
@@ -37,7 +36,6 @@ public class PaymentDetailsEnteredEventDetails extends EventDetails {
     private PaymentDetailsEnteredEventDetails(Builder builder) {
 
         this.corporateSurcharge = builder.corporateSurcharge;
-        this.email = builder.email;
         this.cardType = builder.cardType;
         this.cardBrand = builder.cardBrand;
         this.cardBrandLabel = builder.cardBrandLabel;
@@ -59,7 +57,6 @@ public class PaymentDetailsEnteredEventDetails extends EventDetails {
 
     public static PaymentDetailsEnteredEventDetails from(ChargeEntity charge) {
         Builder builder = new Builder()
-                .withEmail(charge.getEmail())
                 .withGatewayTransactionId(charge.getGatewayTransactionId())
                 .withCorporateSurcharge(charge.getCorporateSurcharge().orElse(null))
                 .withTotalAmount(CorporateCardSurchargeCalculator.getTotalAmountFor(charge))
@@ -93,10 +90,6 @@ public class PaymentDetailsEnteredEventDetails extends EventDetails {
 
     public Long getCorporateSurcharge() {
         return corporateSurcharge;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public String getCardType() {
@@ -173,7 +166,6 @@ public class PaymentDetailsEnteredEventDetails extends EventDetails {
         if (o == null || getClass() != o.getClass()) return false;
         PaymentDetailsEnteredEventDetails that = (PaymentDetailsEnteredEventDetails) o;
         return Objects.equals(corporateSurcharge, that.corporateSurcharge) &&
-                Objects.equals(email, that.email) &&
                 Objects.equals(cardType, that.cardType) &&
                 Objects.equals(cardBrand, that.cardBrand) &&
                 Objects.equals(firstDigitsCardNumber, that.firstDigitsCardNumber) &&
@@ -194,14 +186,13 @@ public class PaymentDetailsEnteredEventDetails extends EventDetails {
 
     @Override
     public int hashCode() {
-        return Objects.hash(corporateSurcharge, email, cardType, cardBrand, firstDigitsCardNumber, lastDigitsCardNumber,
+        return Objects.hash(corporateSurcharge, cardType, cardBrand, firstDigitsCardNumber, lastDigitsCardNumber,
                 gatewayTransactionId, cardholderName, expiryDate, addressLine1, addressLine2, addressPostcode,
                 addressCounty, addressStateProvince, addressCountry, wallet, totalAmount);
     }
 
     private static class Builder {
         private Long corporateSurcharge;
-        private String email;
         private String cardType;
         private String cardBrand;
         private String cardBrandLabel;
@@ -222,11 +213,6 @@ public class PaymentDetailsEnteredEventDetails extends EventDetails {
 
         Builder withCorporateSurcharge(Long corporateSurcharge) {
             this.corporateSurcharge = corporateSurcharge;
-            return this;
-        }
-
-        Builder withEmail(String email) {
-            this.email = email;
             return this;
         }
 
