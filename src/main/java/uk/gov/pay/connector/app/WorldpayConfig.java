66fa2d38d8750b991c88755871909fddbc7f0d32
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2020-12-01 10:40:14 +0000

    PP-7417: Add Worldpay 3ds flex ddc url to yaml config

--- src/main/java/uk/gov/pay/connector/app/WorldpayConfig.java
+++ src/main/java/uk/gov/pay/connector/app/WorldpayConfig.java
@@ -3,10 +3,21 @@ package uk.gov.pay.connector.app;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
 public class WorldpayConfig extends GatewayConfig {
+    
     private boolean secureNotificationEnabled;
+    
     private String notificationDomain;
+    
     private ApplePayConfig applePayConfig;
+    
+    @Valid
+    @NotNull
+    private Map<String, String> threeDsFlexDdcUrls;
 
     public String getNotificationDomain() {
         return notificationDomain;
@@ -19,5 +30,9 @@ public class WorldpayConfig extends GatewayConfig {
     @JsonProperty("applePay")
     public ApplePayConfig getApplePayConfig() {
         return applePayConfig;
+    }
+
+    public Map<String, String> getThreeDsFlexDdcUrls() {
+        return threeDsFlexDdcUrls;
     }
 }
