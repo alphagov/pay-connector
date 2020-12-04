c33f8de7516bb4541eb2e134ebac970fdaf01fc4
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2020-12-04 16:35:20 +0000

    PP-7417: Validation for Worldpay 3DS Flex Jwt Mac Key parameter

---
+++ src/main/java/uk/gov/pay/connector/gatewayaccount/validation/ValidWorldpay3dsFlexJwtMacKey.java
@@ -0,0 +1,22 @@
+package uk.gov.pay.connector.gatewayaccount.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Target({FIELD, PARAMETER, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = Worldpay3dsFlexJwtMacKeyValidator.class)
+public @interface ValidWorldpay3dsFlexJwtMacKey {
+    String message() default "Field [jwt_mac_key] must be a UUID in its lowercase canonical representation";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+}
---
+++ src/main/java/uk/gov/pay/connector/gatewayaccount/validation/Worldpay3dsFlexJwtMacKeyValidator.java
@@ -0,0 +1,17 @@
+package uk.gov.pay.connector.gatewayaccount.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+public class Worldpay3dsFlexJwtMacKeyValidator implements ConstraintValidator<ValidWorldpay3dsFlexJwtMacKey, String> {
+
+    private static final Pattern LOWER_CASE_UUID = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}");
+    
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return isNotBlank(value) && LOWER_CASE_UUID.matcher(value).matches();
+    }
+}
