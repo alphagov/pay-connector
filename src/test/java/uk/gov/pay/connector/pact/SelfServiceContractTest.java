aaf5b50379d5da3c2b7a7be94542a1f68f2dd1fc
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2020-12-15 16:20:54 +0000

    PP-7422: Add mocked Worldpay DDC server for PACT
    This change adds mocked Worldpay Device Data Collection (DDC) server to Self Service Contract test. This will allow us to perform PACT test on Worldpay 3DS Flex credentials verification between Self Service and Connector applications.

--- src/test/java/uk/gov/pay/connector/pact/SelfServiceContractTest.java
+++ src/test/java/uk/gov/pay/connector/pact/SelfServiceContractTest.java
@@ -4,7 +4,14 @@ import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 
 @RunWith(PactRunner.class)
 @Provider("connector")
@@ -12,4 +19,11 @@ import org.junit.runner.RunWith;
         authentication = @PactBrokerAuth(username = "${PACT_BROKER_USERNAME}", password = "${PACT_BROKER_PASSWORD}"),
         consumers = {"selfservice"})
 public class SelfServiceContractTest extends ContractTest {
+    @ClassRule
+    public static WireMockClassRule worldpayDeviceDataCollectionRule = new WireMockClassRule(WORLDPAY_DDC_PORT_NUMBER);
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        worldpayDeviceDataCollectionRule.stubFor(post(urlPathEqualTo("/shopper/3ds/ddc.html")).willReturn(aResponse().withStatus(200)));
+    }
 }
