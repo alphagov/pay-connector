a75756d91405fca6bde5d57785d264eb7e098758
Alex Bishop <alexbishop1@users.noreply.github.com>
2021-01-11 14:51:50 +0000

    PP-7609 Add Gateway3dsExemptionResultObtained event
    Add event for whether or not our request for a 3D Secure exemption
    was honoured.
    
    with @nimalank7

---
+++ src/main/java/uk/gov/pay/connector/events/eventdetails/charge/Gateway3dsExemptionResultObtainedEventDetails.java
@@ -0,0 +1,25 @@
+package uk.gov.pay.connector.events.eventdetails.charge;
+
+import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
+import uk.gov.pay.connector.events.eventdetails.EventDetails;
+
+import java.util.Optional;
+
+public class Gateway3dsExemptionResultObtainedEventDetails extends EventDetails {
+
+    private final String exemption3ds;
+
+    public Gateway3dsExemptionResultObtainedEventDetails(String exemption3ds) {
+        this.exemption3ds = exemption3ds;
+    }
+
+    public static Gateway3dsExemptionResultObtainedEventDetails from(ChargeEntity charge) {
+        String exemption3ds = Optional.ofNullable(charge.getExemption3ds()).map(Enum::toString).orElse(null);
+        return new Gateway3dsExemptionResultObtainedEventDetails(exemption3ds);
+    }
+
+    public String getExemption3ds() {
+        return exemption3ds;
+    }
+
+}
