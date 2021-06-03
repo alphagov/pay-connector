9756e3c95bb6e01084177e3cbe080b89792d6f3f
Krish <krishna.bottla@gmail.com>
2021-06-03 16:37:39 +0100

    PP-8123 Set active_start_date for new & active credentials
    - For sandbox or stripe accounts credentials set to ACTIVE state, set `active_start_date` immediately.
    - For stripe credentials, if an active credentials record exists, set state to ENTERED - this is specific to switching PSP and the state to be set to ACTIVE when service switches PSP

--- src/main/java/uk/gov/pay/connector/gatewayaccountcredentials/dao/GatewayAccountCredentialsDao.java
+++ src/main/java/uk/gov/pay/connector/gatewayaccountcredentials/dao/GatewayAccountCredentialsDao.java
@@ -8,6 +8,8 @@ import uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCreden
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import static uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCredentialState.ACTIVE;
+
 @Transactional
 public class GatewayAccountCredentialsDao extends JpaDao<GatewayAccountCredentialsEntity> {
 
@@ -19,5 +21,19 @@ public class GatewayAccountCredentialsDao extends JpaDao<GatewayAccountCredentia
     @Override
     public void persist(GatewayAccountCredentialsEntity gatewayAccountCredentialsEntity) {
         super.persist(gatewayAccountCredentialsEntity);
+    }
+
+    public boolean hasActiveCredentials(Long gatewayAccountId) {
+        String query = "SELECT COUNT(gace) FROM GatewayAccountCredentialsEntity gace " +
+                " WHERE gace.gatewayAccountEntity.id = :gatewayAccountId AND gace.state = :state";
+
+        long count = (long) entityManager
+                .get()
+                .createQuery(query)
+                .setParameter("gatewayAccountId", gatewayAccountId)
+                .setParameter("state", ACTIVE)
+                .getSingleResult();
+
+        return count > 0;
     }
 }
--- src/main/java/uk/gov/pay/connector/gatewayaccountcredentials/service/GatewayAccountCredentialsService.java
+++ src/main/java/uk/gov/pay/connector/gatewayaccountcredentials/service/GatewayAccountCredentialsService.java
@@ -8,12 +8,14 @@ import uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCreden
 import uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCredentialsEntity;
 
 import javax.inject.Inject;
+import java.time.Instant;
 import java.util.Map;
 
 import static uk.gov.pay.connector.gateway.PaymentGatewayName.SANDBOX;
 import static uk.gov.pay.connector.gateway.PaymentGatewayName.STRIPE;
 import static uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCredentialState.ACTIVE;
 import static uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCredentialState.CREATED;
+import static uk.gov.pay.connector.gatewayaccountcredentials.model.GatewayAccountCredentialState.ENTERED;
 
 public class GatewayAccountCredentialsService {
 
@@ -27,20 +29,33 @@ public class GatewayAccountCredentialsService {
     @Transactional
     public void createGatewayAccountCredentials(GatewayAccountEntity gatewayAccountEntity, String paymentProvider,
                                                 Map<String, String> credentials) {
-        GatewayAccountCredentialState state = calculateState(paymentProvider, credentials);
-        GatewayAccountCredentialsEntity gatewayAccountCredentialsEntity
-                = new GatewayAccountCredentialsEntity(gatewayAccountEntity, paymentProvider, credentials, state);
+        GatewayAccountCredentialState state = calculateState(gatewayAccountEntity, paymentProvider, credentials);
+        GatewayAccountCredentialsEntity gatewayAccountCredentialsEntity;
+
+        if (state == ACTIVE) {
+            gatewayAccountCredentialsEntity
+                    = new GatewayAccountCredentialsEntity(gatewayAccountEntity, paymentProvider, credentials,
+                    state, Instant.now());
+        } else {
+            gatewayAccountCredentialsEntity
+                    = new GatewayAccountCredentialsEntity(gatewayAccountEntity, paymentProvider, credentials, state);
+        }
 
         gatewayAccountCredentialsDao.persist(gatewayAccountCredentialsEntity);
     }
 
-    private GatewayAccountCredentialState calculateState(String paymentProvider, Map<String, String> credentials) {
+    private GatewayAccountCredentialState calculateState(GatewayAccountEntity gatewayAccountEntity,
+                                                         String paymentProvider, Map<String, String> credentials) {
         PaymentGatewayName paymentGatewayName = PaymentGatewayName.valueFrom(paymentProvider);
         if (paymentGatewayName == SANDBOX) {
             return ACTIVE;
         }
-        // todo: state should be ENTERED if another active credentials record exists (when switching PSP)
         if (paymentGatewayName == STRIPE && !credentials.isEmpty()) {
+            boolean hasActiveCredentials =
+                    gatewayAccountCredentialsDao.hasActiveCredentials(gatewayAccountEntity.getId());
+            if (hasActiveCredentials){
+                return ENTERED;
+            }
             return ACTIVE;
         }
 
