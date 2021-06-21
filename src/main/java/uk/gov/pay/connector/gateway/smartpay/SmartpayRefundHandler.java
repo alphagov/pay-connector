ee88a5ad6429f0db984fe74a593d544b89393527
Krish <krishna.bottla@gmail.com>
2021-06-21 16:35:44 +0100

    PP-8125 Smartpay refunds - Get credentials by payment provider

--- src/main/java/uk/gov/pay/connector/gateway/smartpay/SmartpayRefundHandler.java
+++ src/main/java/uk/gov/pay/connector/gateway/smartpay/SmartpayRefundHandler.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.util.Map;
 
 import static uk.gov.pay.connector.gateway.GatewayResponseUnmarshaller.unmarshallResponse;
+import static uk.gov.pay.connector.gateway.PaymentGatewayName.SMARTPAY;
 import static uk.gov.pay.connector.gateway.model.response.GatewayRefundResponse.RefundState.PENDING;
 import static uk.gov.pay.connector.gateway.util.AuthUtil.getGatewayAccountCredentialsAsAuthHeader;
 import static uk.gov.pay.connector.gatewayaccount.model.GatewayAccount.CREDENTIALS_MERCHANT_ID;
@@ -33,7 +34,7 @@ public class SmartpayRefundHandler implements RefundHandler {
                     gatewayUrlMap.get(request.getGatewayAccount().getType()), 
                     request.getGatewayAccount(), 
                     buildRefundOrderFor(request),
-                    getGatewayAccountCredentialsAsAuthHeader(request.getGatewayAccount().getCredentials()));
+                    getGatewayAccountCredentialsAsAuthHeader(request.getGatewayAccount().getCredentials(SMARTPAY.getName())));
             return GatewayRefundResponse.fromBaseRefundResponse(unmarshallResponse(response, SmartpayRefundResponse.class), PENDING);
         } catch (GatewayException e) {
             return GatewayRefundResponse.fromGatewayError(e.toGatewayError());
@@ -50,6 +51,6 @@ public class SmartpayRefundHandler implements RefundHandler {
     }
 
     private String getMerchantCode(GatewayRequest request) {
-        return request.getGatewayAccount().getCredentials().get(CREDENTIALS_MERCHANT_ID);
+        return request.getGatewayAccount().getCredentials(SMARTPAY.getName()).get(CREDENTIALS_MERCHANT_ID);
     }
 }
