a09429b5de9bf0dce654e21f33a0f3d9aa57ff98
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2021-09-06 16:26:00 +0100

    add additional test assertions

--- src/test/java/uk/gov/pay/connector/it/events/StateTransitionsIT.java
+++ src/test/java/uk/gov/pay/connector/it/events/StateTransitionsIT.java
@@ -125,6 +125,8 @@ public class StateTransitionsIT extends ChargingITestBase {
                 .filter(m -> "REFUND_AVAILABILITY_UPDATED".equals(m.get("event_type").getAsString()))
                 .findFirst().get();
         assertThat(message2.get("event_type").getAsString(), is("REFUND_AVAILABILITY_UPDATED"));
+        assertThat(message2.get("service_id").getAsString(), is("external-service-id"));
+        assertThat(message2.get("live").getAsBoolean(), is(false));
         assertThat(message2.get("resource_external_id").getAsString(), is(chargeId));
         assertThat(message2.get("event_details").getAsJsonObject().get("refund_amount_available").getAsInt(), is(6184));
         assertThat(message2.get("event_details").getAsJsonObject().get("refund_amount_refunded").getAsInt(), is(50));
