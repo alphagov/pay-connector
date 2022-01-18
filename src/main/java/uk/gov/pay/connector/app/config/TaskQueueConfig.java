723daa731cdf24516e909344e966a1aec7ec8c7f
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2022-01-18 16:45:07 +0000

    PP-9036 remove unused env var
    - remove collectFeeForStripeFailedPayments env var

--- src/main/java/uk/gov/pay/connector/app/config/TaskQueueConfig.java
+++ src/main/java/uk/gov/pay/connector/app/config/TaskQueueConfig.java
@@ -5,17 +5,12 @@ import io.dropwizard.Configuration;
 public class TaskQueueConfig extends Configuration {
 
     private Boolean taskQueueEnabled;
-    private Boolean collectFeeForStripeFailedPayments;
     private int queueSchedulerNumberOfThreads;
     private int queueSchedulerThreadDelayInSeconds;
     private int failedMessageRetryDelayInSeconds;
 
     public Boolean getTaskQueueEnabled() {
         return taskQueueEnabled;
-    }
-
-    public Boolean getCollectFeeForStripeFailedPayments() {
-        return collectFeeForStripeFailedPayments;
     }
 
     public int getQueueSchedulerNumberOfThreads() {
