d2078da2e249b2a495ef63dd922530f4d74f8b5a
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2022-04-26 14:36:19 +0100

    PP-9145 Build AuthCardDetails to authorise moto api payment
    - Build AuthCardDetails from AuthoriseRequest, CardInfo and other details available to  authorise moto api payments.

---
+++ src/test/java/uk/gov/pay/connector/gateway/model/AuthCardDetailsTest.java
@@ -0,0 +1,66 @@
+package uk.gov.pay.connector.gateway.model;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.pay.connector.charge.model.AddressEntity;
+import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
+import uk.gov.pay.connector.charge.model.domain.ChargeEntityFixture;
+import uk.gov.pay.connector.client.cardid.model.CardInformation;
+import uk.gov.pay.connector.client.cardid.model.CardInformationFixture;
+import uk.gov.pay.connector.client.cardid.model.CardidCardType;
+import uk.gov.pay.connector.common.model.domain.Address;
+import uk.gov.pay.connector.paymentprocessor.model.AuthoriseRequest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static uk.gov.pay.connector.model.domain.AuthCardDetailsFixture.anAuthCardDetails;
+
+class AuthCardDetailsTest {
+
+    private final CardInformation cardInformation = CardInformationFixture.aCardInformation().build();
+    private final AuthoriseRequest authoriseRequest = new AuthoriseRequest("one-time-token", "4242424242424242", "123", "11/99", "Joe");
+
+    @Test
+    void shouldReturnAuthCardDetailsWithCardAndAddressDetail() {
+        ChargeEntity chargeEntity = ChargeEntityFixture.aValidChargeEntity()
+                .withCardDetails(anAuthCardDetails().getCardDetailsEntity())
+                .build();
+
+        AuthCardDetails authCardDetails = AuthCardDetails.of(authoriseRequest, chargeEntity, cardInformation);
+
+        assertAuthCardDetails(authCardDetails, cardInformation);
+        assertThat(authCardDetails.getAddress().isPresent(), is(true));
+
+        Address address = authCardDetails.getAddress().get();
+        AddressEntity addressEntity = chargeEntity.getCardDetails().getBillingAddress().get();
+
+        assertThat(address.getLine1(), is(addressEntity.getLine1()));
+        assertThat(address.getLine2(), is(addressEntity.getLine2()));
+        assertThat(address.getCity(), is(addressEntity.getCity()));
+        assertThat(address.getCounty(), is(addressEntity.getCounty()));
+        assertThat(address.getPostcode(), is(addressEntity.getPostcode()));
+        assertThat(address.getCountry(), is(addressEntity.getCountry()));
+    }
+
+    @Test
+    void shouldReturnAuthCardDetailsWithOutAddress() {
+        ChargeEntity chargeEntity = ChargeEntityFixture.aValidChargeEntity()
+                .build();
+
+        AuthCardDetails authCardDetails = AuthCardDetails.of(authoriseRequest, chargeEntity, cardInformation);
+
+        assertAuthCardDetails(authCardDetails, cardInformation);
+        assertThat(authCardDetails.getAddress().isPresent(), is(false));
+    }
+
+    private void assertAuthCardDetails(AuthCardDetails authCardDetails, CardInformation cardInformation) {
+        assertThat(authCardDetails.getCardNo(), is("4242424242424242"));
+        assertThat(authCardDetails.getCardHolder(), is("Joe"));
+        assertThat(authCardDetails.getEndDate().toString(), is("11/99"));
+        assertThat(authCardDetails.getCvc(), is("123"));
+
+        assertThat(authCardDetails.getCardBrand(), is(cardInformation.getBrand()));
+        assertThat(authCardDetails.isCorporateCard(), is(cardInformation.isCorporate()));
+        assertThat(authCardDetails.getPayersCardType(), is(CardidCardType.toPayersCardType(cardInformation.getType())));
+        assertThat(authCardDetails.getPayersCardPrepaidStatus(), is(cardInformation.getPrepaidStatus()));
+    }
+}
