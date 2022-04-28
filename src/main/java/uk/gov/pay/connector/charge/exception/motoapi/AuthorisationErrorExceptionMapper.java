b0b9353e48c508d19e12ce5a0d16e228a54d2f1d
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2022-04-28 09:24:37 +0100

    PP-9145 Add new exception for moto api
    - Added new exceptions to map authorisation error/rejected response

---
+++ src/main/java/uk/gov/pay/connector/charge/exception/motoapi/AuthorisationErrorExceptionMapper.java
@@ -0,0 +1,22 @@
+package uk.gov.pay.connector.charge.exception.motoapi;
+
+import uk.gov.pay.connector.common.model.api.ErrorResponse;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static uk.gov.service.payments.commons.model.ErrorIdentifier.AUTHORISATION_ERROR;
+
+public class AuthorisationErrorExceptionMapper implements ExceptionMapper<AuthorisationErrorException> {
+    
+    @Override
+    public Response toResponse(AuthorisationErrorException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(AUTHORISATION_ERROR, exception.getMessage());
+
+        return Response.status(402)
+                .entity(errorResponse)
+                .type(APPLICATION_JSON)
+                .build();
+    }
+}
