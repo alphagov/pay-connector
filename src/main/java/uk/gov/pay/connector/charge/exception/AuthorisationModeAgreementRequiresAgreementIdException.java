a0a7a66665755fe7d40edac47289b2ba2abe432a
Alex Bishop <alexbishop1@users.noreply.github.com>
2022-08-12 17:38:51 +0100

    PP-9498 Use correct error identifier for incorrect auth mode
    When we receive a create charge request containing
    "authorisation_mode": "agreement",
    "save_payment_instrument_to_agreement": true and an "agreement_id",
    we return a 400 error (because
    "save_payment_instrument_to_agreement": true cannot be used with
    "authorisation_mode": "agreement").
    
    This 400 response should have the error identifier
    INCORRECT_AUTHORISATION_MODE_FOR_SAVE_PAYMENT_INSTRUMENT_TO_AGREEMENT
    but it was actually using GENERIC because the
    IncorrectAuthorisationModeForSavePaymentToAgreementException
    constructor was directly calling
    RequestUtil.badRequestResponse(String) — which always constructs
    a 400 response with a GENERIC error identifier — rather than
    allowing the registered exception mapper for
    IncorrectAuthorisationModeForSavePaymentToAgreementException to do
    its thing.
    
    In addition to fixing
    IncorrectAuthorisationModeForSavePaymentToAgreementException, also
    fix a couple of other exceptions that directly called
    RequestUtil.badRequestResponse(String) even though they have
    registered exception mappers.

--- src/main/java/uk/gov/pay/connector/charge/exception/AuthorisationModeAgreementRequiresAgreementIdException.java
+++ src/main/java/uk/gov/pay/connector/charge/exception/AuthorisationModeAgreementRequiresAgreementIdException.java
@@ -2,12 +2,10 @@ package uk.gov.pay.connector.charge.exception;
 
 import javax.ws.rs.WebApplicationException;
 
-import static uk.gov.pay.connector.util.ResponseUtil.badRequestResponse;
-
 public class AuthorisationModeAgreementRequiresAgreementIdException extends WebApplicationException {
 
     public AuthorisationModeAgreementRequiresAgreementIdException(String message) {
-        super(badRequestResponse(message));
+        super(message);
     }
 
 }
--- src/main/java/uk/gov/pay/connector/charge/exception/IncorrectAuthorisationModeForSavePaymentToAgreementException.java
+++ src/main/java/uk/gov/pay/connector/charge/exception/IncorrectAuthorisationModeForSavePaymentToAgreementException.java
@@ -2,12 +2,10 @@ package uk.gov.pay.connector.charge.exception;
 
 import javax.ws.rs.WebApplicationException;
 
-import static uk.gov.pay.connector.util.ResponseUtil.badRequestResponse;
-
 public class IncorrectAuthorisationModeForSavePaymentToAgreementException extends WebApplicationException {
 
     public IncorrectAuthorisationModeForSavePaymentToAgreementException() {
-        super(badRequestResponse("If [save_payment_instrument_to_agreement] is true, [authorisation_mode] must be [web]"));
+        super("If [save_payment_instrument_to_agreement] is true, [authorisation_mode] must be [web]");
     }
 
 }
