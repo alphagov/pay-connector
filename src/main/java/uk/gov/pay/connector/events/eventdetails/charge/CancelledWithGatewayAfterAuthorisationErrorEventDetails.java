92c0100f161ba503e078c974438f0869a225a492
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2022-09-22 15:26:01 +0100

    PP-10024 fix missing gateway transaction id
    - When we try to cancel some Stripe charges, the gatewayTransactionId might be misisng from the charge.
      Set gatewayTransactionId on charge after querying Stripe for the payment intent.
    - Use payment intent id to instanciate StripeQueryResponse, instead of our externalid
    - update / add relevant tests

---
+++ src/main/java/uk/gov/pay/connector/events/eventdetails/charge/CancelledWithGatewayAfterAuthorisationErrorEventDetails.java
@@ -0,0 +1,21 @@
+package uk.gov.pay.connector.events.eventdetails.charge;
+
+import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
+import uk.gov.pay.connector.events.eventdetails.EventDetails;
+
+public class CancelledWithGatewayAfterAuthorisationErrorEventDetails extends EventDetails {
+
+    private String gatewayTransactionId;
+
+    public CancelledWithGatewayAfterAuthorisationErrorEventDetails(String gatewayTransactionId) {
+        this.gatewayTransactionId = gatewayTransactionId;
+    }
+    
+    public static CancelledWithGatewayAfterAuthorisationErrorEventDetails from(ChargeEntity charge) {
+        return new CancelledWithGatewayAfterAuthorisationErrorEventDetails(charge.getGatewayTransactionId());
+    }
+
+    public String getGatewayTransactionId() {
+        return gatewayTransactionId;
+    }
+}
--- src/main/java/uk/gov/pay/connector/events/model/charge/CancelledWithGatewayAfterAuthorisationError.java
+++ src/main/java/uk/gov/pay/connector/events/model/charge/CancelledWithGatewayAfterAuthorisationError.java
@@ -1,9 +1,27 @@
 package uk.gov.pay.connector.events.model.charge;
 
+import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
+import uk.gov.pay.connector.chargeevent.model.domain.ChargeEventEntity;
+import uk.gov.pay.connector.events.eventdetails.charge.CancelledWithGatewayAfterAuthorisationErrorEventDetails;
+
 import java.time.ZonedDateTime;
 
-public class CancelledWithGatewayAfterAuthorisationError extends PaymentEventWithoutDetails {
-    public CancelledWithGatewayAfterAuthorisationError(String serviceId, boolean live, String resourceExternalId, ZonedDateTime timestamp) {
-        super(serviceId, live, resourceExternalId, timestamp);
+public class CancelledWithGatewayAfterAuthorisationError extends PaymentEvent {
+    public CancelledWithGatewayAfterAuthorisationError(String serviceId, boolean live, String resourceExternalId,
+                                                       CancelledWithGatewayAfterAuthorisationErrorEventDetails eventDetails, 
+                                                       ZonedDateTime timestamp) {
+        super(serviceId, live, resourceExternalId, eventDetails, timestamp);
+    }
+    
+    public static CancelledWithGatewayAfterAuthorisationError from(ChargeEventEntity chargeEvent) {
+        ChargeEntity charge = chargeEvent.getChargeEntity();
+        
+        return new CancelledWithGatewayAfterAuthorisationError(
+                charge.getServiceId(),
+                charge.getGatewayAccount().isLive(),
+                charge.getExternalId(),
+                CancelledWithGatewayAfterAuthorisationErrorEventDetails.from(charge),
+                chargeEvent.getUpdated()
+        );
     }
 }
