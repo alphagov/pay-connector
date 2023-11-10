029a2162924f485fa44c8913e73bef30ee130ced
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2023-11-10 09:36:39 +0000

    remove prometheus metrics

--- src/main/java/uk/gov/pay/connector/queue/tasks/handlers/ServiceArchivedTaskHandler.java
+++ src/main/java/uk/gov/pay/connector/queue/tasks/handlers/ServiceArchivedTaskHandler.java
@@ -1,6 +1,5 @@
 package uk.gov.pay.connector.queue.tasks.handlers;
 
-import io.prometheus.client.Histogram;
 import uk.gov.pay.connector.gatewayaccount.service.GatewayAccountService;
 import uk.gov.pay.connector.queue.tasks.model.ServiceArchivedTaskData;
 
@@ -9,24 +8,12 @@ import javax.inject.Inject;
 public class ServiceArchivedTaskHandler {
     private final GatewayAccountService gatewayAccountService;
 
-    private static final Histogram duration = Histogram.build()
-            .name("disable_gateway_accounts_and_credentials_job_duration_seconds")
-            .help("Duration of disabling gateway accounts and deleting/redacting credentials job in seconds")
-            .unit("seconds")
-            .register();
-
     @Inject
     public ServiceArchivedTaskHandler(GatewayAccountService gatewayAccountService) {
         this.gatewayAccountService = gatewayAccountService;
     }
 
     public void process(ServiceArchivedTaskData serviceArchivedTaskData) {
-        try (Histogram.Timer responseTimeTimer = duration.startTimer()) {
-            try {
-                gatewayAccountService.disableAccountsAndRedactOrDeleteCredentials(serviceArchivedTaskData.getServiceId());
-            } finally {
-                responseTimeTimer.observeDuration();
-            }
-        }
+        gatewayAccountService.disableAccountsAndRedactOrDeleteCredentials(serviceArchivedTaskData.getServiceId());
     }
 }
