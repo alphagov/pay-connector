3baa0997e49eec1d71d1e0c078c466307a81a14f
AbdirizakIdris <abzak7@hotmail.co.uk>
2023-12-18 16:27:56 +0000

    PP-11553 Create reversal service for fixing stripe refund  - Add method for getting refund from Connector and Ledger with refund Id

---
+++ src/test/java/uk/gov/pay/connector/service/RefundReversalServiceTest.java
@@ -0,0 +1,82 @@
+package uk.gov.pay.connector.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.pay.connector.client.ledger.model.LedgerTransaction;
+import uk.gov.pay.connector.client.ledger.service.LedgerService;
+import uk.gov.pay.connector.common.model.api.ExternalRefundStatus;
+import uk.gov.pay.connector.refund.dao.RefundDao;
+import uk.gov.pay.connector.refund.model.domain.Refund;
+import uk.gov.pay.connector.refund.model.domain.RefundEntity;
+import uk.gov.pay.connector.refund.service.RefundReversalService;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static uk.gov.pay.connector.model.domain.LedgerTransactionFixture.aValidLedgerTransaction;
+import static uk.gov.pay.connector.model.domain.RefundEntityFixture.aValidRefundEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class RefundReversalServiceTest {
+    private RefundReversalService refundReversalService;
+
+    private final String refundExternalId = "refund123";
+
+    @Mock
+    private RefundDao mockRefundDao;
+    @Mock
+    private LedgerService mockLedgerService;
+
+    @BeforeEach
+    void setUp() {
+        refundReversalService = new RefundReversalService(mockLedgerService, mockRefundDao);
+    }
+
+    @Test
+    void shouldFindRefundInLedger() {
+
+        when(mockRefundDao.findByExternalId(refundExternalId)).thenReturn(Optional.empty());
+
+        LedgerTransaction ledgerTransaction = aValidLedgerTransaction()
+                .withExternalId(refundExternalId)
+                .withStatus(ExternalRefundStatus.EXTERNAL_SUBMITTED.getStatus())
+                .build();
+        when(mockLedgerService.getTransaction(refundExternalId)).thenReturn(Optional.of(ledgerTransaction));
+
+        Optional<Refund> result = refundReversalService.findMaybeHistoricRefundByRefundId(refundExternalId);
+
+        assertThat(result.isPresent(), is(true));
+        assertThat(result.get().getExternalId(), is(refundExternalId));
+
+        verifyNoMoreInteractions(mockRefundDao);
+    }
+
+    @Test
+    void shouldFindRefundInConnector() {
+        RefundEntity refundEntityInConnector = aValidRefundEntity().withExternalId(refundExternalId).build();
+
+        when(mockRefundDao.findByExternalId(refundExternalId)).thenReturn(Optional.of(refundEntityInConnector));
+
+        Optional<Refund> result = refundReversalService.findMaybeHistoricRefundByRefundId(refundExternalId);
+
+        assertThat(result.isPresent(), is(true));
+        assertThat(result.get().getExternalId(), is(refundExternalId));
+
+        verifyNoMoreInteractions(mockLedgerService);
+    }
+    @Test
+    void shouldNotFindRefund() {
+        when(mockRefundDao.findByExternalId(refundExternalId)).thenReturn(Optional.empty());
+        when(mockLedgerService.getTransaction(refundExternalId)).thenReturn(Optional.empty());
+
+        Optional<Refund> result = refundReversalService.findMaybeHistoricRefundByRefundId(refundExternalId);
+
+        assertThat(result.isPresent(), is(false));
+    }
+}
