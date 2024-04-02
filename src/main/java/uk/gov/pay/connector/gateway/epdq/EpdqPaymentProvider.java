0de9431de0e5bcd490c894bb24c508fec4c38cec
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-04-02 16:29:39 +0100

    PP-12062: Delete Epdq functions
    It's not possible to authorise, capture or cancel a payment so these functions can go.

--- src/main/java/uk/gov/pay/connector/gateway/epdq/EpdqPaymentProvider.java
+++ src/main/java/uk/gov/pay/connector/gateway/epdq/EpdqPaymentProvider.java
@@ -1,17 +1,12 @@
 package uk.gov.pay.connector.gateway.epdq;
 
-import io.dropwizard.setup.Environment;
-import uk.gov.pay.connector.app.ConnectorConfiguration;
 import uk.gov.pay.connector.charge.model.domain.Charge;
 import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
 import uk.gov.pay.connector.common.model.api.ExternalChargeRefundAvailability;
 import uk.gov.pay.connector.gateway.CaptureResponse;
 import uk.gov.pay.connector.gateway.ChargeQueryGatewayRequest;
 import uk.gov.pay.connector.gateway.ChargeQueryResponse;
-import uk.gov.pay.connector.gateway.GatewayClient;
-import uk.gov.pay.connector.gateway.GatewayClientFactory;
 import uk.gov.pay.connector.gateway.GatewayException;
-import uk.gov.pay.connector.gateway.GatewayException.GatewayErrorException;
 import uk.gov.pay.connector.gateway.PaymentGatewayName;
 import uk.gov.pay.connector.gateway.PaymentProvider;
 import uk.gov.pay.connector.gateway.model.AuthCardDetails;
@@ -22,7 +17,6 @@ import uk.gov.pay.connector.gateway.model.request.CardAuthorisationGatewayReques
 import uk.gov.pay.connector.gateway.model.request.RefundGatewayRequest;
 import uk.gov.pay.connector.gateway.model.response.BaseAuthoriseResponse;
 import uk.gov.pay.connector.gateway.model.response.BaseCancelResponse;
-import uk.gov.pay.connector.gateway.model.response.BaseResponse;
 import uk.gov.pay.connector.gateway.model.response.Gateway3DSAuthorisationResponse;
 import uk.gov.pay.connector.gateway.model.response.GatewayRefundResponse;
 import uk.gov.pay.connector.gateway.model.response.GatewayResponse;
@@ -30,21 +24,15 @@ import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
 import uk.gov.pay.connector.refund.model.domain.Refund;
 
 import javax.inject.Inject;
-import java.time.Clock;
 import java.util.List;
 import java.util.Optional;
 
-import static uk.gov.pay.connector.gateway.GatewayResponseUnmarshaller.unmarshallResponse;
-
 public class EpdqPaymentProvider implements PaymentProvider {
 
-    public static final String ROUTE_FOR_NEW_ORDER = "orderdirect.asp";
     public static final String ROUTE_FOR_MAINTENANCE_ORDER = "maintenancedirect.asp";
-    public static final String ROUTE_FOR_QUERY_ORDER = "querydirect.asp";
     
     @Inject
-    public EpdqPaymentProvider(ConnectorConfiguration configuration, GatewayClientFactory gatewayClientFactory, Environment environment, Clock clock) {
-    }
+    public EpdqPaymentProvider() {}
 
     @Override
     public PaymentGatewayName getPaymentGatewayName() {
@@ -60,19 +48,7 @@ public class EpdqPaymentProvider implements PaymentProvider {
     public GatewayResponse<BaseAuthoriseResponse> authorise(CardAuthorisationGatewayRequest request, ChargeEntity charge) throws GatewayException {
         throw new UnsupportedOperationException();
     }
-
-    private static GatewayResponse getEpdqGatewayResponse(GatewayClient.Response response, Class<? extends BaseResponse> responseClass) throws GatewayErrorException {
-        var responseBuilder = GatewayResponse.GatewayResponseBuilder.responseBuilder();
-        responseBuilder.withResponse(unmarshallResponse(response, responseClass));
-        return responseBuilder.build();
-    }
-
-    private static GatewayResponse getUninterpretedEpdqGatewayResponse(GatewayClient.Response response, Class<? extends BaseResponse> responseClass) throws GatewayErrorException {
-        var responseBuilder = GatewayResponse.GatewayResponseBuilder.responseBuilder();
-        responseBuilder.withResponse(unmarshallResponse(response, responseClass));
-        return responseBuilder.buildUninterpreted();
-    }
-
+    
     @Override
     public GatewayResponse authoriseMotoApi(CardAuthorisationGatewayRequest request) {
         throw new UnsupportedOperationException("MOTO API payments are not supported for ePDQ");
