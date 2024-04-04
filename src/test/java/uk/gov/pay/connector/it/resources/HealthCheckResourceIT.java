e9c9891c884ecd9dd1586fc93efcb61207554283
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2024-04-04 11:45:20 +0100

    PP-12302 refactor HealthCheckResourceIT
    - refactor HealthCheckResourceIT to check healthy states
    - add mocked HealthCheckResourceTest to check unhealthy states

--- src/test/java/uk/gov/pay/connector/it/resources/HealthCheckResourceIT.java
+++ src/test/java/uk/gov/pay/connector/it/resources/HealthCheckResourceIT.java
@@ -1,32 +1,28 @@
 package uk.gov.pay.connector.it.resources;
 
-import org.junit.Rule;
-import org.junit.Test;
-import uk.gov.pay.connector.app.ConnectorApp;
-import uk.gov.pay.connector.app.ConnectorConfiguration;
-import uk.gov.pay.connector.rules.DropwizardAppRule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import uk.gov.pay.connector.it.base.ITestBaseExtension;
 
-import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
 public class HealthCheckResourceIT {
+    @RegisterExtension
+    public static ITestBaseExtension app = new ITestBaseExtension("sandbox");
 
-    @Rule
-    public DropwizardAppRule<ConnectorConfiguration> app = new DropwizardAppRule<>(
-            ConnectorApp.class, resourceFilePath("config/test-it-config.yaml"));
 
     @Test
-    public void healthcheckShouldIdentifyUnhealthyDBAndQueue() {
+    void healthcheckShouldIdentifyHealthyDBAndQueue() {
         given().port(app.getLocalPort())
                 .contentType(JSON)
                 .get("healthcheck")
                 .then()
-                .statusCode(503)
+                .statusCode(200)
                 .body("ping.healthy", is(true))
-                .body("database.healthy", is(false))
+                .body("database.healthy", is(true))
                 .body("deadlocks.healthy", is(true))
-                .body("sqsQueue.healthy", is(false));
+                .body("sqsQueue.healthy", is(true));
     }
 }
