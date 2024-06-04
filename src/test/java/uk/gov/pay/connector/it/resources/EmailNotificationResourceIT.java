4e795a7b4f2de130f42110515e48dd55d6ed0c22
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2024-06-04 14:20:52 +0100

    PP-12355 Add PATCH email notification by service ID and type endpoint (#5311)
    * PP-12355 Add PATCH /v1/api/service/{serviceId}/account/[test|live]/email-notification endpoint

--- src/test/java/uk/gov/pay/connector/it/resources/EmailNotificationResourceIT.java
+++ src/test/java/uk/gov/pay/connector/it/resources/EmailNotificationResourceIT.java
@@ -13,11 +13,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.http.ContentType.JSON;
+import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
+import static uk.gov.pay.connector.gatewayaccount.model.GatewayAccountType.TEST;
 import static uk.gov.pay.connector.it.resources.GatewayAccountResourceITBaseExtensions.ACCOUNTS_API_URL;
 import static uk.gov.pay.connector.util.JsonEncoder.toJson;
 
@@ -169,7 +171,144 @@ public class EmailNotificationResourceIT {
             assertThat(refundEmail.get("enabled"), is(false));
         }
     }
+    
+    @Nested
+    class PatchByServiceIdAndAccountType {
+        String VALID_SERVICE_ID = "a-valid-service-id";
+        String VALID_SERVICE_NAME = "a-test-service";
+        @Test
+        void forEmptyRequest_shouldReturn422() {
+            app.givenSetup()
+                    .body(toJson(Map.of(
+                            "service_id", VALID_SERVICE_ID,
+                            "type", TEST,
+                            "payment_provider", "worldpay",
+                            "service_name", VALID_SERVICE_NAME
+                    )))
+                    .post("/v1/api/accounts");
+                    
+            app.givenSetup().accept(JSON)
+                    .body(new HashMap<>())
+                    .patch(format("/v1/api/service/%s/account/%s/email-notification", VALID_SERVICE_ID, TEST))
+                    .then()
+                    .statusCode(422)
+                    .body("message", containsInAnyOrder("The op field must be 'replace'", "The paths field must be one of: [/refund_issued/template_body, /refund_issued/enabled, /payment_confirmed/template_body, /payment_confirmed/enabled]"))
+                    .body("error_identifier", is(ErrorIdentifier.GENERIC.toString()));
+        }
 
+        @Test
+        void forNonExistentGatewayAccount_shouldReturn404() {
+            String nonExistentServiceId = "a-non-existent-service-id";
+            String templateBody = "lorem ipsum";
+
+            app.givenSetup().accept(JSON)
+                    .body(getPatchRequestBody("/payment_confirmed/" + EmailNotificationResource.EMAIL_NOTIFICATION_TEMPLATE_BODY, templateBody))
+                    .patch(format("/v1/api/service/%s/account/%s/email-notification", nonExistentServiceId, TEST))
+                    .then()
+                    .statusCode(404)
+                    .body("message[0]", is(format("Gateway account not found for service ID [%s] and account type [%s]", nonExistentServiceId, TEST)))
+                    .body("error_identifier", is(ErrorIdentifier.GENERIC.toString()));
+        }
+
+        @Test
+        void forValidRequest_withRefundIssuedEnabled_shouldUpdateRefundEmail() {
+            long gatewayAccountId = Long.parseLong(app.givenSetup()
+                    .body(toJson(Map.of(
+                            "service_id", VALID_SERVICE_ID,
+                            "type", TEST,
+                            "payment_provider", "worldpay",
+                            "service_name", VALID_SERVICE_NAME
+                    )))
+                    .post("/v1/api/accounts")
+                    .then().extract().path("gateway_account_id"));
+
+            app.givenSetup().accept(JSON)
+                    .body(getPatchRequestBody("/refund_issued/enabled", true))
+                    .patch(format("/v1/api/service/%s/account/%s/email-notification", VALID_SERVICE_ID, TEST))
+                    .then()
+                    .statusCode(200);
+
+            Map<String, Object> confirmationEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.PAYMENT_CONFIRMED);
+            Map<String, Object> refundEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.REFUND_ISSUED);
+            assertThat(confirmationEmail.get("enabled"), is(true));
+            assertThat(refundEmail.get("enabled"), is(true));
+        }
+
+        @Test
+        void forValidRequest_withPaymentConfirmedDisabled_shouldUpdateConfirmationEmail() {
+            long gatewayAccountId = Long.parseLong(app.givenSetup()
+                    .body(toJson(Map.of(
+                            "service_id", VALID_SERVICE_ID,
+                            "type", TEST,
+                            "payment_provider", "worldpay",
+                            "service_name", VALID_SERVICE_NAME
+                    )))
+                    .post("/v1/api/accounts")
+                    .then().extract().path("gateway_account_id"));
+
+            app.givenSetup().accept(JSON)
+                    .body(getPatchRequestBody("/payment_confirmed/enabled", false))
+                    .patch(format("/v1/api/service/%s/account/%s/email-notification", VALID_SERVICE_ID, TEST))
+                    .then()
+                    .statusCode(200);
+
+            Map<String, Object> confirmationEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.PAYMENT_CONFIRMED);
+            Map<String, Object> refundEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.REFUND_ISSUED);
+            assertThat(confirmationEmail.get("enabled"), is(false));
+            assertThat(refundEmail.get("enabled"), is(true));
+        }
+
+        @Test
+        void forValidRequest_withRefundIssuedTemplateBody_shouldUpdateRefundEmail() {
+            long gatewayAccountId = Long.parseLong(app.givenSetup()
+                    .body(toJson(Map.of(
+                            "service_id", VALID_SERVICE_ID,
+                            "type", TEST,
+                            "payment_provider", "worldpay",
+                            "service_name", VALID_SERVICE_NAME
+                    )))
+                    .post("/v1/api/accounts")
+                    .then().extract().path("gateway_account_id"));
+            String newTemplateBody = "new value";
+
+            app.givenSetup().accept(JSON)
+                    .body(getPatchRequestBody("/refund_issued/template_body", newTemplateBody))
+                    .patch(format("/v1/api/service/%s/account/%s/email-notification", VALID_SERVICE_ID, TEST))
+                    .then()
+                    .statusCode(200);
+
+            Map<String, Object> confirmationEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.PAYMENT_CONFIRMED);
+            Map<String, Object> refundEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.REFUND_ISSUED);
+            assertThat(confirmationEmail.get("template_body"), nullValue());
+            assertThat(refundEmail.get("template_body"), is(newTemplateBody));
+        }
+
+        @Test
+        void forValidRequest_withPaymentConfirmedTemplateBody_shouldUpdateConfirmationEmail() {
+            long gatewayAccountId = Long.parseLong(app.givenSetup()
+                    .body(toJson(Map.of(
+                            "service_id", VALID_SERVICE_ID,
+                            "type", TEST,
+                            "payment_provider", "worldpay",
+                            "service_name", VALID_SERVICE_NAME
+                    )))
+                    .post("/v1/api/accounts")
+                    .then().extract().path("gateway_account_id"));
+            String newTemplateBody = "new value";
+
+            app.givenSetup().accept(JSON)
+                    .body(getPatchRequestBody(
+                            "/payment_confirmed/template_body", newTemplateBody))
+                    .patch(format("/v1/api/service/%s/account/%s/email-notification", VALID_SERVICE_ID, TEST))
+                    .then()
+                    .statusCode(200);
+
+            Map<String, Object> confirmationEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.PAYMENT_CONFIRMED);
+            Map<String, Object> refundEmail = app.getDatabaseTestHelper().getEmailForAccountAndType(gatewayAccountId, EmailNotificationType.REFUND_ISSUED);
+            assertThat(confirmationEmail.get("template_body"), is(newTemplateBody));
+            assertThat(refundEmail.get("template_body"), nullValue());
+        }
+    }
 
     private String getPatchRequestBody(String path, Object value) {
         return toJson(Map.of("op", "replace", "path", path, "value", value));
