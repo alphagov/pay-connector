2828e987db682fc984a261dd5d79d022453391b0
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2024-07-17 13:49:37 +0100

    PP-12360 Add submitRefund by service ID and type endpoint with happy path tests

--- src/main/java/uk/gov/pay/connector/refund/model/RefundResponse.java
+++ src/main/java/uk/gov/pay/connector/refund/model/RefundResponse.java
@@ -1,6 +1,7 @@
 package uk.gov.pay.connector.refund.model;
 
 import black.door.hate.HalRepresentation;
+import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountType;
 import uk.gov.pay.connector.refund.model.domain.RefundEntity;
 
 import javax.ws.rs.core.UriInfo;
@@ -26,6 +27,29 @@ public class RefundResponse extends HalResourceResponse {
         URI paymentLink = uriInfo.getBaseUriBuilder()
                 .path("/v1/api/accounts/{accountId}/charges/{chargeId}")
                 .build(gatewayAccountId, externalChargeId);
+
+        return new RefundResponse(HalRepresentation.builder()
+                .addProperty("refund_id", refundEntity.getExternalId())
+                .addProperty("amount", refundEntity.getAmount())
+                .addProperty("status", refundEntity.getStatus().toExternal().getStatus())
+                .addProperty("created_date", ISO_INSTANT_MILLISECOND_PRECISION.format(refundEntity.getCreatedDate()))
+                .addProperty("user_external_id", refundEntity.getUserExternalId())
+                .addLink("self", selfLink)
+                .addLink("payment", paymentLink), selfLink);
+    }
+    
+    public static RefundResponse valueOf(RefundEntity refundEntity, String serviceId, GatewayAccountType accountType, UriInfo uriInfo) {
+
+        String externalChargeId = refundEntity.getChargeExternalId();
+        String externalRefundId = refundEntity.getExternalId();
+
+        URI selfLink = uriInfo.getBaseUriBuilder()
+                .path("/v1/api/service/{serviceId}/account/{accountType}/charges/{chargeId}/refunds/{refundId}")
+                .build(serviceId, accountType, externalChargeId, externalRefundId);
+
+        URI paymentLink = uriInfo.getBaseUriBuilder()
+                .path("/v1/api/service/{serviceId}/account/{accountType}/charges/{chargeId}")
+                .build(serviceId, accountType, externalChargeId, externalRefundId);
 
         return new RefundResponse(HalRepresentation.builder()
                 .addProperty("refund_id", refundEntity.getExternalId())
