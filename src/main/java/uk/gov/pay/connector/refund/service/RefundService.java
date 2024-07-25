a8209c582a952498275d03e2099c5ff2020e5a4b
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2024-07-25 14:20:13 +0100

    [squash] comment out unused code

--- src/main/java/uk/gov/pay/connector/refund/service/RefundService.java
+++ src/main/java/uk/gov/pay/connector/refund/service/RefundService.java
@@ -162,47 +162,47 @@ public class RefundService {
                 .findFirst();
     }
 
-    private RefundEntity processRefund(GatewayRefundResponse gatewayRefundResponse, Long refundEntityId,
-                                       GatewayAccountEntity gatewayAccountEntity, Charge charge) {
-        RefundStatus refundStatus = determineRefundStatus(gatewayRefundResponse);
+//    private RefundEntity processRefund(GatewayRefundResponse gatewayRefundResponse, Long refundEntityId,
+//                                       GatewayAccountEntity gatewayAccountEntity, Charge charge) {
+//        RefundStatus refundStatus = determineRefundStatus(gatewayRefundResponse);
+//
+//        if (refundStatus == REFUNDED) {
+//            // If the gateway confirms refunds immediately, the refund status needs
+//            // to be set to REFUND_SUBMITTED and then REFUNDED. This will  help
+//            // services to view refund history in detail in self service.
+//            // see Javadoc (RefundHistory) for details on how history is handled
+//            setRefundStatus(refundEntityId, gatewayAccountEntity, REFUND_SUBMITTED, charge);
+//        }
+//
+//        return updateRefund(gatewayRefundResponse, refundEntityId, refundStatus, gatewayAccountEntity, charge);
+//    }
 
-        if (refundStatus == REFUNDED) {
-            // If the gateway confirms refunds immediately, the refund status needs
-            // to be set to REFUND_SUBMITTED and then REFUNDED. This will  help
-            // services to view refund history in detail in self service.
-            // see Javadoc (RefundHistory) for details on how history is handled
-            setRefundStatus(refundEntityId, gatewayAccountEntity, REFUND_SUBMITTED, charge);
-        }
-
-        return updateRefund(gatewayRefundResponse, refundEntityId, refundStatus, gatewayAccountEntity, charge);
-    }
-
-    @Transactional
-    @SuppressWarnings("WeakerAccess")
-    public RefundEntity updateRefund(GatewayRefundResponse gatewayRefundResponse, Long refundEntityId,
-                                     RefundStatus refundStatus, GatewayAccountEntity gatewayAccountEntity,
-                                     Charge charge) {
-        Optional<RefundEntity> refund = refundDao.findById(refundEntityId);
-
-        refund.ifPresent(refundEntity -> {
-            logger.info("Refund {} ({}) for {} ({} {}) for {} ({}) - {} .'. {} -> {}",
-                    refundEntity.getExternalId(), charge.getPaymentGatewayName(),
-                    refundEntity.getChargeExternalId(), charge.getPaymentGatewayName(),
-                    refundEntity.getGatewayTransactionId(),
-                    gatewayAccountEntity.getAnalyticsId(), gatewayAccountEntity.getId(),
-                    gatewayRefundResponse, refundEntity.getStatus(), refundStatus);
-
-            if (refundStatus == REFUNDED) {
-                userNotificationService.sendRefundIssuedEmail(refundEntity, charge, gatewayAccountEntity);
-            }
-
-            getTransactionId(refundEntity, gatewayRefundResponse).ifPresent(refundEntity::setGatewayTransactionId);
-
-            transitionRefundState(refundEntity, gatewayAccountEntity, refundStatus, charge);
-        });
-
-        return refund.get();
-    }
+//    @Transactional
+//    @SuppressWarnings("WeakerAccess")
+//    public RefundEntity updateRefund(GatewayRefundResponse gatewayRefundResponse, Long refundEntityId,
+//                                     RefundStatus refundStatus, GatewayAccountEntity gatewayAccountEntity,
+//                                     Charge charge) {
+//        Optional<RefundEntity> refund = refundDao.findById(refundEntityId);
+//
+//        refund.ifPresent(refundEntity -> {
+//            logger.info("Refund {} ({}) for {} ({} {}) for {} ({}) - {} .'. {} -> {}",
+//                    refundEntity.getExternalId(), charge.getPaymentGatewayName(),
+//                    refundEntity.getChargeExternalId(), charge.getPaymentGatewayName(),
+//                    refundEntity.getGatewayTransactionId(),
+//                    gatewayAccountEntity.getAnalyticsId(), gatewayAccountEntity.getId(),
+//                    gatewayRefundResponse, refundEntity.getStatus(), refundStatus);
+//
+//            if (refundStatus == REFUNDED) {
+//                userNotificationService.sendRefundIssuedEmail(refundEntity, charge, gatewayAccountEntity);
+//            }
+//
+//            getTransactionId(refundEntity, gatewayRefundResponse).ifPresent(refundEntity::setGatewayTransactionId);
+//
+//            transitionRefundState(refundEntity, gatewayAccountEntity, refundStatus, charge);
+//        });
+//
+//        return refund.get();
+//    }
     
     @Transactional
     public Optional<RefundEntity> updateRefundWithTransactionIdAndState(GatewayRefundResponse gatewayRefundResponse, Long refundEntityId,
