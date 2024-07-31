e161c3289148a9c308d4879df7154bd9bb92a855
oswaldquek <oswaldquek@users.noreply.github.com>
2024-07-31 13:40:26 +0100

    PP-12413: Prevent creation of duplicate Stripe test accounts (#5469)
    * PP-12413: Prevent creation of duplicate Stripe test accounts
    
    Because there is a possibility of concurrent requests to `POST
    /v1/service/{serviceId}/request-stripe-test-account`, do a check to prevent a
    duplicate test Stripe gateway account from being created.

---
+++ src/main/java/uk/gov/pay/connector/gatewayaccount/exception/MultipleStripeTestGatewayAccountsException.java
@@ -0,0 +1,16 @@
+package uk.gov.pay.connector.gatewayaccount.exception;
+
+import javax.ws.rs.WebApplicationException;
+
+import static java.lang.String.format;
+import static uk.gov.pay.connector.util.ResponseUtil.conflictErrorResponse;
+
+public class MultipleStripeTestGatewayAccountsException extends WebApplicationException {
+    
+    public MultipleStripeTestGatewayAccountsException(String serviceId, 
+                                                      String gatewayAccountExternalId, 
+                                                      String gatewayAccountCredentialExternalId) {
+        super(conflictErrorResponse(format("Service '%s' already has an active Stripe gateway account with external id " +
+                "'%s' with gateway account credential is '%s' ", serviceId, gatewayAccountExternalId, gatewayAccountCredentialExternalId)));
+    }
+}
