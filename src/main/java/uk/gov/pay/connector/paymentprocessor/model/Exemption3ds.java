2f3b664632708868a744479bbd360f3a7091ceae
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2024-11-15 17:28:35 +0000

    PP-13331 create exemption object when appropriate
    Add Exemption and Outcome objects to ChargeResponse.
    Populate new objects with appropriate values when needed.

--- src/main/java/uk/gov/pay/connector/paymentprocessor/model/Exemption3ds.java
+++ src/main/java/uk/gov/pay/connector/paymentprocessor/model/Exemption3ds.java
@@ -2,8 +2,19 @@ package uk.gov.pay.connector.paymentprocessor.model;
 
 public enum Exemption3ds {
 
-    EXEMPTION_NOT_REQUESTED,
-    EXEMPTION_HONOURED,
-    EXEMPTION_REJECTED,
-    EXEMPTION_OUT_OF_SCOPE
+    EXEMPTION_NOT_REQUESTED("not requested"),
+    EXEMPTION_HONOURED("honoured"),
+    EXEMPTION_REJECTED("rejected"),
+    EXEMPTION_OUT_OF_SCOPE("out of scope");
+
+    private String displayName;
+
+    Exemption3ds(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
