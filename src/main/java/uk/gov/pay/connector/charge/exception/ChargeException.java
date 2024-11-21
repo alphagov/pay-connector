745f136c5e7cf9753e2494528f932c5d8a608372
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-11-21 09:51:43 +0000

    PP-13315: Return 422 when return url is not https and gateway account is live
    As part of this commit,
    * AllowedStrings and AllowedStringsValidator are sneakily deleted
    * A generic ChargeException that can take an ErrorIdentifier and http response
      status code is created. There is scope to delete a bunch of custom Exception
      by using this class but this refactor can go into another PR.

---
+++ src/main/java/uk/gov/pay/connector/charge/exception/ChargeException.java
@@ -0,0 +1,25 @@
+package uk.gov.pay.connector.charge.exception;
+
+import uk.gov.service.payments.commons.model.ErrorIdentifier;
+
+import javax.ws.rs.WebApplicationException;
+
+public class ChargeException extends WebApplicationException {
+
+    private final ErrorIdentifier errorIdentifier;
+    private final int httpResponseStatusCode;
+
+    public ChargeException(String message, ErrorIdentifier errorIdentifier, int httpResponseStatusCode) {
+        super(message);
+        this.errorIdentifier = errorIdentifier;
+        this.httpResponseStatusCode = httpResponseStatusCode;
+    }
+
+    public ErrorIdentifier getErrorIdentifier() {
+        return errorIdentifier;
+    }
+
+    public int getHttpResponseStatusCode() {
+        return httpResponseStatusCode;
+    }
+}
