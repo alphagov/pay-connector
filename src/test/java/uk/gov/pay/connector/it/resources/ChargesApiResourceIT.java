e23ea047e15ed8786082e49fd0bead92dbb13034
Rafiq Gasmi <rafiq.gasmi@digital.cabinet-office.gov.uk>
2025-09-01 09:44:45 +0100

    PP-14378 - test

--- src/test/java/uk/gov/pay/connector/it/resources/ChargesApiResourceIT.java
+++ src/test/java/uk/gov/pay/connector/it/resources/ChargesApiResourceIT.java
@@ -1017,11 +1017,20 @@ public class ChargesApiResourceIT {
         log.error("testbasees");
         System.out.println("testbasees");
         Assertions.fail();
-        ValidatableResponse t = testBaseExtension.getCharge(chargeId);
-
-        t.body("settlement_summary.capture_submit_time", isWithin(20, SECONDS))
-                .body("settlement_summary.capture_submit_time", matchesPattern("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(.\\d{1,3})?Z"))
-                .body("settlement_summary.captured_date", equalTo(expectedDayOfCapture));
+        int maxAttempts = 30;
+        ValidatableResponse t = null;
+        for (int i = 0; i < maxAttempts; i++) {
+            t = testBaseExtension.getCharge(chargeId);
+            String captureSubmitTime = t.extract().path("settlement_summary.capture_submit_time");
+            if (captureSubmitTime != null) {
+                System.out.println(i);
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        if (t == null || t.extract().path("settlement_summary.capture_submit_time") == null) {
+            Assertions.fail("settlement_summary.capture_submit_time was not set after polling");
+        }
     }
     
     private void createCharge(String externalChargeId, long chargeId) {
