c15cd097892864bc10be4fa3abba078230ff6189
SandorArpa <sandor.arpa@digital.cabinet-office.gov.uk>
2025-12-01 10:45:31 +0000

    PP-14700 remove deprecated sandbox card number
    Remove the deprecated sandbox card number.

--- src/main/java/uk/gov/pay/connector/gateway/sandbox/SandboxCardNumbers.java
+++ src/main/java/uk/gov/pay/connector/gateway/sandbox/SandboxCardNumbers.java
@@ -36,7 +36,6 @@ public interface SandboxCardNumbers {
 
     Set<String> GOOD_CORPORATE_CARDS = Set.of(
             "4988080000000000",
-            "4000620000000007", // deprecated, soon to be removed
             "4111111111111111",
             "4293189100000008",
             GOOD_CORPORATE_PREPAID_UNKNOWN_CREDIT_CARD);
