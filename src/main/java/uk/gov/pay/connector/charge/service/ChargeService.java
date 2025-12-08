fb6c21e53817cd8842ec1f9a51c42c33aff70018
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-12-08 09:50:37 +0000

    PP-14757 refactor - flatten if statements

--- src/main/java/uk/gov/pay/connector/charge/service/ChargeService.java
+++ src/main/java/uk/gov/pay/connector/charge/service/ChargeService.java
@@ -1229,7 +1229,8 @@ public class ChargeService {
         if (hasAgreementId(chargeCreateRequest)) {
             if (!chargeCreateRequest.getSavePaymentInstrumentToAgreement()) {
                 throw new UnexpectedAttributeException("agreement_id");
-            } else if (!gatewayAccount.isRecurringEnabled()) {
+            } 
+            if (!gatewayAccount.isRecurringEnabled()) {
                 throw new RecurringCardPaymentsNotAllowedException(
                         "Attempt to save payment instrument to agreement for gateway account " +
                                 gatewayAccount.getId() +
@@ -1239,7 +1240,8 @@ public class ChargeService {
             if (chargeCreateRequest.getSavePaymentInstrumentToAgreement()) {
                 throw new ChargeException("If [save_payment_instrument_to_agreement] is true, [agreement_id] must be specified",
                         GENERIC, HttpStatus.SC_BAD_REQUEST);
-            } else if (chargeCreateRequest.getAgreementPaymentType().isPresent()) {
+            }
+            if (chargeCreateRequest.getAgreementPaymentType().isPresent()) {
                 throw new UnexpectedAttributeException("agreement_payment_type");
             }
         }
@@ -1255,15 +1257,20 @@ public class ChargeService {
                     "Attempt to use authorisation mode 'agreement' for gateway account " +
                             gatewayAccount.getId() +
                             ", which does not have recurring card payments enabled");
-        } else if (chargeCreateRequest.getAgreementId() == null) {
+        } 
+        if (chargeCreateRequest.getAgreementId() == null) {
             throw new MissingMandatoryAttributeException("agreement_id");
-        } else if (chargeCreateRequest.getSavePaymentInstrumentToAgreement()) {
+        } 
+        if (chargeCreateRequest.getSavePaymentInstrumentToAgreement()) {
             throw new IncorrectAuthorisationModeForSavePaymentToAgreementException();
-        } else if (chargeCreateRequest.isMoto()) {
+        } 
+        if (chargeCreateRequest.isMoto()) {
             throw new UnexpectedAttributeException("moto");
-        } else if (chargeCreateRequest.getEmail().isPresent()) {
+        }
+        if (chargeCreateRequest.getEmail().isPresent()) {
             throw new UnexpectedAttributeException("email");
-        } else if (chargeCreateRequest.getPrefilledCardHolderDetails().isPresent()) {
+        }
+        if (chargeCreateRequest.getPrefilledCardHolderDetails().isPresent()) {
             throw new UnexpectedAttributeException("prefilled_cardholder_details");
         }
     }
