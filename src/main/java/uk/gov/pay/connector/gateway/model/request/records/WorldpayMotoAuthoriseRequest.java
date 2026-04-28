40d72ea58cce1c3f1349007b63b8be64393aa0de
HJ <92534363+hjvoid@users.noreply.github.com>
2026-04-28 11:44:53 +0100

    PP-15117 record for Worldpay moto auth requests (#6293)

---
+++ src/main/java/uk/gov/pay/connector/gateway/model/request/records/WorldpayMotoAuthoriseRequest.java
@@ -0,0 +1,25 @@
+package uk.gov.pay.connector.gateway.model.request.records;
+
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+public record WorldpayMotoAuthoriseRequest(
+        String username,
+        String password,
+        String merchantCode,
+        String orderCode,
+        String description,
+        String amountInPence,
+        String cardNumber,
+        String expiryDateMonthTwoDigits,
+        String expiryDateYearFourDigits,
+        String cardholderName,
+        String cvc
+) {
+    @Override
+    public String toString(){
+        return getClass().getSimpleName() + '[' 
+                + "merchantCode= " + merchantCode 
+                + ", orderCode= " + orderCode + ']';
+    } 
+}
