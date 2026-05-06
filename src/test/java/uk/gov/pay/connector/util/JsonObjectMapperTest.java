fbb8279e02f6ff3ae7f1295ea87b5425c351a27e
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2026-05-06 13:52:56 +0100

    PP-15084 Add method to JsonObjectMapper for JSON serialization (#6310)
    - Added helper method to `JsonObjectMapper` to convert objects to JSON strings for Adyen payloads.
    
    Co-authored-by: Pippa101 <94748151+Pippa101@users.noreply.github.com>

--- src/test/java/uk/gov/pay/connector/util/JsonObjectMapperTest.java
+++ src/test/java/uk/gov/pay/connector/util/JsonObjectMapperTest.java
@@ -1,17 +1,25 @@
 package uk.gov.pay.connector.util;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-
 import jakarta.ws.rs.WebApplicationException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class JsonObjectMapperTest {
-    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private JsonObjectMapper jsonObjectMapper = new JsonObjectMapper(objectMapper);
 
     @Test
@@ -27,6 +35,35 @@ class JsonObjectMapperTest {
         String jsonString = "{\"id\":\"abc\"}";
         assertThrows(WebApplicationException.class, () -> jsonObjectMapper.getObject(jsonString, LocalTestObject.class));
     }
+
+    @Nested
+    @DisplayName("ObjectToString")
+    class TestObjectToString {
+
+        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
+
+        @Test
+        void shouldConvertObjectToJsonStringSuccessfully() {
+            LocalTestObject localTestObject = new LocalTestObject();
+            localTestObject.setId(12345);
+
+            String result = jsonObjectMapper.objectToString(localTestObject);
+
+            assertEquals("{\"id\":12345}", result);
+        }
+
+        @Test
+        void shouldThrowWebApplicationExceptionWhenSerializationFails() throws JsonProcessingException {
+            LocalTestObject localTestObject = new LocalTestObject();
+            when(mockObjectMapper.writeValueAsString(localTestObject))
+                    .thenThrow(new JsonProcessingException("error-serialising-object") {
+                    });
+
+            jsonObjectMapper = new JsonObjectMapper(mockObjectMapper);
+
+            assertThrows(WebApplicationException.class, () -> jsonObjectMapper.objectToString(localTestObject));
+        }
+    }
 }
 
 class LocalTestObject {
@@ -35,5 +72,9 @@ class LocalTestObject {
 
     int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
