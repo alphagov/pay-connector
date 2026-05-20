bc93c5884f40227ffd062110469c2e313eb5f23c
Andrzej Prochyra <andrzej.prochyra@digital.cabinet-office.gov.uk>
2026-05-20 12:05:38 +0100

    PP-15146: implement successful system cancel scenario

---
+++ src/main/java/uk/gov/pay/connector/gateway/adyen/handler/AdyenCancelHandler.java
@@ -0,0 +1,69 @@
+package uk.gov.pay.connector.gateway.adyen.handler;
+
+import io.dropwizard.jackson.Jackson;
+import uk.gov.pay.connector.app.adyen.AdyenGatewayConfig;
+import uk.gov.pay.connector.gateway.GatewayClient;
+import uk.gov.pay.connector.gateway.GatewayException;
+import uk.gov.pay.connector.gateway.adyen.AdyenRequestFactory;
+import uk.gov.pay.connector.gateway.adyen.request.AdyenCancelRequest;
+import uk.gov.pay.connector.gateway.model.request.CancelGatewayRequest;
+import uk.gov.pay.connector.gateway.model.response.BaseCancelResponse;
+import uk.gov.pay.connector.gateway.model.response.GatewayResponse;
+import uk.gov.pay.connector.util.JsonObjectMapper;
+
+import static uk.gov.pay.connector.gateway.adyen.utils.AdyenRequestUtil.getCancelUrl;
+import static uk.gov.pay.connector.gateway.adyen.utils.AdyenRequestUtil.getHeaders;
+import static uk.gov.pay.connector.gateway.model.response.BaseCancelResponse.CancelStatus.SUBMITTED;
+
+public class AdyenCancelHandler {
+
+    private final GatewayClient client;
+    private final AdyenRequestFactory requestFactory;
+    private final AdyenGatewayConfig config;
+
+    public AdyenCancelHandler(GatewayClient client, AdyenRequestFactory requestFactory, AdyenGatewayConfig config) {
+        this.client = client;
+        this.requestFactory = requestFactory;
+        this.config = config;
+    }
+
+    public GatewayResponse<BaseCancelResponse> cancel(CancelGatewayRequest request) {
+        var responseBuilder = GatewayResponse.GatewayResponseBuilder.responseBuilder();
+        var cancelRequest = new AdyenCancelRequest(
+                getCancelUrl(config, request),
+                getHeaders(config, request.isLiveAccount()),
+                requestFactory.createPaymentCancelRequest(request),
+                request.getGatewayAccount().getType(),
+                new JsonObjectMapper(Jackson.newObjectMapper()));
+        try {
+            client.postRequestFor(cancelRequest);
+        } catch (GatewayException.GenericGatewayException e) {
+            throw new RuntimeException(e);
+        } catch (GatewayException.GatewayErrorException e) {
+            throw new RuntimeException(e);
+        } catch (GatewayException.GatewayConnectionTimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        return responseBuilder.withResponse(new BaseCancelResponse() {
+            @Override
+            public String getTransactionId() {
+                return "";
+            }
+
+            @Override
+            public CancelStatus cancelStatus() {
+                return SUBMITTED;
+            }
+
+            @Override
+            public String getErrorCode() {
+                return "";
+            }
+
+            @Override
+            public String getErrorMessage() {
+                return "";
+            }
+        }).build();
+    }
+}
---
+++ src/test/java/uk/gov/pay/connector/gateway/adyen/handler/AdyenCancelHandlerTest.java
@@ -0,0 +1,166 @@
+package uk.gov.pay.connector.gateway.adyen.handler;
+
+import com.jayway.jsonassert.JsonAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.pay.connector.app.ConnectorConfiguration;
+import uk.gov.pay.connector.app.adyen.AdyenGatewayConfig;
+import uk.gov.pay.connector.app.adyen.AdyenIds;
+import uk.gov.pay.connector.app.adyen.ApiKeys.CompanyAccountApiKeys;
+import uk.gov.pay.connector.app.adyen.BaseUrls.CheckoutUrls;
+import uk.gov.pay.connector.gateway.GatewayClient;
+import uk.gov.pay.connector.gateway.adyen.AdyenRequestFactory;
+import uk.gov.pay.connector.gateway.model.request.CancelGatewayRequest;
+import uk.gov.pay.connector.gateway.model.request.GatewayClientPostRequest;
+import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
+import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountType;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
+import static org.mockito.BDDMockito.then;
+import static uk.gov.pay.connector.app.adyen.ApiKeysFixture.anApiKeys;
+import static uk.gov.pay.connector.app.adyen.BaseUrlsFixture.aBaseUrls;
+import static uk.gov.pay.connector.charge.model.domain.ChargeEntityFixture.aValidChargeEntity;
+import static uk.gov.pay.connector.gateway.model.response.BaseCancelResponse.CancelStatus.SUBMITTED;
+import static uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntityFixture.aGatewayAccountEntity;
+import static uk.gov.pay.connector.gatewayaccount.model.GatewayAccountType.LIVE;
+
+@ExtendWith(MockitoExtension.class)
+class AdyenCancelHandlerTest {
+
+    public static final String A_MERCHANT_ACCOUNT_ID = "a-merchant-account-id";
+    public static final String AN_EXTERNAL_ID = "a-charge-external-id";
+    public static final String LIVE_CHECKOUT_BASE_URL = "https://checkout.example.com";
+    public static final String A_GATEWAY_TRANSACTION_ID = "a-gateway-transaction-id";
+    public static final String TEST_COMPANY_ACCOUNT_API_KEY = "test-company-account-API-key"; // pragma: allowlist secret
+    public static final String LIVE_COMPANY_ACCOUNT_API_KEY = "live-company-account-API-key"; // pragma: allowlist secret
+
+    @Mock
+    private GatewayClient mockGatewayClient;
+    @Captor
+    private ArgumentCaptor<GatewayClientPostRequest> captor;
+
+    private AdyenCancelHandler cancelHandler;
+
+    @BeforeEach
+    void setUp() {
+        var mockAdyenIds = new AdyenIds(A_MERCHANT_ACCOUNT_ID, A_MERCHANT_ACCOUNT_ID);
+        var mockAdyenGatewayConfig = mock(AdyenGatewayConfig.class);
+        given(mockAdyenGatewayConfig.getMerchantAccountIds())
+                .willReturn(mockAdyenIds);
+        var mockApiKeys = anApiKeys()
+                .withCompanyAccount(
+                        new CompanyAccountApiKeys(TEST_COMPANY_ACCOUNT_API_KEY, LIVE_COMPANY_ACCOUNT_API_KEY))
+                .build();
+        given(mockAdyenGatewayConfig.getApiKeys())
+                .willReturn(mockApiKeys);
+        var mockBaseUrls = aBaseUrls()
+                .withCheckout(new CheckoutUrls("https://example.com", LIVE_CHECKOUT_BASE_URL))
+                .build();
+        given(mockAdyenGatewayConfig.getBaseUrls())
+                .willReturn(mockBaseUrls);
+        var mockConfiguration = mock(ConnectorConfiguration.class);
+        given(mockConfiguration.getAdyenGatewayConfig())
+                .willReturn(mockAdyenGatewayConfig);
+        cancelHandler = new AdyenCancelHandler(
+                mockGatewayClient,
+                new AdyenRequestFactory(mockConfiguration),
+                mockAdyenGatewayConfig);
+    }
+
+    @Test
+    void should_POST_request_to_the_cancel_an_authorised_payment_URL_and_pass_the_gateway_transaction_ID_as_the_path_parameter() throws Exception {
+        var request = CancelGatewayRequest.valueOf(
+                aValidChargeEntity()
+                        .withGatewayTransactionId(A_GATEWAY_TRANSACTION_ID)
+                        .withGatewayAccountEntity(makeGatewayAccountEntityForAccountType(LIVE))
+                        .build());
+
+        cancelHandler.cancel(request);
+
+        then(mockGatewayClient).should()
+                .postRequestFor(captor.capture());
+        assertThat(captor.getValue().getUrl().toString(), is(
+                LIVE_CHECKOUT_BASE_URL + "/payments/" + A_GATEWAY_TRANSACTION_ID + "/cancels"));
+    }
+
+    @Test
+    void should_POST_a_GatewayOrder_with_reference_and_merchant_account_in_payload() throws Exception {
+        var request = CancelGatewayRequest.valueOf(
+                aValidChargeEntity()
+                        .withExternalId(AN_EXTERNAL_ID)
+                        .withGatewayAccountEntity(makeGatewayAccountEntityForAccountType(LIVE))
+                        .build());
+
+        cancelHandler.cancel(request);
+
+        then(mockGatewayClient).should()
+                .postRequestFor(captor.capture());
+        var gatewayOrderPayload = captor.getValue()
+                .getGatewayOrder()
+                .getPayload();
+        JsonAssert.with(gatewayOrderPayload)
+                .assertEquals("reference", AN_EXTERNAL_ID)
+                .assertEquals("merchantAccount", A_MERCHANT_ACCOUNT_ID);
+    }
+
+    @Test
+    void should_POST_with_company_API_key_as_X_API_Key_header() throws Exception {
+        var request = CancelGatewayRequest.valueOf(
+                aValidChargeEntity()
+                        .withGatewayAccountEntity(makeGatewayAccountEntityForAccountType(LIVE))
+                        .build());
+
+        cancelHandler.cancel(request);
+
+        then(mockGatewayClient).should()
+                .postRequestFor(captor.capture());
+        var headers = captor.getValue().getHeaders();
+        assertThat(headers, hasEntry("X-API-Key", LIVE_COMPANY_ACCOUNT_API_KEY));
+    }
+
+    @ParameterizedTest
+    @EnumSource(GatewayAccountType.class)
+    void should_POST_request_with_account_type(GatewayAccountType type) throws Exception {
+        var request = CancelGatewayRequest.valueOf(
+                aValidChargeEntity()
+                        .withGatewayAccountEntity(makeGatewayAccountEntityForAccountType(type))
+                        .build());
+
+        cancelHandler.cancel(request);
+
+        then(mockGatewayClient).should()
+                .postRequestFor(captor.capture());
+        var accountType = captor.getValue().getGatewayAccountType();
+        assertThat(accountType, is(type.toString()));
+    }
+
+    @Test
+    void should_return_SUBMITTED_cancel_status() {
+        var request = CancelGatewayRequest.valueOf(
+                aValidChargeEntity()
+                        .withGatewayAccountEntity(makeGatewayAccountEntityForAccountType(LIVE))
+                        .build());
+
+        var gatewayResponse = cancelHandler.cancel(request);
+
+        assertThat(gatewayResponse.getBaseResponse().isPresent(), is(true));
+        assertThat(gatewayResponse.getBaseResponse().get().cancelStatus(), is(SUBMITTED));
+    }
+
+    private static GatewayAccountEntity makeGatewayAccountEntityForAccountType(GatewayAccountType type) {
+        return aGatewayAccountEntity()
+                .withType(type)
+                .build();
+    }
+}
