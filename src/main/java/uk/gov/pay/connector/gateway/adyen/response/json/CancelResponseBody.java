48d3c1173ee4654e8959bd3d8d765efd7427a024
Andrzej Prochyra <andrzej.prochyra@digital.cabinet-office.gov.uk>
2026-06-09 16:36:34 +0100

    PP-15146: Introduce `AdyenCancelResponse` (#6401)
    * PP-15146: introduce `AdyenCancelResponse` and implement response mapping and error handling

---
+++ src/main/java/uk/gov/pay/connector/gateway/adyen/response/json/CancelResponseBody.java
@@ -0,0 +1,12 @@
+package uk.gov.pay.connector.gateway.adyen.response.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record CancelResponseBody(
+        @JsonProperty("paymentPspReference")
+        String paymentPspReference) {
+}
