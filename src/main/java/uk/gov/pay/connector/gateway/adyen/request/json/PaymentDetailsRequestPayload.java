39e242268d8db3817d10588170d78a0280575a6f
Rafiq-Gasmi <145993770+Rafiq-Gasmi@users.noreply.github.com>
2026-06-11 09:00:16 +0100

    Update src/main/java/uk/gov/pay/connector/gateway/adyen/request/json/PaymentDetailsRequestPayload.java
    better naming
    
    Co-authored-by: Krishna Bottla <40598480+kbottla@users.noreply.github.com>

--- src/main/java/uk/gov/pay/connector/gateway/adyen/request/json/PaymentDetailsRequestPayload.java
+++ src/main/java/uk/gov/pay/connector/gateway/adyen/request/json/PaymentDetailsRequestPayload.java
@@ -2,7 +2,7 @@ package uk.gov.pay.connector.gateway.adyen.request.json;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record PaymentDetailsRequestPayload(@JsonProperty("details") Details details) {
+public record Authorise3dsRequestPayload(@JsonProperty("details") Details details) {
     public record Details(@JsonProperty("redirectResult") String redirectResult) {
     }
 }
