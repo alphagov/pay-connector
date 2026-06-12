a8a0253babdc56c1a97dd3cc369d1e4c32012fe1
Rafiq-Gasmi <145993770+Rafiq-Gasmi@users.noreply.github.com>
2026-06-12 12:55:10 +0100

    PP-14542 - Adyen 3DS authorise handler and tests (#6412)
    * PP-14542 - Adyen 3DS authorise handler and tests
    
    * PP-14542 - Add tests for null Auth3dsResult and generic exception handling; fix generic catch variable
    
    * PP-14542 - prevent NoSuchElementException failure
    
    * PP-14542 - add test to cover non parseable errors
    
    * PP-14542 - PR Comments fixes, removed adyenAuthorise3dsHandler to be added into a separate PR. added static imports. set cause for logging. removed unneeded logging as it's logged at a higher level.

---
+++ src/main/java/uk/gov/pay/connector/gateway/adyen/handler/AdyenAuthorise3dsHandler.java
@@ -0,0 +1,130 @@
+package uk.gov.pay.connector.gateway.adyen.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.pay.connector.app.ConnectorConfiguration;
+import uk.gov.pay.connector.app.adyen.AdyenGatewayConfig;
+import uk.gov.pay.connector.gateway.GatewayClient;
+import uk.gov.pay.connector.gateway.GatewayException;
+import uk.gov.pay.connector.gateway.adyen.AdyenRequestFactory;
+import uk.gov.pay.connector.gateway.adyen.request.Adyen3dsAuthorisationRequest;
+import uk.gov.pay.connector.gateway.adyen.response.json.AdyenError;
+import uk.gov.pay.connector.gateway.adyen.response.json.Authorise3dsResponseBody;
+import uk.gov.pay.connector.gateway.model.request.Auth3dsResponseGatewayRequest;
+import uk.gov.pay.connector.gateway.model.response.BaseAuthoriseResponse.AuthoriseStatus;
+import uk.gov.pay.connector.gateway.model.response.Gateway3DSAuthorisationResponse;
+import uk.gov.pay.connector.util.JsonObjectMapper;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static uk.gov.pay.connector.gateway.adyen.utils.AdyenRequestUtil.get3dsAuthUrl;
+import static uk.gov.pay.connector.gateway.adyen.utils.AdyenRequestUtil.getHeaders;
+import static uk.gov.pay.connector.gateway.model.response.BaseAuthoriseResponse.AuthoriseStatus.ERROR;
+import static uk.gov.service.payments.logging.LoggingKeys.GATEWAY_ERROR;
+import static uk.gov.service.payments.logging.LoggingKeys.HTTP_STATUS;
+import static uk.gov.service.payments.logging.LoggingKeys.PAYMENT_EXTERNAL_ID;
+import static uk.gov.service.payments.logging.LoggingKeys.PROVIDER_PAYMENT_ID;
+
+public class AdyenAuthorise3dsHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdyenAuthorise3dsHandler.class);
+
+    private final GatewayClient gatewayClient;
+    private final AdyenGatewayConfig adyenGatewayConfig;
+    private final AdyenRequestFactory adyenRequestFactory;
+    private final JsonObjectMapper jsonObjectMapper;
+
+    public AdyenAuthorise3dsHandler(GatewayClient gatewayClient,
+                                    ConnectorConfiguration connectorConfiguration,
+                                    JsonObjectMapper jsonObjectMapper) {
+        this.gatewayClient = gatewayClient;
+        this.adyenGatewayConfig = connectorConfiguration.getAdyenGatewayConfig();
+        this.adyenRequestFactory = new AdyenRequestFactory(connectorConfiguration);
+        this.jsonObjectMapper = jsonObjectMapper;
+    }
+
+    public Gateway3DSAuthorisationResponse authorise3dsResponse(Auth3dsResponseGatewayRequest request) {
+        var redirectResult = request.getAuth3dsResult() == null ? null : request.getAuth3dsResult().getRedirectResult();
+        if (isBlank(redirectResult)) {
+            LOGGER.atWarn()
+                    .setMessage("Adyen 3DS response authorisation failed because redirect result is blank")
+                    .addKeyValue(PAYMENT_EXTERNAL_ID, request.getChargeExternalId())
+                    .log();
+            return Gateway3DSAuthorisationResponse.of(ERROR);
+        }
+
+        var adyen3dsAuthorisationRequest = new Adyen3dsAuthorisationRequest(
+                get3dsAuthUrl(adyenGatewayConfig, request),
+                getHeaders(adyenGatewayConfig, request.getGatewayAccount().isLive(), request.getRequestType(), request.getChargeExternalId()),
+                request.getGatewayAccount().getType(),
+                adyenRequestFactory.createPaymentDetailsRequest(request),
+                jsonObjectMapper
+        );
+
+        try {
+            var jsonResponse = gatewayClient.postRequestFor(adyen3dsAuthorisationRequest).getEntity();
+            var responseBody = jsonObjectMapper.getObject(jsonResponse, Authorise3dsResponseBody.class);
+            var mappedStatus = mapStatus(responseBody.resultCode());
+
+            return Gateway3DSAuthorisationResponse.of(
+                    jsonResponse,
+                    mappedStatus,
+                    responseBody.pspReference()
+            );
+        } catch (GatewayException.GatewayErrorException e) {
+            return handleGatewayErrorException(request, e);
+        } catch (GatewayException.GatewayConnectionTimeoutException | GatewayException.GenericGatewayException e) {
+            LOGGER.atWarn()
+                    .setCause(e)
+                    .setMessage("Adyen 3DS authorisation request failed")
+                    .addKeyValue(PAYMENT_EXTERNAL_ID, request.getChargeExternalId())
+                    .addKeyValue(GATEWAY_ERROR, e.getMessage())
+                    .log();
+            return Gateway3DSAuthorisationResponse.of(e.getMessage(), AuthoriseStatus.EXCEPTION);
+        } catch (Exception e) {
+            LOGGER.atWarn()
+                    .setCause(e)
+                    .setMessage("Adyen 3DS authorisation response could not be processed")
+                    .addKeyValue(PAYMENT_EXTERNAL_ID, request.getChargeExternalId())
+                    .addKeyValue(GATEWAY_ERROR, e.getMessage())
+                    .log();
+            return Gateway3DSAuthorisationResponse.of(e.getMessage(), AuthoriseStatus.EXCEPTION);
+        }
+    }
+
+    private Gateway3DSAuthorisationResponse handleGatewayErrorException(
+            Auth3dsResponseGatewayRequest request,
+            GatewayException.GatewayErrorException exception) {
+        try {
+            var adyenError = jsonObjectMapper.getObject(exception.getResponseFromGateway(), AdyenError.class);
+            LOGGER.atWarn()
+                    .setMessage("Adyen 3DS authorisation request failed")
+                    .addKeyValue(PAYMENT_EXTERNAL_ID, request.getChargeExternalId())
+                    .addKeyValue(PROVIDER_PAYMENT_ID, adyenError.pspReference())
+                    .addKeyValue(HTTP_STATUS, exception.getStatus())
+                    .addKeyValue(GATEWAY_ERROR, adyenError.message())
+                    .log();
+        } catch (Exception e) {
+            LOGGER.atWarn()
+                    .setCause(e)
+                    .setMessage("Adyen 3DS authorisation request failed with non-parseable error response")
+                    .addKeyValue(PAYMENT_EXTERNAL_ID, request.getChargeExternalId())
+                    .addKeyValue(HTTP_STATUS, exception.getStatus())
+                    .addKeyValue(GATEWAY_ERROR, exception.getMessage())
+                    .log();
+        }
+
+        return Gateway3DSAuthorisationResponse.of(exception.getMessage(), AuthoriseStatus.EXCEPTION);
+    }
+
+    private AuthoriseStatus mapStatus(String resultCode) {
+        if (resultCode == null) {
+            return ERROR;
+        }
+        return switch (resultCode) {
+            case "Authorised" -> AuthoriseStatus.AUTHORISED;
+            case "Refused" -> AuthoriseStatus.REJECTED;
+            case "Cancelled" -> AuthoriseStatus.CANCELLED;
+            default -> ERROR;
+        };
+    }
+}
---
+++ src/test/java/uk/gov/pay/connector/gateway/adyen/handler/AdyenAuthorise3dsHandlerTest.java
@@ -0,0 +1,195 @@
+package uk.gov.pay.connector.gateway.adyen.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonassert.JsonAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.pay.connector.app.ConnectorConfiguration;
+import uk.gov.pay.connector.app.adyen.AdyenGatewayConfig;
+import uk.gov.pay.connector.app.adyen.ApiKeys;
+import uk.gov.pay.connector.app.adyen.BaseUrls;
+import uk.gov.pay.connector.gateway.GatewayClient;
+import uk.gov.pay.connector.gateway.GatewayException;
+import uk.gov.pay.connector.gateway.model.Auth3dsResult;
+import uk.gov.pay.connector.gateway.model.request.Auth3dsResponseGatewayRequest;
+import uk.gov.pay.connector.gateway.model.request.GatewayClientPostRequest;
+import uk.gov.pay.connector.gateway.model.response.BaseAuthoriseResponse;
+import uk.gov.pay.connector.util.JsonObjectMapper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uk.gov.pay.connector.charge.model.domain.ChargeEntityFixture.aValidChargeEntity;
+
+@ExtendWith(MockitoExtension.class)
+class AdyenAuthorise3dsHandlerTest {
+
+    private static final String TEST_ADYEN_CHECKOUT_BASE_URL = "https://example.com/test/someVersion";
+
+    @Mock
+    private GatewayClient mockClient;
+    @Mock
+    private ConnectorConfiguration mockConfig;
+    @Mock
+    private GatewayClient.Response mockGatewayClientResponse;
+    @Captor
+    private ArgumentCaptor<GatewayClientPostRequest> captor;
+
+    private final JsonObjectMapper jsonObjectMapper = new JsonObjectMapper(new ObjectMapper());
+    private AdyenAuthorise3dsHandler adyenAuthorise3dsHandler;
+
+    @BeforeEach
+    void setUp() {
+        BaseUrls mockBaseUrls = mock(BaseUrls.class);
+        lenient().when(mockBaseUrls.checkout()).thenReturn(new BaseUrls.CheckoutUrls(TEST_ADYEN_CHECKOUT_BASE_URL, "https://example.com/live/someVersion"));
+        AdyenGatewayConfig mockAdyenGatewayConfig = mock(AdyenGatewayConfig.class);
+        lenient().when(mockAdyenGatewayConfig.getBaseUrls()).thenReturn(mockBaseUrls);
+
+        ApiKeys mockApiKeys = mock(ApiKeys.class);
+        ApiKeys.CompanyAccountApiKeys mockCompanyApiKeys = mock(ApiKeys.CompanyAccountApiKeys.class);
+        lenient().when(mockApiKeys.companyAccount()).thenReturn(mockCompanyApiKeys);
+        lenient().when(mockCompanyApiKeys.test()).thenReturn("test");
+        lenient().when(mockAdyenGatewayConfig.getApiKeys()).thenReturn(mockApiKeys);
+        lenient().when(mockConfig.getAdyenGatewayConfig()).thenReturn(mockAdyenGatewayConfig);
+
+        adyenAuthorise3dsHandler = new AdyenAuthorise3dsHandler(mockClient, mockConfig, jsonObjectMapper);
+    }
+
+    @Test
+    void should_send_redirect_result_to_adyen_payments_details_endpoint() throws Exception {
+        when(mockClient.postRequestFor(any())).thenReturn(mockGatewayClientResponse);
+        when(mockGatewayClientResponse.getEntity()).thenReturn(successResponse("Authorised"));
+        var request = buildRequestWith("eyJ0cmFuc1N0YXR1cyI6IlkifQ==");
+
+        adyenAuthorise3dsHandler.authorise3dsResponse(request);
+
+        then(mockClient).should().postRequestFor(captor.capture());
+        assertThat(captor.getValue().getUrl().toString(), is("https://example.com/test/someVersion/payments/details"));
+        JsonAssert.with(captor.getValue().getGatewayOrder().getPayload())
+                .assertThat("$.details.redirectResult", equalTo("eyJ0cmFuc1N0YXR1cyI6IlkifQ=="));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Authorised,AUTHORISED",
+            "Refused,REJECTED",
+            "Cancelled,CANCELLED",
+            "Error,ERROR",
+            "ChallengeShopper,ERROR"
+    })
+    void should_map_adyen_result_code_to_authorisation_status(String resultCode, BaseAuthoriseResponse.AuthoriseStatus expectedStatus) throws Exception {
+        when(mockClient.postRequestFor(any())).thenReturn(mockGatewayClientResponse);
+        when(mockGatewayClientResponse.getEntity()).thenReturn(successResponse(resultCode));
+
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(buildRequestWith("redirect-result"));
+
+        assertThat(response.getMappedChargeStatus(), is(expectedStatus.getMappedChargeStatus()));
+        assertThat(response.getTransactionId().isPresent(), is(true));
+        assertThat(response.getTransactionId().get(), is("adyen-3ds-psp-reference"));
+    }
+
+    @Test
+    void should_map_null_result_code_to_authorisation_status_error() throws Exception {
+        when(mockClient.postRequestFor(any())).thenReturn(mockGatewayClientResponse);
+        when(mockGatewayClientResponse.getEntity()).thenReturn(
+                """
+                {
+                  "pspReference": "adyen-3ds-psp-reference",
+                  "resultCode": null
+                }
+                """
+        );
+
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(buildRequestWith("redirect-result"));
+
+        assertThat(response.getMappedChargeStatus(), is(BaseAuthoriseResponse.AuthoriseStatus.ERROR.getMappedChargeStatus()));
+        assertThat(response.getTransactionId().isPresent(), is(true));
+        assertThat(response.getTransactionId().get(), is("adyen-3ds-psp-reference"));
+    }
+
+    @Test
+    void should_return_error_when_auth3ds_result_is_null() {
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(
+                Auth3dsResponseGatewayRequest.valueOf(aValidChargeEntity().withPaymentProvider("adyen").build(), null)
+        );
+
+        assertThat(response.getMappedChargeStatus(), is(BaseAuthoriseResponse.AuthoriseStatus.ERROR.getMappedChargeStatus()));
+        assertThat(response.getTransactionId().isPresent(), is(false));
+    }
+
+    @Test
+    void should_handle_unexpected_exception_and_return_exception_status() throws Exception {
+        when(mockClient.postRequestFor(any())).thenThrow(new RuntimeException("boom"));
+
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(buildRequestWith("redirect-result"));
+
+        assertThat(response.isException(), is(true));
+        assertThat(response.toString(), is("boom"));
+    }
+
+    @Test
+    void should_return_exception_status_when_timeout_is_returned_from_gateway() throws Exception {
+        when(mockClient.postRequestFor(any())).thenThrow(new GatewayException.GatewayConnectionTimeoutException("timeout"));
+
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(buildRequestWith("redirect-result"));
+
+        assertThat(response.isException(), is(true));
+    }
+
+    @Test
+    void should_return_exception_status_when_4xx_5xx_is_returned_from_gateway() throws Exception {
+        when(mockClient.postRequestFor(any())).thenThrow(new GatewayException.GatewayErrorException("non-success", """
+                {
+                  "status": 500,
+                  "message": "failure",
+                  "errorCode": "910",
+                  "errorType": "internal",
+                  "pspReference": "error-reference"
+                }
+                """, 500));
+
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(buildRequestWith("redirect-result"));
+
+        assertThat(response.isException(), is(true));
+    }
+
+    @Test
+    void should_handle_non_parseable_error_response_and_return_exception_status() throws Exception {
+        when(mockClient.postRequestFor(any())).thenThrow(new GatewayException.GatewayErrorException("non-success", "not-a-json", 502));
+
+        var response = adyenAuthorise3dsHandler.authorise3dsResponse(buildRequestWith("redirect-result"));
+
+        assertThat(response.isException(), is(true));
+        assertThat(response.toString(), is("non-success"));
+    }
+
+    private Auth3dsResponseGatewayRequest buildRequestWith(String redirectResult) {
+        var auth3dsResult = new Auth3dsResult();
+        auth3dsResult.setRedirectResult(redirectResult);
+        return Auth3dsResponseGatewayRequest.valueOf(
+                aValidChargeEntity().withPaymentProvider("adyen").build(),
+                auth3dsResult
+        );
+    }
+
+    private String successResponse(String resultCode) {
+        return """
+                {
+                  "pspReference": "adyen-3ds-psp-reference",
+                  "resultCode": "%s"
+                }
+                """.formatted(resultCode);
+    }
+}
