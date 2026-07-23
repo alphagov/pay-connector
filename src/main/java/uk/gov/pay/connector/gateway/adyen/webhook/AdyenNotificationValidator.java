d06d52b580fa82bd0a0132445961f639e959113c
Pippa101 <94748151+Pippa101@users.noreply.github.com>
2026-07-23 13:46:16 +0100

    PP-15615: Abstract notification validation into AdyenNotificationValidator class. (#6540)
    - This is part of the work to refactor Adyen Webhooks
    - created a validation class to contain logic for validating Ip addresses from Adyen Notifications.

---
+++ src/main/java/uk/gov/pay/connector/gateway/adyen/webhook/AdyenNotificationValidator.java
@@ -0,0 +1,45 @@
+package uk.gov.pay.connector.gateway.adyen.webhook;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.pay.connector.app.adyen.AdyenGatewayConfig;
+import uk.gov.pay.connector.util.IpDomainMatcher;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static uk.gov.pay.connector.gateway.PaymentGatewayName.ADYEN;
+import static uk.gov.service.payments.logging.LoggingKeys.PROVIDER;
+
+public class AdyenNotificationValidator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdyenNotificationValidator.class);
+    private static final String NOTIFICATION_SOURCE = "notification_source";
+
+    private final IpDomainMatcher ipDomainMatcher;
+    private final String notificationDomain;
+
+    public AdyenNotificationValidator(AdyenGatewayConfig gatewayConfig, IpDomainMatcher ipDomainMatcher) {
+        this.notificationDomain = gatewayConfig.getNotificationDomain();
+        this.ipDomainMatcher = ipDomainMatcher;
+    }
+
+    public boolean isValidIpAddress(String forwardedIpAddresses) {
+        if (isBlank(forwardedIpAddresses)) {
+            LOGGER.atInfo()
+                    .setMessage("Adyen notification missing X-Forwarded-For header")
+                    .addKeyValue(PROVIDER, ADYEN.getName())
+                    .log();
+            return false;
+        }
+
+        if (!ipDomainMatcher.ipMatchesDomain(forwardedIpAddresses, notificationDomain)) {
+            LOGGER.atInfo()
+                    .addKeyValue(PROVIDER, ADYEN.getName())
+                    .addKeyValue(NOTIFICATION_SOURCE, forwardedIpAddresses)
+                    .log("Adyen notification from ip '{}' not matching configured domain '{}'",
+                            forwardedIpAddresses, notificationDomain);
+            return false;
+        }
+
+        return true;
+    }
+}
