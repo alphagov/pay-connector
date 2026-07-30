bb67850d4195e2f3c92612188960ad3fee41f715
Rafiq Gasmi <rafiq.gasmi@digital.cabinet-office.gov.uk>
2026-07-30 10:16:41 +0100

    PP-15560 - Adding Delete Stored Payment Handler and tests.
    - Refactored and Expanded AdyenMerchantAccountHelper retrieve Merchant Account using boolean

--- src/main/java/uk/gov/pay/connector/gateway/model/request/records/AdyenMerchantAccountHelper.java
+++ src/main/java/uk/gov/pay/connector/gateway/model/request/records/AdyenMerchantAccountHelper.java
@@ -2,8 +2,6 @@ package uk.gov.pay.connector.gateway.model.request.records;
 
 import jakarta.inject.Inject;
 import uk.gov.pay.connector.app.ConnectorConfiguration;
-import uk.gov.pay.connector.gateway.model.request.GatewayRequest;
-import uk.gov.pay.connector.gatewayaccount.model.GatewayAccount;
 import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntity;
 
 public class AdyenMerchantAccountHelper {
@@ -14,15 +12,15 @@ public class AdyenMerchantAccountHelper {
     public AdyenMerchantAccountHelper(ConnectorConfiguration config) {
         this.configuration = config;
     }
-    
+
     public String getMerchantAccount(GatewayAccountEntity gatewayAccountEntity) {
-        String merchantAccountId;
-        if (gatewayAccountEntity.isLive()) {
-            merchantAccountId = configuration.getAdyenGatewayConfig().getMerchantAccountIds().live();
-        } else {
-            merchantAccountId = configuration.getAdyenGatewayConfig().getMerchantAccountIds().test();
-        }
-        return merchantAccountId;
+        return getMerchantAccount(gatewayAccountEntity.isLive());
     }
-    
+
+    public String getMerchantAccount(boolean live) {
+        return live
+                ? configuration.getAdyenGatewayConfig().getMerchantAccountIds().live()
+                : configuration.getAdyenGatewayConfig().getMerchantAccountIds().test();
+    }
+
 }
