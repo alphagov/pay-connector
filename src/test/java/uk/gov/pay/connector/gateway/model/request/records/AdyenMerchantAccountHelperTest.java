df1c5fa7ce75ea12b2dd40c42a3a8475b06165bf
Rafiq Gasmi <rafiq.gasmi@digital.cabinet-office.gov.uk>
2026-07-31 14:55:23 +0100

    PP-15560 - Refactor AdyenMerchantAccountHelperTest to use parameterisedTest

--- src/test/java/uk/gov/pay/connector/gateway/model/request/records/AdyenMerchantAccountHelperTest.java
+++ src/test/java/uk/gov/pay/connector/gateway/model/request/records/AdyenMerchantAccountHelperTest.java
@@ -1,8 +1,10 @@
 package uk.gov.pay.connector.gateway.model.request.records;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.pay.connector.app.ConnectorConfiguration;
@@ -11,15 +13,19 @@ import uk.gov.pay.connector.app.adyen.AdyenIds;
 import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountEntityFixture;
 import uk.gov.pay.connector.gatewayaccount.model.GatewayAccountType;
 
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 class AdyenMerchantAccountHelperTest {
+
+    private static final String LIVE_MERCHANT_ID = "live-merchant-123";
+    private static final String TEST_MERCHANT_ID = "test-merchant-123";
     
     private AdyenMerchantAccountHelper adyenMerchantAccountHelper;
     
@@ -34,56 +40,38 @@ class AdyenMerchantAccountHelperTest {
     void setUp() {
         given(mockConnectorConfiguration.getAdyenGatewayConfig()).willReturn(mockAdyenGatewayConfig);
         given(mockAdyenGatewayConfig.getMerchantAccountIds()).willReturn(mockAdyenIds);
-        
+        lenient().when(mockAdyenIds.live()).thenReturn(LIVE_MERCHANT_ID);
+        lenient().when(mockAdyenIds.test()).thenReturn(TEST_MERCHANT_ID);
+
         adyenMerchantAccountHelper = new AdyenMerchantAccountHelper(mockConnectorConfiguration);
     }
 
-    @Test
-    void shouldReturnLiveMerchantAccountIdWhenLiveIsTrue() {
-        when(mockAdyenIds.live()).thenReturn("live-merchant-123");
+    @ParameterizedTest(name = "{0} account type returns merchant id {1}")
+    @MethodSource("gatewayAccountTypes")
+    void shouldReturnMerchantAccountForGatewayAccountEntity(GatewayAccountType accountType, String expectedMerchantId) {
+        var gatewayAccountEntity = GatewayAccountEntityFixture.aGatewayAccountEntity().withType(accountType).build();
 
-        var gatewayAccountEntity = GatewayAccountEntityFixture.aGatewayAccountEntity().withType(GatewayAccountType.LIVE).build();
-        String result = adyenMerchantAccountHelper.getMerchantAccount(gatewayAccountEntity);
-
-        assertThat(result, is("live-merchant-123"));
-
-        verify(mockAdyenIds).live();
-        verify(mockAdyenIds, never()).test();
+        assertThat(adyenMerchantAccountHelper.getMerchantAccount(gatewayAccountEntity), is(expectedMerchantId));
     }
 
-    @Test
-    void shouldReturnTestMerchantAccountIdWhenLiveIsFalse() {
-        when(mockAdyenIds.test()).thenReturn("test-merchant-123");
-
-        var gatewayAccountEntity = GatewayAccountEntityFixture.aGatewayAccountEntity().withType(GatewayAccountType.TEST).build();
-        String result = adyenMerchantAccountHelper.getMerchantAccount(gatewayAccountEntity);
-
-        assertThat(result, is("test-merchant-123"));
-
-        verify(mockAdyenIds).test();
-        verify(mockAdyenIds, never()).live();
-    }
-    
-    @Test
-    void shouldReturnLiveMerchantAccountIdWhenLiveFlagIsTrue() {
-        when(mockAdyenIds.live()).thenReturn("live-merchant-123");
-
-        String result = adyenMerchantAccountHelper.getMerchantAccount(true);
-
-        assertThat(result, is("live-merchant-123"));
-        verify(mockAdyenIds).live();
-        verify(mockAdyenIds, never()).test();
+    @ParameterizedTest(name = "live={0} returns merchant id {1}")
+    @MethodSource("liveFlags")
+    void shouldReturnMerchantAccountForLiveFlag(boolean live, String expectedMerchantId) {
+        assertThat(adyenMerchantAccountHelper.getMerchantAccount(live), is(expectedMerchantId));
     }
 
-    @Test
-    void shouldReturnTestMerchantAccountIdWhenLiveFlagIsFalse() {
-        when(mockAdyenIds.test()).thenReturn("test-merchant-123");
+    private static Stream<Arguments> gatewayAccountTypes() {
+        return Stream.of(
+                arguments(GatewayAccountType.LIVE, LIVE_MERCHANT_ID),
+                arguments(GatewayAccountType.TEST, TEST_MERCHANT_ID)
+        );
+    }
 
-        String result = adyenMerchantAccountHelper.getMerchantAccount(false);
-
-        assertThat(result, is("test-merchant-123"));
-        verify(mockAdyenIds).test();
-        verify(mockAdyenIds, never()).live();
+    private static Stream<Arguments> liveFlags() {
+        return Stream.of(
+                arguments(true, LIVE_MERCHANT_ID),
+                arguments(false, TEST_MERCHANT_ID)
+        );
     }
 
 }
