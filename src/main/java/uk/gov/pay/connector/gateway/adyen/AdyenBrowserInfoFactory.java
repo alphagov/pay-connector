6a935c7774583d1b7c7bb64c83978ee79323dcb7
joshuapook-MT <joshua.pook@madetech.com>
2026-08-12 11:06:35 +0100

    PP-15725 Extract BrowserInfo creation into factory (#6619)
    PP-15725 Extract BrowserInfo creation into factory
    
    - Added BrowserDataFor3ds interface
    - Added AdyenBrowserInfoFactory and implemented create method with defaults
    - Updated BrowserInfo to AdyenBrowserInfo
    - Updated AdyenRequestFactory to use new AdyenBrowserInfoFactory for creating browser info
    
    Co-authored-by: Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
    Co-authored-by: Alex Bishop <alexbishop1@users.noreply.github.com>

---
+++ src/main/java/uk/gov/pay/connector/gateway/adyen/AdyenBrowserInfoFactory.java
@@ -0,0 +1,49 @@
+package uk.gov.pay.connector.gateway.adyen;
+
+import uk.gov.pay.connector.gateway.adyen.response.json.AdyenBrowserInfo;
+import uk.gov.pay.connector.gateway.model.BrowserDataFor3ds;
+
+import java.util.Locale;
+import java.util.Optional;
+
+
+public class AdyenBrowserInfoFactory {
+
+    public AdyenBrowserInfo create(BrowserDataFor3ds browserDataFor3ds) {
+        return new AdyenBrowserInfo(
+                browserDataFor3ds.getBrowserAcceptHeader().orElse("text/html"),
+                browserDataFor3ds.getBrowserColorDepth().flatMap(this::parseInteger).map(this::parseColorDepth).orElse(1),
+                false,
+                browserDataFor3ds.getBrowserJavaScriptEnabled().orElse(false),
+                browserDataFor3ds.getBrowserLanguage()
+                        .filter(value -> !Locale.forLanguageTag(value).getLanguage().isEmpty())
+                        .orElse("en-GB"),
+                browserDataFor3ds.getBrowserScreenHeight().flatMap(this::parseInteger).filter(value -> value >= 0).orElse(0),
+                browserDataFor3ds.getBrowserScreenWidth().flatMap(this::parseInteger).filter(value -> value >= 0).orElse(0),
+                browserDataFor3ds.getBrowserTZ().flatMap(this::parseInteger).filter(value -> value >= -1440 && value <= 1440).orElse(0),
+                browserDataFor3ds.getBrowserUserAgent().orElse("Mozilla/5.0")
+        );
+    }
+
+    private Optional<Integer> parseInteger(String value) {
+        try {
+            return Optional.of(Integer.valueOf(value));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    private int parseColorDepth(Integer colorDepth) {
+        return switch (colorDepth) {
+            case 1, 4, 8, 15, 16, 24, 32, 48 -> colorDepth;
+            case Integer i when i > 1 && i < 4 -> 1;
+            case Integer i when i > 4 && i < 8 -> 4;
+            case Integer i when i > 8 && i < 15 -> 8;
+            case Integer i when i > 16 && i < 24 -> 16;
+            case Integer i when i > 24 && i < 32 -> 24;
+            case Integer i when i > 32 && i < 48 -> 32;
+            case Integer i when i > 48 -> 48;
+            default -> 1;
+        };
+    }
+}
--- src/test/java/uk/gov/pay/connector/gateway/adyen/AdyenRequestFactoryTest.java
+++ src/test/java/uk/gov/pay/connector/gateway/adyen/AdyenRequestFactoryTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.slf4j.event.KeyValuePair;
-import org.slf4j.event.Level;
 import uk.gov.pay.connector.agreement.model.AgreementEntity;
 import uk.gov.pay.connector.app.ConnectorConfiguration;
 import uk.gov.pay.connector.app.LinksConfig;
@@ -37,9 +35,6 @@ import uk.gov.service.payments.commons.model.CardExpiryDate;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThrows;
@@ -381,40 +376,6 @@ class AdyenRequestFactoryTest {
         assertThat(request.origin(), is(nullValue()));
         assertThat(request.shopperEmail(), is(nullValue()));
         assertThat(request.shopperIP(), is(nullValue()));
-    }
-
-    @Test
-    void should_log_warning_when_browser_info_integer_cannot_be_parsed() {
-        var authoriseRequest = aCardAuthorisationGatewayRequest()
-                .withAuthCardDetails(anAuthCardDetails()
-                        .withJsScreenColorDepth("invalid")
-                        .withJsScreenWidth("not-a-number")
-                        .withJsTimezoneOffsetMins("bad-offset")
-                        .build())
-                .withCredentials(ADYEN_CREDENTIALS)
-                .build();
-
-        adyenRequestFactory.createPaymentRequest(authoriseRequest);
-
-        var loggingEvents = logs.getEvents();
-
-        assertThat(loggingEvents.size(), is(3));
-
-        assertThat(loggingEvents, everyItem(hasProperty("level", is(Level.WARN))));
-        assertThat(loggingEvents, everyItem(hasProperty("message", is("Unable to parse browser info integer"))));
-
-        var keyValuePairs = loggingEvents.stream()
-                .flatMap(event -> event.getKeyValuePairs().stream())
-                .toList();
-
-        assertThat(keyValuePairs, contains(
-                new KeyValuePair("property", "js_screen_color_depth"),
-                new KeyValuePair("value", "invalid"),
-                new KeyValuePair("property", "js_screen_width"),
-                new KeyValuePair("value", "not-a-number"),
-                new KeyValuePair("property", "js_timezone_offset_min"),
-                new KeyValuePair("value", "bad-offset")
-        ));
     }
 
     @Test
