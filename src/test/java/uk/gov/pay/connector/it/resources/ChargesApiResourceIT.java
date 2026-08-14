fba133b28556ec0a5da002677b4965c8a6514c7d
Rafiq Gasmi <rafiq.gasmi@digital.cabinet-office.gov.uk>
2026-08-14 15:00:20 +0100

    PP-15761 - Increase Thread.sleep to 2 second

--- src/test/java/uk/gov/pay/connector/it/resources/ChargesApiResourceIT.java
+++ src/test/java/uk/gov/pay/connector/it/resources/ChargesApiResourceIT.java
@@ -102,7 +102,7 @@ public class ChargesApiResourceIT {
                 .then()
                 .statusCode(204);
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
 
         // Trigger the capture process programmatically which normally would be invoked by the scheduler.
         app.getInstanceFromGuiceContainer(CardCaptureProcess.class).handleCaptureMessages();
