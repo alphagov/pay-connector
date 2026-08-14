23a31d31eae1a81f821af1a44f8f4211c05688ea
GabrielBoardman-MadeTech <gabriel.boardman@madetech.com>
2026-08-14 11:44:44 +0100

    PP-15716 - Create AdyenGooglePayAuthRequest record
    - Created a new record with only the parameaters we
    wish to pass from frontend
    - Added a test for the record
    
    Co-authored-by: Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
    Co-authored-by: Alex Bishop <alexbishop1@users.noreply.github.com>

---
+++ src/main/java/uk/gov/pay/connector/wallets/googlepay/api/AdyenGooglePayAuthRequest.java
@@ -0,0 +1,38 @@
+package uk.gov.pay.connector.wallets.googlepay.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import uk.gov.pay.connector.wallets.WalletAuthorisationRequest;
+import uk.gov.pay.connector.wallets.WalletType;
+
+public record AdyenGooglePayAuthRequest(
+        @Schema(hidden = true)
+        @NotNull
+        @Valid
+        @JsonProperty("payment_info")
+        GooglePayPaymentInfo paymentInfo,
+
+        @NotNull
+        @Valid
+        @JsonProperty("token") 
+        String token
+) implements WalletAuthorisationRequest {
+
+    @Override
+    public GooglePayPaymentInfo getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    @Override
+    public WalletType getWalletType() {
+        return WalletType.GOOGLE_PAY;
+    }
+    
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[paymentInfo=" + paymentInfo + ", token=redacted]";
+    }
+
+}
