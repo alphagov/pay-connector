26a7953a3d73dd1d05a57d9c8244c77c1f42eba0
GabrielBoardman-MadeTech <gabriel.boardman@madetech.com>
2026-08-21 15:58:10 +0100

    PP-15721 - Create factory for AdyenGooglePayAuthorisePayload
    - created a factory for AdyenGooglePayAuthorisePayload
    - added AdyenGooglePayAuthRequest field to GooglePayAuthorisationGatewayRequest
    - created appropriate tests and test helpers
    
    Co-authored-by: Hugo Jeffreys<hjvoid@users.noreply.github.com>

--- src/main/java/uk/gov/pay/connector/wallets/googlepay/GooglePayAuthorisationGatewayRequest.java
+++ src/main/java/uk/gov/pay/connector/wallets/googlepay/GooglePayAuthorisationGatewayRequest.java
@@ -2,21 +2,36 @@ package uk.gov.pay.connector.wallets.googlepay;
 
 import uk.gov.pay.connector.charge.model.domain.ChargeEntity;
 import uk.gov.pay.connector.gateway.model.request.AuthorisationGatewayRequest;
+import uk.gov.pay.connector.wallets.googlepay.api.AdyenGooglePayAuthRequest;
 import uk.gov.pay.connector.wallets.googlepay.api.GooglePayAuthRequest;
 
 public class GooglePayAuthorisationGatewayRequest extends AuthorisationGatewayRequest {
     private GooglePayAuthRequest googlePayAuthRequest;
+    private AdyenGooglePayAuthRequest adyenGooglePayAuthRequest;
 
     public GooglePayAuthorisationGatewayRequest(ChargeEntity charge, GooglePayAuthRequest googlePayAuthRequest) {
         super(charge);
         this.googlePayAuthRequest = googlePayAuthRequest;
     }
 
+    public GooglePayAuthorisationGatewayRequest(ChargeEntity charge, AdyenGooglePayAuthRequest googlePayAuthRequest) {
+        super(charge);
+        this.adyenGooglePayAuthRequest = googlePayAuthRequest;
+    }
+
     public GooglePayAuthRequest getGooglePayAuthRequest() {
         return googlePayAuthRequest;
     }
+    
+    public AdyenGooglePayAuthRequest getAdyenGooglePayAuthRequest() {
+        return adyenGooglePayAuthRequest;
+    }
 
     public static GooglePayAuthorisationGatewayRequest valueOf(ChargeEntity charge, GooglePayAuthRequest googlePayAuthRequest) {
+        return new GooglePayAuthorisationGatewayRequest(charge, googlePayAuthRequest);
+    }
+
+    public static GooglePayAuthorisationGatewayRequest valueOf(ChargeEntity charge, AdyenGooglePayAuthRequest googlePayAuthRequest) {
         return new GooglePayAuthorisationGatewayRequest(charge, googlePayAuthRequest);
     }
 }
